f491c95fe67d7afb31360d332cf4714d164dfc32
Aaron Craelius <Arc@Aarons-MacBook-Air-3.local>
2018-08-20 15:31:04 -0400

    Converted to multi-project gradle format

--- ceres-base/src/test/kotlin/ceres/AVLTest.kt
+++ ceres-base/src/test/kotlin/ceres/AVLTest.kt
@@ -1,6 +1,10 @@
 package ceres.AVL
 
-class TestAVLTree: StringSpec() {
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
+class AVLTest {
+    @Test fun test() {
+        assertEquals(1, 4)
+    }
 }
-
