78138a1f7ba2dc38004b1389545770e2549dcd9c
Aaron Craelius <aaron@regen.network>
2018-12-19 12:34:41 -0500

    Added nix build config

---
+++ default.nix
@@ -0,0 +1,25 @@
+with import <nixpkgs>{};
+
+buildGoPackage rec {
+  name = "regen-ledger";
+
+  goPackagePath = "gitlab.com/regen-network/regen-ledger";
+  subPackages = [ "cmd/xrnd" "cmd/xrncli" ];
+
+  src = ./.;
+
+  # buildInputs = [ makeWrapper ];
+  # binPath = lib.makeBinPath [ ];
+
+  goDeps = ./deps.nix;
+
+  #postInstall = ''
+  #  wrapProgram $bin/bin/dep2nix --prefix PATH ':' ${binPath}
+  #'';
+
+  meta = with stdenv.lib; {
+    description = "Regen Networks's Regen Ledger distributed ledger for planetary regeneration";
+    # TODO license = licenses.bsd3;
+    homepage = https://gitlab.com/regen-network/regen-ledger;
+  };
+}
