39e500e11bbcdb1581fa571cdc09bf5fd6075c0a
Aaron Craelius <aaron@regen.network>
2018-12-20 11:36:30 -0500

    Implemented xrncli init

--- cmd/xrncli/main.go
+++ cmd/xrncli/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/spf13/viper"
 	"os"
+	"path/filepath"
+	"text/template"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/keys"
@@ -16,20 +19,21 @@ import (
 	"github.com/tendermint/tendermint/libs/cli"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	cmn "github.com/tendermint/tendermint/libs/common"
 	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
 	auth "github.com/cosmos/cosmos-sdk/x/auth/client/rest"
 	bankcmd "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
 	bank "github.com/cosmos/cosmos-sdk/x/bank/client/rest"
-	dataclient "gitlab.com/regen-network/regen-ledger/x/data/client"
-	agentclient "gitlab.com/regen-network/regen-ledger/x/agent/client"
-	datarest "gitlab.com/regen-network/regen-ledger/x/data/client/rest"
 	"gitlab.com/regen-network/regen-ledger"
+	agentclient "gitlab.com/regen-network/regen-ledger/x/agent/client"
+	dataclient "gitlab.com/regen-network/regen-ledger/x/data/client"
+	datarest "gitlab.com/regen-network/regen-ledger/x/data/client/rest"
 )
 
 const (
-	storeAcc = "acc"
+	storeAcc   = "acc"
 	storeData  = "data"
-	storeAgent  = "agent"
+	storeAgent = "agent"
 )
 
 var defaultCLIHome = os.ExpandEnv("$HOME/.xrncli")
@@ -86,24 +90,76 @@ const (
 	flagValHash = "validator-set"
 )
 
+const defaultConfigTemplate = `# This is a TOML config file.
+# For more information, see https://github.com/toml-lang/toml
+
+##### main base config options #####
+
+# The node to connect to
+node = "{{ .Node }}"
+
+# The chain ID
+chain-id = "{{ .ChainId }}"
+`
+
+type cliConfig struct {
+	Node    string
+	ChainId string
+}
+
 // not implemented in "github.com/cosmos/cosmos-sdk/client/rpc"
 // so implementing here
 func initClientCommand() *cobra.Command {
+	var home, node, chainId string
+
 	cmd := &cobra.Command{
 		Use:   "init",
 		Short: "Initialize light client",
 		RunE: func(cmd *cobra.Command, args []string) error {
-            fmt.Println("Not implemented")
-            return nil
+			configTemplate, err := template.New("configFileTemplate").Parse(defaultConfigTemplate)
+
+			if err != nil {
+				panic(err)
+			}
+
+			home = os.ExpandEnv(home)
+
+			if err := cmn.EnsureDir(home, 0700); err != nil {
+				cmn.PanicSanity(err.Error())
+			}
+
+			if err := cmn.EnsureDir(filepath.Join(home, "config"), 0700); err != nil {
+				cmn.PanicSanity(err.Error())
+			}
+
+			configFilePath := filepath.Join(home, "config/config.toml")
+
+			if !cmn.FileExists(configFilePath) {
+				var buffer bytes.Buffer
+
+				if err := configTemplate.Execute(&buffer, cliConfig{
+					Node:node,
+					ChainId:chainId,
+				}); err != nil {
+					panic(err)
+				}
+
+				cmn.MustWriteFile(configFilePath, buffer.Bytes(), 0644)
+			} else {
+				fmt.Println("config.toml already exists")
+			}
+
+			return nil
 		},
 	}
-	cmd.Flags().StringP(client.FlagChainID, "c", "", "ID of chain we connect to")
-	cmd.Flags().StringP(client.FlagNode, "n", "tcp://localhost:26657", "Node to connect to")
-	cmd.Flags().String(flagGenesis, "", "Genesis file to verify header validity")
-	cmd.Flags().String(flagCommit, "", "File with trusted and signed header")
-	cmd.Flags().String(flagValHash, "", "Hash of trusted validator set (hex-encoded)")
-	viper.BindPFlag(client.FlagChainID, cmd.Flags().Lookup(client.FlagChainID))
-	viper.BindPFlag(client.FlagNode, cmd.Flags().Lookup(client.FlagNode))
+	cmd.Flags().StringVar(&home, cli.HomeFlag, "$HOME/.xrncli", "directory for config and data")
+	cmd.Flags().StringVar(&chainId, client.FlagChainID, "", "ID of chain we connect to")
+	cmd.Flags().StringVar(&node, client.FlagNode, "tcp://localhost:26657", "Node to connect to")
+	//cmd.Flags().String(flagGenesis, "", "Genesis file to verify header validity")
+	//cmd.Flags().String(flagCommit, "", "File with trusted and signed header")
+	//cmd.Flags().String(flagValHash, "", "Hash of trusted validator set (hex-encoded)")
+	//viper.BindPFlag(client.FlagChainID, cmd.Flags().Lookup(client.FlagChainID))
+	//viper.BindPFlag(client.FlagNode, cmd.Flags().Lookup(client.FlagNode))
 
 	return cmd
 }
@@ -170,4 +226,3 @@ func addNodeFlags(cmd *cobra.Command) {
 	cmd.Flags().Bool(client.FlagTrustNode, false, "Trust connected full node (don't verify proofs for responses)")
 	cmd.Flags().Bool(client.FlagChainID, false, "Chain ID of Tendermint node")
 }
-
