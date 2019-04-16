b38f51054807a9cbe335230f4b770828f1e1cb5a
Aaron Craelius <aaron@regen.network>
2019-04-16 19:14:04 +0000

    update upgrade scripts

---
+++ scripts/prepare-upgrade.sh
@@ -0,0 +1,10 @@
+set -x
+UPGRADE_COMMIT=$1
+
+if  [ $UPGRADE_COMMIT != "null" ]; then
+    echo "Doing upgrade to $UPGRADE_COMMIT"
+    cd $REGEN_LEDGER_REPO
+    git fetch
+    git clean -f
+    git checkout -f $UPGRADE_COMMIT
+fi
