d5e0b974b96b1c1c904d066773694224e78a5eb2
anilCSE <anil@vitwit.com>
2020-01-24 01:00:25 +0530

    Add patagonia proposal upgrade handler

--- app/app.go
+++ app/app.go
@@ -205,9 +205,9 @@ func NewXrnApp(logger log.Logger, db dbm.DB, traceStore io.Writer, loadLatest bo
 	app.upgradeKeeper = upgrade.NewKeeper(app.upgradeStoreKey, app.cdc)
 
 	// this configures a no-op upgrade handler for the "patagonia" upgrade
-	app.upgradeKeeper.SetUpgradeHandler("darien-gap", func(ctx sdk.Context, plan upgrade.Plan) {
+	app.upgradeKeeper.SetUpgradeHandler("patagonia", func(ctx sdk.Context, plan upgrade.Plan) {
 		// Add some more coins to the faucet account
-		addr, err := sdk.AccAddressFromBech32("xrn:1xrsmkqh305m09tc5x73v4t3wtcuqmf6cgy2taw")
+		addr, err := sdk.AccAddressFromBech32("xrn:1vkxgpw4xtyeljzvqnxxy84kpa6udqaqw8leqjg")
 		if err == nil {
 			_, _ = app.bankKeeper.AddCoins(ctx, addr, sdk.Coins{sdk.Coin{Denom: "tree", Amount: sdk.NewInt(10000000)}})
 		}
