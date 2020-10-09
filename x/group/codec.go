780aef5f41957291ff77525900f031d4db187991
atheesh <atheesh@vitwit.com>
2020-10-09 12:34:00 +0530

    cleaning x folder

--- x/group/codec.go
+++
@@ -1,9 +0,0 @@
-package group
-
-import "github.com/cosmos/cosmos-sdk/codec"
-
-func RegisterCodec(cdc *codec.Codec) {
-	cdc.RegisterConcrete(MsgCreateGroup{}, "group/MsgCreateGroup", nil)
-	cdc.RegisterConcrete(Group{}, "group/Group", nil)
-	cdc.RegisterConcrete(GroupAccount{}, "group/GroupAccount", nil)
-}
