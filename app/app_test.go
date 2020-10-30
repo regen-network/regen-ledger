c8a26c93bc1993cecb495afa51949699470997c3
atheesh <atheesh@vitwit.com>
2020-10-30 12:51:00 +0530

    lint

--- app/app_test.go
+++ app/app_test.go
@@ -10,8 +10,6 @@ import (
 	"github.com/tendermint/tendermint/libs/log"
 	db "github.com/tendermint/tm-db"
 
-	"github.com/cosmos/cosmos-sdk/codec"
-
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
@@ -36,25 +34,6 @@ func TestGaiadExport(t *testing.T) {
 	newGapp := NewXrnApp(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, map[int64]bool{}, DefaultNodeHome, 0, MakeEncodingConfig())
 	_, err = newGapp.ExportAppStateAndValidators(false, []string{})
 	require.NoError(t, err, "ExportAppStateAndValidators should not have an error")
-}
-
-func setGenesis(gapp *XrnApp) error {
-
-	genesisState := NewDefaultGenesisState()
-	stateBytes, err := codec.MarshalJSONIndent(gapp.cdc, genesisState)
-	if err != nil {
-		return err
-	}
-
-	// Initialize the chain
-	gapp.InitChain(
-		abci.RequestInitChain{
-			Validators:    []abci.ValidatorUpdate{},
-			AppStateBytes: stateBytes,
-		},
-	)
-	gapp.Commit()
-	return nil
 }
 
 // ensure that blocked addresses are properly set in bank keeper
