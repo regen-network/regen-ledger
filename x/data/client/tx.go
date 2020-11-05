5589bdd2a502b624c09abaa60ec051e6871e51fc
Amaury Martiny <amaury.martiny@protonmail.com>
2020-11-05 11:32:06 +0100

    Add MsgAnchorDataCmd

--- x/data/client/tx.go
+++ x/data/client/tx.go
@@ -1,11 +1,16 @@
 package client
 
 import (
+	"context"
+	"encoding/base64"
+
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
-	"github.com/regen-network/regen-ledger/x/data"
 	"github.com/spf13/cobra"
+
+	"github.com/regen-network/regen-ledger/util"
+	"github.com/regen-network/regen-ledger/x/data"
 )
 
 // TxCmd returns a root CLI command handler for all x/data transaction commands.
@@ -25,12 +30,13 @@ func TxCmd() *cobra.Command {
 	return cmd
 }
 
+// MsgAnchorDataCmd created a CLI tx command for MsgAnchorData.
 func MsgAnchorDataCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use: "send [from_key_or_address] [to_address] [amount]",
-		Short: `Send funds from one account to another. Note, the'--from' flag is
-ignored as it is implied from [from_key_or_address].`,
-		Args: cobra.ExactArgs(3),
+		Use: "anchor [cid]",
+		Short: `Anchors a piece of data to the blockchain based on its secure
+		hash, effectively providing a tamper resistant timestamp.`,
+		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx := client.GetClientContextFromCmd(cmd)
 			clientCtx, err := client.ReadTxCommandFlags(clientCtx, cmd.Flags())
@@ -38,9 +44,23 @@ ignored as it is implied from [from_key_or_address].`,
 				return err
 			}
 
-			// TODO
+			cidBz, err := base64.StdEncoding.DecodeString(args[0])
+			if err != nil {
+				return err
+			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags())
+			msg := data.MsgAnchorDataRequest{
+				Sender: clientCtx.GetFromAddress().String(),
+				Cid:    cidBz,
+			}
+			svcMsgClientConn := &util.ServiceMsgClientConn{}
+			msgClient := data.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.AnchorData(context.Background(), &msg)
+			if err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.Msgs...)
 		},
 	}
 
