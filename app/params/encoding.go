90116ae92f36957a1c4a8852333a067ed3d051a3
aleem1413 <aleem@vitwit.com>
2020-11-06 11:10:25 +0530

    review changes

---
+++ app/params/encoding.go
@@ -0,0 +1,15 @@
+package params
+
+import (
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+)
+
+// EncodingConfig specifies the concrete encoding types to used for regen app.
+type EncodingConfig struct {
+	InterfaceRegistry types.InterfaceRegistry
+	Marshaler         codec.Marshaler
+	TxConfig          client.TxConfig
+	Amino             *codec.LegacyAmino // or even nil may be?
+}
