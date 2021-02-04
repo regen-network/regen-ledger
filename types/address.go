e07a770e8f1ff77644405dfd6e147961a3892392
Aaron Craelius <aaronc@users.noreply.github.com>
2021-02-04 13:17:52 -0500

    revert address changes until 0.42 alpha1

--- types/address.go
+++ types/address.go
@@ -3,10 +3,11 @@ package types
 import "crypto/sha256"
 
 func AddressHash(prefix string, contents []byte) []byte {
-	prefixHash := sha256.Sum256([]byte(prefix))
-	preImage := make([]byte, 32+len(contents))
-	preImage = append(preImage, prefixHash[:]...)
-	preImage = append(preImage, contents...)
+	preImage := []byte(prefix)
+	if len(contents) != 0 {
+		preImage = append(preImage, 0)
+		preImage = append(preImage, contents...)
+	}
 	sum := sha256.Sum256(preImage)
-	return sum[:32]
+	return sum[:20]
 }
