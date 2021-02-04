da63c7f2571d3a302cf1eae5e9ee00b8418b4c71
Aaron Craelius <aaronc@users.noreply.github.com>
2021-02-04 17:28:58 -0500

    update docs

--- x/data/server/lookup/doc.go
+++ x/data/server/lookup/doc.go
@@ -8,9 +8,12 @@ were chosen based on testing and should provide a good balance of performance an
 efficiency.
 
 Shortened identifiers are created or retrieved using the idempotent Table.GetOrCreateID method.
-Identifiers will be 4 bytes long and retrieved with a single KV-store lookup in the vast majority
-of cases and will sometimes be 5 or rarely 6 bytes long and require 2 to 3 total reads. In some
-rare cases (which have not appeared in tests), identifiers may be longer or require more lookups.
+
+Using the default algorithm which uses the first 4 bytes of a 64-bit FNV-1a hash and then
+increases the length the the case of collisions, identifiers will be 4 bytes long and retrieved
+with a single KV-store lookup in the vast majority of cases and will sometimes be 5 or rarely
+6 bytes long and require 2 to 3 total reads. In some rare cases (which have not appeared in tests),
+identifiers may be longer or require more lookups.
 
 Given a shortened identifier, the underlying binary data can be retrieved with the Table.GetValue
 method.
