faca8c47ebaef6df9acafcd0f1df88dbed4484a1
aleem1413 <aleem@vitwit.com>
2021-02-08 10:28:49 +0530

    delete proto files

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -28,7 +28,7 @@ go mod tidy
 
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  protoc \
+  buf protoc \
   -I "proto" \
   -I "third_party/proto" \
   --gocosmos_out=\
@@ -39,7 +39,7 @@ Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
   $(find "${dir}" -maxdepth 1 -name '*.proto')
 
-  protoc \
+  buf protoc \
   -I "proto" \
   -I "third_party/proto" \
   --go-cosmos2_out=\
@@ -51,7 +51,7 @@ google/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
   $(find "${dir}" -maxdepth 1 -name '*.proto')
 
   # command to generate gRPC gateway (*.pb.gw.go in respective modules) files
-  protoc \
+  buf protoc \
   -I "proto" \
   -I "third_party/proto" \
   --grpc-gateway_out=logtostderr=true:. \
@@ -73,7 +73,7 @@ google/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
 done
 
 # generate codec/testdata proto code
-protoc -I "proto" -I "third_party/proto" -I "testutil/testdata" --gocosmos_out=plugins=interfacetype+grpc,\
+buf protoc -I "proto" -I "third_party/proto" -I "testutil/testdata" --gocosmos_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. ./testutil/testdata/*.proto
 
 # move proto files to the right places
