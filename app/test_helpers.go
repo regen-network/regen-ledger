4da22a1329d422db0e285d034b963f22f5492a01
atheesh <atheesh@vitwit.com>
2021-02-26 12:04:34 +0530

    review changes

--- app/test_helpers.go
+++
@@ -1,9 +0,0 @@
-package app
-
-// EmptyAppOptions is a stub implementing AppOptions
-type EmptyAppOptions struct{}
-
-// Get implements AppOptions
-func (ao EmptyAppOptions) Get(o string) interface{} {
-	return nil
-}
