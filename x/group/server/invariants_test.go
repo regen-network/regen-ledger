e4327fcd2b42901a41bfb00b3a1d72348b7f2030
likhita-809 <likhita@vitwit.com>
2021-03-20 12:41:26 +0530

    invariant test in progress

---
+++ x/group/server/invariants_test.go
@@ -0,0 +1,24 @@
+package server
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/regen-network/regen-ledger/orm"
+)
+
+func TesttallyVotesInvariant(t *testing.T) {
+	// ff := server.NewFixtureFactory(t, 6)
+	var ctx sdk.Context //ff.Setup().Context()
+	// c := ff.Setup().Context()
+	blockHeight := ctx.BlockHeight()
+	var proposalTable orm.AutoUInt64Table
+
+	invar := Invar{
+		sdkCtx:        ctx,
+		proposalTable: proposalTable,
+	}
+	invarRes := tallyVotesInvariant(invar)
+
+}
