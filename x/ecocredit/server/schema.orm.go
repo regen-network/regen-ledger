69f4b16d8db5153385ff9c25be52bf97dcbf621f
Aaron Craelius <aaronc@users.noreply.github.com>
2021-04-06 10:38:22 -0400

    ideas

---
+++ x/ecocredit/server/schema.orm.go
@@ -0,0 +1,11 @@
+package server
+
+import "github.com/regen-network/regen-ledger/x/group"
+
+func SaveGroupInfo(info *group.GroupInfo) error {
+	panic("TODO")
+}
+
+func GetGroupInfo(groupId uint64) (*group.GroupInfo, error) {
+	panic("TODO")
+}
