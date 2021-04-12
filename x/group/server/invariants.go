6d4af8201515a64284f98831c2cc6d4f5d7ad953
likhita-809 <likhita@vitwit.com>
2021-04-12 13:17:26 +0530

    fix lint issues

--- x/group/server/invariants.go
+++ x/group/server/invariants.go
@@ -137,10 +137,10 @@ func groupTotalWeightInvariant(ctx sdk.Context, groupTable orm.Table, groupMembe
 	membersWeight := apd.New(0, 0)
 	for i := 0; i < len(groupInfo); i++ {
 		memIt, err := groupMemberByGroupIndex.Get(ctx, groupInfo[i].GroupId)
-		defer memIt.Close()
 		if err != nil {
 			return msg, broken, err
 		}
+		defer memIt.Close()
 		for {
 			_, err = memIt.LoadNext(&groupMember)
 			if err != nil {
