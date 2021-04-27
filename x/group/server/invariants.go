b5dd71d385d9fb045f6e0bd098c23c6accd47803
likhita-809 <likhita@vitwit.com>
2021-04-27 15:34:32 +0530

    wip invariants

--- x/group/server/invariants.go
+++ x/group/server/invariants.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"fmt"
 	"math"
 
 	"github.com/cockroachdb/apd/v2"
@@ -52,8 +51,7 @@ func (s serverImpl) groupTotalWeightInvariant() sdk.Invariant {
 func (s serverImpl) proposalTallyInvariant() sdk.Invariant {
 	return func(ctx sdk.Context) (string, bool) {
 		// msg, broken, err := proposalTallyInvariant(ctx, s.proposalTable, s.voteByProposalIndex)
-		msg, broken, err := proposalTallyInvariant(ctx, s.proposalTable, s.voteByProposalIndex, s.groupMemberTable, s.groupAccountTable)
-
+		msg, broken, err := proposalTallyInvariant(ctx, s.groupAccountByGroupIndex, s.groupMemberByGroupIndex, s.proposalByGroupAccountIndex, s.groupTable)
 		if err != nil {
 			panic(err)
 		}
@@ -185,60 +183,83 @@ func groupTotalWeightInvariant(ctx sdk.Context, groupTable orm.Table, groupMembe
 	return msg, broken, err
 }
 
-func proposalTallyInvariant(ctx sdk.Context, proposalTable orm.AutoUInt64Table, voteByProposalIndex orm.UInt64Index, groupMemberTable orm.PrimaryKeyTable, groupAccountTable orm.PrimaryKeyTable) (string, bool, error) {
-
+// func proposalTallyInvariant(ctx sdk.Context, proposalTable orm.AutoUInt64Table, groupAccountByGroupIndex orm.UInt64Index, groupMemberByGroupIndex orm.UInt64Index, voteByProposalIndex orm.UInt64Index, proposalByGroupAccountIndex orm.Index, groupMemberTable orm.PrimaryKeyTable, groupAccountTable orm.PrimaryKeyTable, groupTable orm.Table, voteByVoterIndex orm.Index) (string, bool, error) {
+func proposalTallyInvariant(ctx sdk.Context, groupAccountByGroupIndex orm.UInt64Index, groupMemberByGroupIndex orm.UInt64Index, proposalByGroupAccountIndex orm.Index, groupTable orm.Table) (string, bool, error) {
 	var msg string
 	var broken bool
 
-	var proposal []group.Proposal
+	var groupInfo group.GroupInfo
+	var proposal group.Proposal
 	// var vote group.Vote
-	var groupMember []group.GroupMember
-	var groupAcc []group.GroupAccountInfo
+	var groupMember group.GroupMember
+	var groupAcc group.GroupAccountInfo
 
-	proposalIt, err := proposalTable.PrefixScan(ctx, 1, math.MaxUint64)
+	groupIt, err := groupTable.PrefixScan(ctx, nil, nil)
 	if err != nil {
 		return msg, broken, err
 	}
+	defer groupIt.Close()
 
-	_, err = orm.ReadAll(proposalIt, &proposal)
-	if err != nil {
-		return msg, broken, err
-	}
-
-	groupMemIt, err := groupMemberTable.PrefixScan(ctx, nil, nil)
-	if err != nil {
-		return msg, broken, err
-	}
-
-	_, err = orm.ReadAll(groupMemIt, &groupMember)
-	if err != nil {
-		return msg, broken, err
-	}
-
-	groupAccIt, err := groupAccountTable.PrefixScan(ctx, nil, nil)
-	if err != nil {
-		return msg, broken, err
-	}
-	_, err = orm.ReadAll(groupAccIt, &groupAcc)
-	fmt.Println(err)
-	fmt.Println(len(groupAcc))
-	fmt.Println(len(groupMember))
-	panic("")
-	if err != nil {
-		return msg, broken, err
-	}
-
-	// address, err := sdk.AccAddressFromBech32(proposal[0].Address)
-	// if err != nil {
-	// 	return msg, broken, sdkerrors.Wrap(err, "group account")
-	// }
-
-	// err = groupAccountTable.GetOne(ctx, address.Bytes(), &groupAcc)
-	// // fmt.Println(err)
-	// // panic("")
+	// proposalIt, err := proposalTable.PrefixScan(ctx, 1, math.MaxUint64)
 	// if err != nil {
 	// 	return msg, broken, err
 	// }
+	// _, err = orm.ReadAll(proposalIt, &proposal)
+	// if err != nil {
+	// 	return msg, broken, err
+	// }
+
+	var membersWeight *apd.Decimal = apd.New(0, 0)
+	for {
+		_, err := groupIt.LoadNext(&groupInfo)
+		if orm.ErrIteratorDone.Is(err) {
+			break
+		}
+		groupAccIt, err := groupAccountByGroupIndex.Get(ctx, groupInfo.GroupId)
+		if err != nil {
+			return msg, broken, err
+		}
+
+		for {
+			_, err := groupAccIt.LoadNext(&groupAcc)
+			if orm.ErrIteratorDone.Is(err) {
+				break
+			}
+
+			proposalIt, err := proposalByGroupAccountIndex.Get(ctx, []byte(groupAcc.Address))
+			if err != nil {
+				return msg, broken, err
+			}
+
+			for {
+				_, err := proposalIt.LoadNext(&proposal)
+				if orm.ErrIteratorDone.Is(err) {
+					break
+				}
+			}
+			groupMemIt, err := groupMemberByGroupIndex.Get(ctx, groupAcc.GroupId)
+			if err != nil {
+				return msg, broken, err
+			}
+
+			for {
+				_, err := groupMemIt.LoadNext(&groupMember)
+				if orm.ErrIteratorDone.Is(err) {
+					break
+				}
+				curMemWeight, err := regenMath.ParseNonNegativeDecimal(groupMember.GetMember().GetWeight())
+				if err != nil {
+					return msg, broken, err
+				}
+				err = regenMath.Add(membersWeight, membersWeight, curMemWeight)
+				if err != nil {
+					return msg, broken, err
+				}
+			}
+
+		}
+
+	}
 	return msg, broken, err
 }
 
--- x/group/server/invariants_test.go
+++ x/group/server/invariants_test.go
@@ -21,6 +21,7 @@ import (
 
 func getCtxCodecKey(t *testing.T) (sdk.Context, *codec.ProtoCodec, *sdk.KVStoreKey) {
 	interfaceRegistry := types.NewInterfaceRegistry()
+	group.RegisterTypes(interfaceRegistry)
 	cdc := codec.NewProtoCodec(interfaceRegistry)
 	key := sdk.NewKVStoreKey(group.ModuleName)
 	db := dbm.NewMemDB()
@@ -344,35 +345,63 @@ func TestGroupTotalWeightInvariant(t *testing.T) {
 func TestProposalTallyInvariant(t *testing.T) {
 	curCtx, cdc, key := getCtxCodecKey(t)
 
+	// Group Table
+	groupTableBuilder := orm.NewTableBuilder(GroupTablePrefix, key, &group.GroupInfo{}, orm.FixLengthIndexKeys(orm.EncodedSeqLength), cdc)
+	groupTable := groupTableBuilder.Build()
+
 	// Group Account Table
 	_ = orm.NewSequence(key, GroupAccountTableSeqPrefix)
 	groupAccountTableBuilder := orm.NewPrimaryKeyTableBuilder(GroupAccountTablePrefix, key, &group.GroupAccountInfo{}, orm.Max255DynamicLengthIndexKeyCodec{}, cdc)
+	groupAccountByGroupIndex := orm.NewUInt64Index(groupAccountTableBuilder, GroupAccountByGroupIndexPrefix, func(value interface{}) ([]uint64, error) {
+		group := value.(*group.GroupAccountInfo).GroupId
+		return []uint64{group}, nil
+	})
 	groupAccountTable := groupAccountTableBuilder.Build()
 
 	// Group Member Table
 	groupMemberTableBuilder := orm.NewPrimaryKeyTableBuilder(GroupMemberTablePrefix, key, &group.GroupMember{}, orm.Max255DynamicLengthIndexKeyCodec{}, cdc)
+	groupMemberByGroupIndex := orm.NewUInt64Index(groupMemberTableBuilder, GroupMemberByGroupIndexPrefix, func(val interface{}) ([]uint64, error) {
+		group := val.(*group.GroupMember).GroupId
+		return []uint64{group}, nil
+	})
 	groupMemberTable := groupMemberTableBuilder.Build()
 
 	// Proposal Table
 	proposalTableBuilder := orm.NewAutoUInt64TableBuilder(ProposalTablePrefix, ProposalTableSeqPrefix, key, &group.Proposal{}, cdc)
+	proposalByGroupAccountIndex := orm.NewIndex(proposalTableBuilder, ProposalByGroupAccountIndexPrefix, func(value interface{}) ([]orm.RowID, error) {
+		account := value.(*group.Proposal).Address
+		addr, err := sdk.AccAddressFromBech32(account)
+		if err != nil {
+			return nil, err
+		}
+		return []orm.RowID{addr.Bytes()}, nil
+	})
 	proposalTable := proposalTableBuilder.Build()
 
 	// Vote Table
 	voteTableBuilder := orm.NewPrimaryKeyTableBuilder(VoteTablePrefix, key, &group.Vote{}, orm.Max255DynamicLengthIndexKeyCodec{}, cdc)
-	voteByProposalIndex := orm.NewUInt64Index(voteTableBuilder, VoteByProposalIndexPrefix, func(value interface{}) ([]uint64, error) {
-		return []uint64{value.(*group.Vote).ProposalId}, nil
-	})
+	// voteByProposalIndex := orm.NewUInt64Index(voteTableBuilder, VoteByProposalIndexPrefix, func(value interface{}) ([]uint64, error) {
+	// 	return []uint64{value.(*group.Vote).ProposalId}, nil
+	// })
+	// voteByVoterIndex := orm.NewIndex(voteTableBuilder, VoteByVoterIndexPrefix, func(value interface{}) ([]orm.RowID, error) {
+	// 	addr, err := sdk.AccAddressFromBech32(value.(*group.Vote).Voter)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// 	return []orm.RowID{addr.Bytes()}, nil
+	// })
 	voteTable := voteTableBuilder.Build()
 
 	_, _, adminAddr := testdata.KeyTestPubAddr()
 	_, _, addr1 := testdata.KeyTestPubAddr()
-	// _, _, addr2 := testdata.KeyTestPubAddr()
+	_, _, addr2 := testdata.KeyTestPubAddr()
 	// _, _, addr3 := testdata.KeyTestPubAddr()
 
 	curBlockTime, err := gogotypes.TimestampProto(curCtx.BlockTime())
 	require.NoError(t, err)
 
 	specs := map[string]struct {
+		groupReq    []*group.GroupInfo
 		groupAccReq []*group.GroupAccountInfo
 		policy      group.DecisionPolicy
 		membersReq  []*group.GroupMember
@@ -381,6 +410,32 @@ func TestProposalTallyInvariant(t *testing.T) {
 		expErr      bool
 	}{
 		"invariant not broken": {
+			groupReq: []*group.GroupInfo{
+				{
+					GroupId:     1,
+					Admin:       adminAddr.String(),
+					Version:     1,
+					TotalWeight: "3",
+				},
+			},
+			groupAccReq: []*group.GroupAccountInfo{
+				{
+					Address: addr1.String(),
+					GroupId: 1,
+					Admin:   adminAddr.String(),
+					Version: 1,
+				},
+				{
+					Address: addr2.String(),
+					GroupId: 1,
+					Admin:   adminAddr.String(),
+					Version: 1,
+				},
+			},
+			policy: group.NewThresholdDecisionPolicy(
+				"1",
+				gogotypes.Duration{Seconds: 1},
+			),
 			membersReq: []*group.GroupMember{
 				{
 					GroupId: 1,
@@ -389,15 +444,11 @@ func TestProposalTallyInvariant(t *testing.T) {
 						Weight:  "2",
 					},
 				},
-			},
-			voteReq: []*group.Vote{
 				{
-					ProposalId: 1,
-					Voter:      addr1.String(),
-					Choice:     group.Choice_CHOICE_YES,
-					SubmittedAt: gogotypes.Timestamp{
-						Seconds: timestamppb.Now().Seconds,
-						Nanos:   timestamppb.Now().Nanos,
+					GroupId: 1,
+					Member: &group.Member{
+						Address: addr2.String(),
+						Weight:  "1",
 					},
 				},
 			},
@@ -411,46 +462,45 @@ func TestProposalTallyInvariant(t *testing.T) {
 					GroupAccountVersion: 1,
 					Status:              group.ProposalStatusSubmitted,
 					Result:              group.ProposalResultUnfinalized,
-					VoteState:           group.Tally{YesCount: "2", NoCount: "0", AbstainCount: "0", VetoCount: "0"},
+					VoteState:           group.Tally{YesCount: "2", NoCount: "1", AbstainCount: "0", VetoCount: "0"},
 					Timeout:             gogotypes.Timestamp{Seconds: 600},
 					ExecutorResult:      group.ProposalExecutorResultNotRun,
 				},
 			},
-			groupAccReq: []*group.GroupAccountInfo{
+			voteReq: []*group.Vote{
 				{
-					Address: addr1.String(),
-					GroupId: 1,
-					Admin:   adminAddr.String(),
-					Version: 1,
+					ProposalId: 1,
+					Voter:      addr1.String(),
+					Choice:     group.Choice_CHOICE_YES,
+					SubmittedAt: gogotypes.Timestamp{
+						Seconds: timestamppb.Now().Seconds,
+						Nanos:   timestamppb.Now().Nanos,
+					},
+				},
+				{
+					ProposalId: 1,
+					Voter:      addr2.String(),
+					Choice:     group.Choice_CHOICE_NO,
+					SubmittedAt: gogotypes.Timestamp{
+						Seconds: timestamppb.Now().Seconds,
+						Nanos:   timestamppb.Now().Nanos,
+					},
 				},
 			},
-			policy: group.NewThresholdDecisionPolicy(
-				"1",
-				gogotypes.Duration{Seconds: 1},
-			),
 			expErr: false,
 		},
 	}
 
 	for _, spec := range specs {
 		cacheCurCtx, _ := curCtx.CacheContext()
+		groupReq := spec.groupReq
 		proposals := spec.proposalReq
 		members := spec.membersReq
 		votes := spec.voteReq
 		groupAcc := spec.groupAccReq
 
-		for i := 0; i < len(proposals); i++ {
-			_, err = proposalTable.Create(cacheCurCtx, proposals[i])
-			require.NoError(t, err)
-		}
-
-		for i := 0; i < len(members); i++ {
-			err = groupMemberTable.Create(cacheCurCtx, members[i])
-			require.NoError(t, err)
-		}
-
-		for i := 0; i < len(votes); i++ {
-			err = voteTable.Create(cacheCurCtx, votes[i])
+		for i := 0; i < len(groupReq); i++ {
+			err := groupTable.Create(cacheCurCtx, group.ID(groupReq[i].GroupId).Bytes(), groupReq[i])
 			require.NoError(t, err)
 		}
 
@@ -464,7 +514,23 @@ func TestProposalTallyInvariant(t *testing.T) {
 			require.NoError(t, err)
 		}
 
-		_, broken, _ := proposalTallyInvariant(cacheCurCtx, proposalTable, voteByProposalIndex, groupMemberTable, groupAccountTable)
+		for i := 0; i < len(members); i++ {
+			err = groupMemberTable.Create(cacheCurCtx, members[i])
+			require.NoError(t, err)
+		}
+
+		for i := 0; i < len(proposals); i++ {
+			_, err = proposalTable.Create(cacheCurCtx, proposals[i])
+			require.NoError(t, err)
+		}
+
+		for i := 0; i < len(votes); i++ {
+			err = voteTable.Create(cacheCurCtx, votes[i])
+			require.NoError(t, err)
+		}
+
+		// _, broken, _ := proposalTallyInvariant(cacheCurCtx, proposalTable, groupAccountByGroupIndex, groupMemberByGroupIndex, voteByProposalIndex, proposalByGroupAccountIndex, groupMemberTable, groupAccountTable, groupTable, voteByVoterIndex)
+		_, broken, _ := proposalTallyInvariant(cacheCurCtx, groupAccountByGroupIndex, groupMemberByGroupIndex, proposalByGroupAccountIndex, groupTable)
 		require.Equal(t, spec.expErr, broken)
 
 	}
