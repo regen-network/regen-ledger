6f1e9d455ea4e0273e3702a54d5af9f36a1f7314
MD Aleem <72057206+aleem1314@users.noreply.github.com>
2021-05-28 17:50:51 +0530

    revert: power reduction (#365)
    * revert DefaultPowerReduction
    
    * update comment
    
    Co-authored-by: Anil Kumar Kammari <anil@vitwit.com>

--- app/app.go
+++ app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"encoding/json"
 	"io"
+	"math/big"
 	"net/http"
 	"os"
 
@@ -139,7 +140,11 @@ var (
 	}
 )
 
-func init() {}
+func init() {
+	// this changes the power reduction from 10e6 to 10e2, which will give
+	// every validator 10,000 times more voting power than they currently have
+	sdk.DefaultPowerReduction = sdk.NewIntFromBigInt(new(big.Int).Exp(big.NewInt(10), big.NewInt(2), nil))
+}
 
 // Extended ABCI application
 type RegenApp struct {
