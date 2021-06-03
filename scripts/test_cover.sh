1ac55093b76c81034ae8b0500d39490dfa396481
aleem1314 <aleem@vitwit.com>
2021-06-03 17:00:06 +0530

    WIP test-cover

--- scripts/test_cover.sh
+++ scripts/test_cover.sh
@@ -1,14 +1,19 @@
-#!/usr/bin/env bash
-set -e
-
-PKGS=$(go list ./...)
+#!/bin/sh
 
 set -e
+
+SUBMODULES=$(find . -type f -name 'go.mod' -print0 | xargs -0 -n1 dirname | sort)
+
 echo "mode: atomic" > coverage.txt
-for pkg in ${PKGS[@]}; do
-    go test -v -timeout 30m -race -coverprofile=profile.out -covermode=atomic -tags='ledger test_ledger_mock' "$pkg"
-    if [ -f profile.out ]; then
-        tail -n +2 profile.out >> coverage.txt;
-        rm profile.out
-    fi
+
+for m in ${SUBMODULES[@]}; do
+    cd $(pwd)/$m
+    PKGS=$(go list ./...)
+    for pkg in ${PKGS[@]}; do
+        go test -v -timeout 30m -race -coverprofile=profile.out -covermode=atomic -tags='ledger test_ledger_mock' "$pkg"
+        if [ -f profile.out ]; then
+            tail -n +2 profile.out >> coverage.txt;
+            rm profile.out
+        fi
+    done
 done
