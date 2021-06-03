7d540db7961b3a0f59314d8f6ba077b7c361a560
likhita-809 <likhita@vitwit.com>
2021-06-03 18:34:27 +0530

    Revert "delete eco-credit balance when it goes to zero"
    This reverts commit 116a5942ab9b319467f4f9901cc7cbceb5009f43.

--- x/ecocredit/server/msg_server.go
+++ x/ecocredit/server/msg_server.go
@@ -3,15 +3,16 @@ package server
 import (
 	"fmt"
 
+	"github.com/regen-network/regen-ledger/types"
+
 	"github.com/cockroachdb/apd/v2"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/regen-network/regen-ledger/orm"
-	"github.com/regen-network/regen-ledger/types"
 	"github.com/regen-network/regen-ledger/types/math"
-	"github.com/regen-network/regen-ledger/x/ecocredit"
 	"github.com/regen-network/regen-ledger/x/ecocredit/util"
+	"github.com/regen-network/regen-ledger/x/ecocredit"
 )
 
 func (s serverImpl) CreateClass(ctx types.Context, req *ecocredit.MsgCreateClassRequest) (*ecocredit.MsgCreateClassResponse, error) {
