82dfe8dc1e0bc3912d2cc01407c8c4fafaefdf3e
aleem1314 <aleem@vitwit.com>
2021-06-03 17:12:27 +0530

    fix errors

--- scripts/test_cover.sh
+++ scripts/test_cover.sh
@@ -3,11 +3,11 @@
 set -e
 
 SUBMODULES=$(find . -type f -name 'go.mod' -print0 | xargs -0 -n1 dirname | sort)
-
+PWD=$(pwd)
 echo "mode: atomic" > coverage.txt
 
 for m in ${SUBMODULES[@]}; do
-    cd $(pwd)/$m
+    cd $PWD/$m
     PKGS=$(go list ./...)
     for pkg in ${PKGS[@]}; do
         go test -v -timeout 30m -race -coverprofile=profile.out -covermode=atomic -tags='ledger test_ledger_mock' "$pkg"
