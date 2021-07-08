a7e503c234a4e75274f90e0f8e7b3d92a59af4a3
Marie Gauthier <marie.gauthier63@gmail.com>
2021-07-08 10:38:47 +0200

    Update types/module/server/router.go
    Co-authored-by: Tyler <48813565+technicallyty@users.noreply.github.com>

--- types/module/server/router.go
+++ types/module/server/router.go
@@ -212,7 +212,7 @@ func (rtr *router) testQueryFactory() InvokerFactory {
 }
 
 func TypeURL(req proto.Message) string {
-	return fmt.Sprintf("/%s", proto.MessageName(req))
+	return "/%s" + proto.MessageName(req)
 }
 
 func noopInterceptor(_ context.Context, _ interface{}, _ *grpc.UnaryServerInfo, _ grpc.UnaryHandler) (interface{}, error) {
