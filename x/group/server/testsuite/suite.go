7480f69b14e5287f7208b78a8b1c586f8af911a7
technicallyty <48813565+tytech3@users.noreply.github.com>
2021-07-21 11:07:46 -0700

    fix: set allowlist disabled so group tests pass

--- x/group/server/testsuite/suite.go
+++ x/group/server/testsuite/suite.go
@@ -82,6 +82,7 @@ func (s *IntegrationTestSuite) SetupSuite() {
 	ecocreditParams := ecocredit.DefaultParams()
 	ecocreditParams.CreditClassFee = sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(0))) // overwriting the fee to 0stake
 	s.paramSpace.SetParamSet(s.sdkCtx, &ecocreditParams)
+	s.paramSpace.Set(s.sdkCtx, ecocredit.KeyAllowlistEnabled, false)
 
 	s.genesisCtx = types.Context{Context: sdkCtx}
 	s.Require().NoError(s.bankKeeper.MintCoins(s.sdkCtx, minttypes.ModuleName, sdk.NewCoins(sdk.NewInt64Coin("test", 400000000))))
