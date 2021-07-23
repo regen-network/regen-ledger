f4592e81cc069296c85e744dee18523b6eda9299
Ru Horlick <ruperthorlick@gmail.com>
2021-07-23 15:42:54 +0100

    Enable group integration tests

--- app/testsuite/modules_test.go
+++ app/testsuite/modules_test.go
@@ -8,12 +8,17 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	ecocredit "github.com/regen-network/regen-ledger/x/ecocredit/client/testsuite"
-	// group "github.com/regen-network/regen-ledger/x/group/client/testsuite"
+	group "github.com/regen-network/regen-ledger/x/group/client/testsuite"
 )
 
-func TestModules(t *testing.T) {
+func TestEcocreditIntegration(t *testing.T) {
 	cfg := DefaultConfig()
 
 	suite.Run(t, ecocredit.NewIntegrationTestSuite(cfg))
-	// suite.Run(t, group.NewIntegrationTestSuite(cfg))
+}
+
+func TestGroupIntegration(t *testing.T) {
+	cfg := DefaultConfig()
+
+	suite.Run(t, group.NewIntegrationTestSuite(cfg))
 }
