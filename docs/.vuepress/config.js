c36091a8737b7539547363c331cacdf334407355
ryanchrypto <12519942+ryanchrypto@users.noreply.github.com>
2021-07-29 15:09:04 -0700

    docs: add google analytics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -82,5 +82,9 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    '@vuepress/google-analytics',
+    {
+      'ga': 'UA-28106310-7'
+    }
   ]
 }
