cf9efcfab8e206057166bd9ab3f292fa1e5fcc90
technicallyty <48813565+tytech3@users.noreply.github.com>
2021-08-04 13:40:56 -0700

    fix method sig

--- x/ecocredit/params_test.go
+++ x/ecocredit/params_test.go
@@ -58,7 +58,7 @@ func Test_validateAllowlistCreditCreators(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := validateAllowlistCreditCreators(tt.args); (err != nil) != tt.wantErr {
+			if err := validateAllowlistCreditDesigners(tt.args); (err != nil) != tt.wantErr {
 				t.Errorf("validateAllowlistCreditCreators() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
