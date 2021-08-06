f767f287eab213c24ebf5122cde1847a20eb4c42
technicallyty <48813565+tytech3@users.noreply.github.com>
2021-08-06 07:24:37 -0700

    cleanup tests

--- x/ecocredit/server/testsuite/suite.go
+++ x/ecocredit/server/testsuite/suite.go
@@ -76,7 +76,6 @@ func (s *IntegrationTestSuite) TestScenario() {
 	addr4 := s.signers[6].String()
 	addr5 := s.signers[7].String()
 
-	s.paramSpace.Set(s.sdkCtx, ecocredit.KeyAllowedClassDesigners, []string{designer.String()})
 	// create class with insufficient funds and it should fail
 	createClsRes, err := s.msgClient.CreateClass(s.ctx, &ecocredit.MsgCreateClass{
 		Designer: designer.String(),
@@ -623,9 +622,6 @@ func (s *IntegrationTestSuite) TestScenario() {
 	}
 
 	/****   TEST ALLOWLIST CREDIT DESIGNERS   ****/
-
-	// reset paramspace to empty
-	s.paramSpace.Set(s.sdkCtx, ecocredit.KeyAllowedClassDesigners, []string{})
 	allowlistCases := []struct {
 		name             string
 		designerAcc      sdk.AccAddress
