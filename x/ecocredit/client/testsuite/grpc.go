c68e4dfdd440fc358c8bfe06524d1f79b40f2ef4
aleem1314 <aleem@vitwit.com>
2021-09-17 16:32:54 +0530

    WIP adding tests

--- x/ecocredit/client/testsuite/grpc.go
+++ x/ecocredit/client/testsuite/grpc.go
@@ -147,3 +147,70 @@ func (s *IntegrationTestSuite) TestGetBatches() {
 		})
 	}
 }
+
+func (s *IntegrationTestSuite) TestGetBatch() {
+	val := s.network.Validators[0]
+
+	testCases := []struct {
+		name       string
+		url        string
+		expErr     bool
+		errMsg     string
+		batchDenom string
+	}{
+		{
+			"batch not found",
+			fmt.Sprintf("%s/regen/ecocredit/v1alpha1/batches/%s", val.APIAddress, "C999"),
+			true,
+			"not found",
+			"",
+		},
+		{
+			"valid test",
+			fmt.Sprintf("%s/regen/ecocredit/v1alpha1/batches/%s", val.APIAddress, "C01"),
+			false,
+			"",
+			"C01",
+		},
+	}
+
+	require := s.Require()
+	for _, tc := range testCases {
+		tc := tc
+		s.Run(tc.name, func() {
+			resp, err := rest.GetRequest(tc.url)
+			require.NoError(err)
+
+			var batch ecocredit.QueryBatchInfoResponse
+			err = val.ClientCtx.Codec.UnmarshalJSON(resp, &batch)
+
+			if tc.expErr {
+				require.Error(err)
+			} else {
+				require.NoError(err)
+				require.NotNil(batch.Info)
+				require.Contains(batch.Info.BatchDenom, tc.batchDenom)
+			}
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestCreditTypes() {
+	require := s.Require()
+	val := s.network.Validators[0]
+
+	url := fmt.Sprintf("%s/regen/ecocredit/v1alpha1/credit-types", val.APIAddress)
+	resp, err := rest.GetRequest(url)
+	require.NoError(err)
+
+	var creditTypes ecocredit.QueryCreditTypesResponse
+	err = val.ClientCtx.Codec.UnmarshalJSON(resp, &creditTypes)
+
+	require.NoError(err)
+	require.Equal(creditTypes.String(), `{[name:"carbon" abbreviation:"C" unit:"metric ton CO2 equivalent" precision:6 ]}`)
+}
+
+func (s *IntegrationTestSuite) TestGetBalance() {
+	// val := s.network.Validators[0]
+
+}
