a1e5b7b405cafc7dd3d07c72738b38a82d0cb91b
aleem1314 <aleem@vitwit.com>
2021-09-21 20:25:45 +0530

    feat: add tests

--- x/ecocredit/client/tx.go
+++ x/ecocredit/client/tx.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"strconv"
 	"strings"
 
 	sdkclient "github.com/cosmos/cosmos-sdk/client"
@@ -371,62 +370,4 @@ Parameters:
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
 		},
 	})
-}
-
-func checkDuplicateKey(d *json.Decoder, path []string) error {
-	// Get next token from JSON
-	t, err := d.Token()
-	if err != nil {
-		return err
-	}
-
-	delim, ok := t.(json.Delim)
-
-	// There's nothing to do for simple values (strings, numbers, bool, nil)
-	if !ok {
-		return nil
-	}
-
-	switch delim {
-	case '{':
-		keys := make(map[string]bool)
-		for d.More() {
-			// Get field key
-			t, err := d.Token()
-			if err != nil {
-				return err
-			}
-			key := t.(string)
-
-			// Check for duplicates
-			if keys[key] {
-				return fmt.Errorf("duplicate key %s", key)
-			}
-			keys[key] = true
-
-			// Check value
-			if err := checkDuplicateKey(d, append(path, key)); err != nil {
-				return err
-			}
-		}
-		// Consume trailing }
-		if _, err := d.Token(); err != nil {
-			return err
-		}
-
-	case '[':
-		i := 0
-		for d.More() {
-			if err := checkDuplicateKey(d, append(path, strconv.Itoa(i))); err != nil {
-				return err
-			}
-			i++
-		}
-		// Consume trailing ]
-		if _, err := d.Token(); err != nil {
-			return err
-		}
-
-	}
-	return nil
 }
