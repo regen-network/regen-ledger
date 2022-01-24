d4fadc9ab3b851cc7bdca180f984a683bad68782
Aaron Craelius <aaronc@users.noreply.github.com>
2022-01-24 14:50:37 -0500

    docs

--- types/ormstore/db.go
+++ types/ormstore/db.go
@@ -9,6 +9,9 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// NewStoreKeyDB creates an ormdb.ModuleDB from an ormdb.ModuleDB and a StoreKey.
+// It is an interim solution for using the ORM in existing Cosmos SDK modules
+// before fuller integration has been done.
 func NewStoreKeyDB(desc ormdb.ModuleSchema, key storetypes.StoreKey, options ormdb.ModuleDBOptions) (ormdb.ModuleDB, error) {
 	getBackend := func(ctx context.Context) (ormtable.Backend, error) {
 		sdkCtx := sdk.UnwrapSDKContext(ctx)
