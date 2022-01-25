0e33f7307f633b94a59bf0e9d18c0eec1e2d40f9
Aaron Craelius <aaron@regen.network>
2022-01-25 10:26:13 -0500

    feat(types): add interim orm support (#698)

--- go.mod
+++ go.mod
@@ -16,7 +16,7 @@ require (
 	github.com/spf13/cobra v1.2.1
 	github.com/stretchr/testify v1.7.0
 	github.com/tendermint/tendermint v0.34.14
-	github.com/tendermint/tm-db v0.6.4
+	github.com/tendermint/tm-db v0.6.6
 )
 
 require (
@@ -110,14 +110,14 @@ require (
 	github.com/tendermint/crypto v0.0.0-20191022145703-50d29ede1e15 // indirect
 	github.com/tendermint/go-amino v0.16.0 // indirect
 	github.com/zondax/hid v0.9.0 // indirect
-	go.etcd.io/bbolt v1.3.5 // indirect
+	go.etcd.io/bbolt v1.3.6 // indirect
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20210903071746-97244b99971b // indirect
 	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220118154757-00ab72f36ad5 // indirect
-	google.golang.org/grpc v1.40.0 // indirect
+	google.golang.org/grpc v1.43.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/ini.v1 v1.63.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ types/ormstore/db_test.go
@@ -0,0 +1,64 @@
+package ormstore
+
+import (
+	"testing"
+
+	ecocreditv1beta1 "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1beta1"
+
+	"github.com/cosmos/cosmos-sdk/orm/model/ormdb"
+	"github.com/cosmos/cosmos-sdk/store"
+	"github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	storetypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/libs/log"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	dbm "github.com/tendermint/tm-db"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+func sdkContextForStoreKey(key *types.KVStoreKey) sdk.Context {
+	db := dbm.NewMemDB()
+	cms := store.NewCommitMultiStore(db)
+	cms.MountStoreWithDB(key, storetypes.StoreTypeIAVL, db)
+	err := cms.LoadLatestVersion()
+	if err != nil {
+		panic(err)
+	}
+	return sdk.NewContext(cms, tmproto.Header{}, false, log.NewNopLogger())
+}
+
+func TestStoreKeyDB(t *testing.T) {
+	storeKey := types.NewKVStoreKey("test")
+	db, err := NewStoreKeyDB(
+		ormdb.ModuleSchema{FileDescriptors: map[uint32]protoreflect.FileDescriptor{
+			1: ecocreditv1beta1.File_regen_ecocredit_v1beta1_state_proto,
+		}},
+		storeKey,
+		ormdb.ModuleDBOptions{},
+	)
+	require.NoError(t, err)
+	sdkCtx := sdkContextForStoreKey(storeKey)
+	ctx := sdk.WrapSDKContext(sdkCtx)
+
+	creditTypeTable := db.GetTable(&ecocreditv1beta1.CreditType{})
+	require.NotNil(t, creditTypeTable)
+
+	require.NoError(t, creditTypeTable.Save(ctx, &ecocreditv1beta1.CreditType{
+		Name:         "carbon",
+		Abbreviation: "C",
+		Unit:         "tons of co2e",
+		Precision:    6,
+	}))
+
+	creditType := &ecocreditv1beta1.CreditType{
+		Abbreviation: "C",
+	}
+	found, err := creditTypeTable.Get(ctx, creditType)
+	require.NoError(t, err)
+	require.True(t, found)
+	require.Equal(t, "C", creditType.Abbreviation)
+	require.Equal(t, "carbon", creditType.Name)
+	require.Equal(t, "tons of co2e", creditType.Unit)
+	require.Equal(t, uint32(6), creditType.Precision)
+}
