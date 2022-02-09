37ba1e38589fd9518434f616998d58fd5baef172
Robert Zaremba <robert@zaremba.ch>
2022-02-10 00:36:45 +0100

    add tests

---
+++ x/ecocredit/basket/tx_msg_test.go
@@ -0,0 +1,59 @@
+package basket
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/testutil/testdata"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/stretchr/testify/require"
+	"github.com/thanhpk/randstr"
+)
+
+func errorMatches(t *testing.T, err error, expect string) {
+	if expect == "" {
+		require.NoError(t, err)
+	} else {
+		require.Error(t, err)
+		require.Contains(t, err.Error(), expect)
+	}
+}
+
+func TestMsgCreateValidateBasic(t *testing.T) {
+	_, _, addr1 := testdata.KeyTestPubAddr()
+	a := addr1.String()
+
+	tcs := []struct {
+		msg MsgCreate
+		err string
+	}{
+		{MsgCreate{Curator: "wrong"}, "malformed curator address"},
+		{MsgCreate{Curator: a, Name: ""}, "name must not"},
+		{MsgCreate{Curator: a, Name: randstr.String(101)}, "name must not"},
+		{MsgCreate{Curator: a, Name: randstr.String(60), Exponent: 33}, "exponent must"},
+
+		{MsgCreate{Curator: a, Name: randstr.String(60), Exponent: 0}, ""},
+		{MsgCreate{Curator: a, Name: randstr.String(1), Exponent: 32}, ""},
+	}
+
+	for i, tc := range tcs {
+		t.Run(fmt.Sprint("test-", i), func(t *testing.T) {
+			err := tc.msg.ValidateBasic()
+			errorMatches(t, err, tc.err)
+		})
+	}
+}
+
+func TestMsgCreateGetSigners(t *testing.T) {
+	_, _, addr1 := testdata.KeyTestPubAddr()
+	m := MsgCreate{Curator: addr1.String(), Name: "name", Exponent: 2}
+	require.Equal(t, []sdk.AccAddress{addr1}, m.GetSigners())
+}
+
+func TestMsgCreateSignBytes(t *testing.T) {
+	_, _, addr1 := testdata.KeyTestPubAddr()
+	m := MsgCreate{Curator: addr1.String(), Name: "name", Exponent: 2}
+	bz := m.GetSignBytes()
+	require.NotEmpty(t, bz)
+}
--- x/ecocredit/go.mod
+++ x/ecocredit/go.mod
@@ -113,6 +113,7 @@ require (
 	github.com/tendermint/btcd v0.1.1 // indirect
 	github.com/tendermint/crypto v0.0.0-20191022145703-50d29ede1e15 // indirect
 	github.com/tendermint/go-amino v0.16.0 // indirect
+	github.com/thanhpk/randstr v1.0.4 // indirect
 	github.com/zondax/hid v0.9.0 // indirect
 	go.etcd.io/bbolt v1.3.6 // indirect
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
