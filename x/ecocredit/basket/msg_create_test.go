47888746bb0e41c4e265556893e2a6f51587fa01
Robert Zaremba <robert@zaremba.ch>
2022-02-10 23:34:51 +0100

    adding tests

--- x/ecocredit/basket/msg_create_test.go
+++ x/ecocredit/basket/msg_create_test.go
@@ -1,8 +1,8 @@
 package basket
 
 import (
-	"fmt"
 	"testing"
+	"time"
 
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -26,40 +26,98 @@ func TestMsgCreateValidateBasic(t *testing.T) {
 	name := randstr.String(nameMaxLen)
 	dName := randstr.String((displayNameMaxLen + displayNameMinLen) / 2)
 	creditName := randstr.String(10)
-	start := gogotypes.TimestampNow()
+	start := &DateCriteria{&DateCriteria_MinStartDate{gogotypes.TimestampNow()}}
+
 	classes := []string{"eco_class1"}
 
 	tcs := []struct {
+		id  string
 		msg MsgCreate
 		err string
 	}{
-		{MsgCreate{Curator: "wrong"}, "malformed curator address"},
-		{MsgCreate{Curator: a, Name: ""}, "name must not be empty"},
-		{MsgCreate{Curator: a, Name: randstr.String(nameMaxLen + 1)}, "name must not be empty and must not be longer than"},
-		{MsgCreate{Curator: a, Name: name, DisplayName: ""}, "display_name must be between"},
-		{MsgCreate{Curator: a, Name: name, DisplayName: randstr.String(displayNameMaxLen + 1)}, "display_name must be between"},
-		{MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax + 1}, "exponent must not be bigger than"},
-		{MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax},
+		{"curator-1",
+			MsgCreate{Curator: "wrong"},
+			"malformed curator address"},
+		{"name-1",
+			MsgCreate{Curator: a, Name: ""}, "name must not be empty"},
+		{"name-2",
+			MsgCreate{Curator: a, Name: randstr.String(nameMaxLen + 1)},
+			"name must not be empty and must not be longer than"},
+		{"name-3",
+			MsgCreate{Curator: a, Name: name, DisplayName: ""},
+			"display_name must be between"},
+		{"name-4",
+			MsgCreate{Curator: a, Name: name, DisplayName: randstr.String(displayNameMaxLen + 1)},
+			"display_name must be between"},
+		{"exponent-1",
+			MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax + 1},
+			"exponent must not be bigger than"},
+		{"credity_type-1",
+			MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax},
 			"credit_type_name must be defined"},
-		{MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax, CreditTypeName: randstr.String(creditTNameMaxLen + 1)},
+		{"credity_type-2",
+			MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax, CreditTypeName: randstr.String(creditNameMaxLen + 1)},
 			"credit_type_name must not be longer"},
-		{MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax, CreditTypeName: creditName, MinStartDate: start},
+		{"date_criteria-1",
+			MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax, CreditTypeName: creditName, DateCriteria: &DateCriteria{}},
+			"unsupported date_criteria value"},
+		{"allowed_classes-1",
+			MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax, CreditTypeName: creditName, DateCriteria: start},
 			"allowed_classes is required"},
-		{MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax, CreditTypeName: creditName, MinStartDate: start, AllowedClasses: []string{"class1", ""}},
+		{"allowed_classes-2", MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax, CreditTypeName: creditName, DateCriteria: start, AllowedClasses: []string{"class1", ""}},
 			"allowed_classes[1] must be defined"},
-		{MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax, CreditTypeName: creditName, MinStartDate: start, AllowedClasses: classes, Fee: sdk.Coins{sdk.Coin{Denom: "1a"}}},
+		{"fee-1", MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax, CreditTypeName: creditName, DateCriteria: start, AllowedClasses: classes, Fee: sdk.Coins{sdk.Coin{Denom: "1a"}}},
 			"invalid denom"},
-		{MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax, CreditTypeName: creditName, MinStartDate: start, AllowedClasses: classes, Fee: sdk.Coins{sdk.Coin{"aa", sdk.NewInt(-1)}}},
+		{"fee-2", MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: exponentMax, CreditTypeName: creditName, DateCriteria: start, AllowedClasses: classes, Fee: sdk.Coins{sdk.Coin{"aa", sdk.NewInt(-1)}}},
 			"invalid denom"},
 
-		{MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: 0, CreditTypeName: creditName, MinStartDate: start, AllowedClasses: classes}, ""},
+		{"good-1",
+			MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: 0, CreditTypeName: creditName, DateCriteria: start, AllowedClasses: classes}, ""},
 		// nil min_start_time is also OK
-		{MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: 0, CreditTypeName: creditName, MinStartDate: nil, AllowedClasses: classes}, ""},
+		{"good-date-criteria-not-required",
+			MsgCreate{Curator: a, Name: name, DisplayName: dName, Exponent: 0, CreditTypeName: creditName, DateCriteria: nil, AllowedClasses: classes}, ""},
 	}
 
-	for i, tc := range tcs {
-		t.Run(fmt.Sprint("test-", i), func(t *testing.T) {
+	for _, tc := range tcs {
+		t.Run(tc.id, func(t *testing.T) {
 			err := tc.msg.ValidateBasic()
+			errorMatches(t, err, tc.err)
+		})
+	}
+}
+
+func TestMsgCreateValidateDateCriteria(t *testing.T) {
+	tcs := []struct {
+		id  string
+		d   DateCriteria
+		err string
+	}{
+		{"nil-min_start_date",
+			DateCriteria{&DateCriteria_MinStartDate{nil}},
+			"unsupported date_criteria value"},
+		{"bad-min_start_date",
+			DateCriteria{&DateCriteria_MinStartDate{&gogotypes.Timestamp{
+				Seconds: time.Date(1400, 1, 1, 0, 0, 0, 0, time.UTC).Unix()}}},
+			"date_criteria.min_start_date must be after"},
+		{"nil-start_date_window",
+			DateCriteria{&DateCriteria_StartDateWindow{}},
+			"unsupported date_criteria value"},
+		{"nil-start_date_window",
+			DateCriteria{&DateCriteria_StartDateWindow{&gogotypes.Duration{
+				Seconds: 3600}}},
+			"date_criteria.start_date_window must be at least"},
+
+		{"good-min_start_date",
+			DateCriteria{&DateCriteria_MinStartDate{gogotypes.TimestampNow()}},
+			""},
+		{"good-start_date_window",
+			DateCriteria{&DateCriteria_StartDateWindow{&gogotypes.Duration{
+				Seconds: 3600 * 24 * 2}}},
+			""},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.id, func(t *testing.T) {
+			err := validateDateCriteria(&tc.d)
 			errorMatches(t, err, tc.err)
 		})
 	}
