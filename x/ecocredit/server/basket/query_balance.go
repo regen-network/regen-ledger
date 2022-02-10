b984101ddb41b36cc419ccd75c9a9522bc344133
Aaron Craelius <aaronc@users.noreply.github.com>
2022-02-10 15:37:11 -0500

    add nil check

--- x/ecocredit/server/basket/query_balance.go
+++ x/ecocredit/server/basket/query_balance.go
@@ -3,10 +3,17 @@ package basket
 import (
 	"context"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	baskettypes "github.com/regen-network/regen-ledger/x/ecocredit/basket"
 )
 
 func (k Keeper) BasketBalance(ctx context.Context, request *baskettypes.QueryBasketBalanceRequest) (*baskettypes.QueryBasketBalanceResponse, error) {
+	if request == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "empty request")
+	}
+
 	basket, err := k.stateStore.BasketStore().GetByBasketDenom(ctx, request.BasketDenom)
 	if err != nil {
 		return nil, err
