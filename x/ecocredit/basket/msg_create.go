747d81973488cb694c84077d9ad2725ac90ea3d1
Robert Zaremba <robert@zaremba.ch>
2022-02-10 11:52:03 +0100

    more validation

--- x/ecocredit/basket/msg_create.go
+++ x/ecocredit/basket/msg_create.go
@@ -13,18 +13,47 @@ var (
 )
 
 const nameMaxLen = 32
+const minDisplayName = 3
+const maxDisplayName = 32
 
 // ValidateBasic does a stateless sanity check on the provided data.
 func (m MsgCreate) ValidateBasic() error {
 	if _, err := sdk.AccAddressFromBech32(m.Curator); err != nil {
 		return sdkerrors.ErrInvalidAddress.Wrap("malformed curator address " + err.Error())
 	}
+	// TODO: add proper validation once we will have proper requirements.
+	// https://github.com/regen-network/regen-ledger/issues/732
 	if m.Name == "" || len(m.Name) > nameMaxLen {
-		return sdkerrors.ErrInvalidRequest.Wrap("name must not be empty and must not be longer than 100 characters")
+		return sdkerrors.ErrInvalidRequest.Wrap("name must not be empty and must not be longer than 32 characters")
+	}
+	if len(m.DisplayName) < 3 || len(m.DisplayName) > maxDisplayName {
+		return sdkerrors.ErrInvalidRequest.Wrap("display_name length must be between 3 and 32 characters")
 	}
 	if m.Exponent > 32 {
 		return sdkerrors.ErrInvalidRequest.Wrap("exponent must not be bigger than 32")
 	}
+	if m.CreditTypeName == "" {
+		return sdkerrors.ErrInvalidRequest.Wrap("credit_type_name is required")
+	}
+
+	if m.MinStartDate == nil {
+		return sdkerrors.ErrInvalidRequest.Wrap("min_start_date is required")
+	}
+
+	if len(m.AllowedClasses) == 0 {
+		return sdkerrors.ErrInvalidRequest.Wrap("allowed_classes is required")
+	}
+	for i := range m.AllowedClasses {
+		if m.AllowedClasses[i] == "" {
+			return sdkerrors.ErrInvalidRequest.Wrapf("allowed_classes[%d] must be defined", i)
+		}
+	}
+	return nil
+}
+
+// Validate additional validation with access to the state data.
+func (m MsgCreate) Validate(minFee sdk.Coin) error {
+	// TODO: update function to do stateful validation
 	return nil
 }
 
