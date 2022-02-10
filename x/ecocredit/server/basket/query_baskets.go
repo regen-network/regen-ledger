9332f40a5ec4984aca6ee32bd5fdfe1dbb183ec6
Aaron Craelius <aaron@regen.network>
2022-02-10 16:06:50 -0500

    feat(x/ecocredit): add BasketBalance query (#751)
    * feat(x/ecocredit): add BasketBalance query
    
    * go mod tidy
    
    * add nil check

---
+++ x/ecocredit/server/basket/query_baskets.go
@@ -0,0 +1,12 @@
+package basket
+
+import (
+	"context"
+
+	baskettypes "github.com/regen-network/regen-ledger/x/ecocredit/basket"
+)
+
+func (k Keeper) Baskets(ctx context.Context, request *baskettypes.QueryBasketsRequest) (*baskettypes.QueryBasketsResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
