c33dbcadd456b042974e5cd64b24d71ed4bc2476
Aaron Craelius <aaronc@users.noreply.github.com>
2022-02-11 15:20:46 -0500

    WIP on tests

---
+++ x/ecocredit/module/module_test.go
@@ -0,0 +1,41 @@
+package module
+
+import (
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHybridORMLegacyGenesis(t *testing.T) {
+	m := Module{}
+	cdc := codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
+	bz := m.DefaultGenesis(cdc)
+	require.NotNil(t, bz)
+	require.NoError(t, m.ValidateGenesis(cdc, nil, bz))
+
+	require.NoError(t, m.ValidateGenesis(cdc, nil, []byte(`
+{
+  "params":{
+    "allowlist_enabled":true
+  }
+}`)))
+
+	require.NoError(t, m.ValidateGenesis(cdc, nil, []byte(`
+{
+  "regen.ecocredit.basket.v1.Basket":[{
+    "basket_denom":"foo"
+  }]
+}`)))
+
+	require.NoError(t, m.ValidateGenesis(cdc, nil, []byte(`
+{
+  "params":{
+    "allowlist_enabled":true
+  },
+  "regen.ecocredit.basket.v1.Basket":[{
+    "basket_denom":"foo"
+  }]
+}`)))
+}
