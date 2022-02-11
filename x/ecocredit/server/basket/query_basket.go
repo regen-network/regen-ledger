7b2ab69f1f8a153e8922d93eba8e65ee660bc9e3
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2022-02-11 09:06:06 -0800

    pulsar to proto

--- x/ecocredit/server/basket/query_basket.go
+++ x/ecocredit/server/basket/query_basket.go
@@ -2,15 +2,27 @@ package basket
 
 import (
 	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	baskettypes "github.com/regen-network/regen-ledger/x/ecocredit/basket"
 )
 
 func (k Keeper) Basket(ctx context.Context, request *baskettypes.QueryBasketRequest) (*baskettypes.QueryBasketResponse, error) {
+	if request == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "empty request")
+	}
+
 	basket, err := k.stateStore.BasketStore().GetByBasketDenom(ctx, request.BasketDenom)
 	if err != nil {
 		return nil, err
 	}
 
-	return &baskettypes.QueryBasketResponse{Basket: basket}, nil
+	basketGogo := &baskettypes.Basket{}
+	err = PulsarToGogoSlow(basket, basketGogo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &baskettypes.QueryBasketResponse{Basket: basketGogo}, nil
 }
