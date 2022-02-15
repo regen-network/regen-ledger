a838093de7f0f96588f05fca27210f26f9a36fc2
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-02-15 14:35:15 -0800

    feat: add upgrade handler for basket fee

--- app/stable_appconfig.go
+++ app/stable_appconfig.go
@@ -87,6 +87,17 @@ func (app *RegenApp) registerUpgradeHandlers() {
 		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 	})
 
+	upgradeV22xName := "v2.2-upgrade"
+	app.UpgradeKeeper.SetUpgradeHandler(upgradeV22xName, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		// get the params
+		ecoParams := app.ParamsKeeper.Subspace(ecocredittypes.ModuleName)
+
+		// set basket creation fee to 1,0000 REGEN
+		ecoParams.Set(ctx, ecocredittypes.KeyBasketCreationFee, sdk.NewCoins(sdk.NewInt64Coin("uregen", 1e9)))
+
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
+
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
 	if err != nil {
 		panic(err)
