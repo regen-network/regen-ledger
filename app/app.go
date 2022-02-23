3ddc81ea91f1c158fbcbf71ee40bd52852a79642
Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
2022-02-23 13:36:12 -0800

    chore: v3 version bump (#799)

--- app/app.go
+++ app/app.go
@@ -97,7 +97,7 @@ import (
 	ecocreditmodule "github.com/regen-network/regen-ledger/x/ecocredit/module"
 
 	// unnamed import of statik for swagger UI support
-	_ "github.com/regen-network/regen-ledger/v2/client/docs/statik"
+	_ "github.com/regen-network/regen-ledger/v3/client/docs/statik"
 )
 
 const (
