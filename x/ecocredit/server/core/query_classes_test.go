68f5d40cfcd2d0eed145377ddbeba25c15fffe91
Tyler <48813565+technicallyty@users.noreply.github.com>
2022-03-04 14:56:40 -0800

    Apply suggestions from code review
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>

--- x/ecocredit/server/core/query_classes_test.go
+++ x/ecocredit/server/core/query_classes_test.go
@@ -31,6 +31,7 @@ func TestQuery_Classes(t *testing.T) {
 	assert.NilError(t, err)
 	assert.Equal(t, 2, len(res.Classes))
 	assert.Equal(t, "C01", res.Classes[0].Name)
+	assert.Equal(t, "C02", res.Classes[1].Name)
 
 	// request with pagination
 	res, err = s.k.Classes(s.ctx, &v1beta1.QueryClassesRequest{Pagination: &query.PageRequest{
@@ -41,4 +42,5 @@ func TestQuery_Classes(t *testing.T) {
 	assert.NilError(t, err)
 	assert.Equal(t, 1, len(res.Classes))
 	assert.Equal(t, uint64(2), res.Pagination.Total)
+	assert.Equal(t, "C02", res.Classes[0].Name)
 }
