5bdbd5c6a6611c45d709ca7db0d441f21b42b5a1
Tyler <48813565+technicallyty@users.noreply.github.com>
2022-03-07 10:52:01 -0800

    feat(x/ecocredit): `CreateClass` ORM (#816)
    * fix: generate all v1beta1 codes in their own packages, fix core servers
    
    * chore: reduce FormatClassID function
    
    * chore: reduce to single state store instance
    
    * chore: fix invariants
    
    * feat: add row id to events/responses
    
    * fix: formatclassid compatability
    
    * Update x/ecocredit/server/core/msg_server.go
    
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    
    * chore: address review + switch remaining string addr to bytes
    
    * fix: query byte addrs
    
    * chore: issue link
    
    * chore: invariants test use test memory backend
    
    * test: add GetClassIdFromBatchDenom test
    
    * fix: change assertClassIssuer to use has
    
    * chore: remove row_id from response/events
    
    * refactor: use class row id for class issuer table
    
    * feat: add AddIssuers and RemoveIssuers from update class issuers method
    
    * fix: credit types use ORM and remove marketplace code
    
    * chore: add comment to remove params
    
    * chore: remove non-core queries
    
    * wip: first few
    
    * feat: core impl keeper
    
    * feat: impl, test
    
    * feat(wip): unit tests
    
    * wip: tests
    
    * feat: update class unit tests
    
    * chore: add query utils
    
    * chore: cleanup queries
    
    * chore: go mod tidy
    
    * fix: dec_test should allow empty string
    
    * wip: more tests
    
    * feat: query unit tests
    
    * chore: setup
    
    * chore: proto-gen
    
    * chore: cleanup protos
    
    * chore: commit generated code files
    
    * chore: bump ORM
    
    * chore: make tidy
    
    * chore: tidy
    
    * fix: go mod replace
    
    * chore: go.sum
    
    * chore: bump
    
    * chore: revert to string for request
    
    * chore: make tidy
    
    * Update proto/regen/ecocredit/v1beta1/query.proto
    
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    
    * chore: proto-gen
    
    * feat: create class
    
    * chore: move general funcs to utils
    
    * Update x/ecocredit/server/core/create_class_test.go
    
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    
    * chore: godoc
    
    * Apply suggestions from code review
    
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    
    * chore: check sequence, remove test case
    
    * chore: pin rosetta version to pre-breaking change
    
    * testing
    
    * chore: tidY
    
    * try this
    
    * chore: remove replace directive
    
    Co-authored-by: technicallyty <48813565+tytech3@users.noreply.github.com>
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>

--- x/ecocredit/server/core/utils.go
+++ x/ecocredit/server/core/utils.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/regen-network/regen-ledger/x/ecocredit"
 )
 
 // assertClassIssuer makes sure that the issuer is part of issuers of given classID.
@@ -18,4 +19,17 @@ func (k Keeper) assertClassIssuer(goCtx context.Context, classID uint64, issuer 
 		return sdkerrors.ErrUnauthorized.Wrapf("%s is not an issuer for the class", issuer)
 	}
 	return nil
+}
+
+// getCreditType searches for a credit type that matches the given abbreviation within a credit type slice.
+func (k Keeper) getCreditType(ctAbbrev string, creditTypes []*ecocredit.CreditType) (ecocredit.CreditType, error) {
+	//creditTypeName = ecocredit.NormalizeCreditTypeName(creditTypeName)
+	for _, creditType := range creditTypes {
+		// credit type name's stored via params have enforcement on normalization, so we can be sure they will already
+		// be normalized here.
+		if creditType.Abbreviation == ctAbbrev {
+			return *creditType, nil
+		}
+	}
+	return ecocredit.CreditType{}, sdkerrors.ErrInvalidType.Wrapf("%s is not a valid credit type", ctAbbrev)
 }
