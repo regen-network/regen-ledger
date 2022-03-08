37c2b900754ca508340e9fb284ff48d895eda9fe
Tyler <48813565+technicallyty@users.noreply.github.com>
2022-03-08 09:19:05 -0800

    feat(x/ecocredit): query `Params` ORM (#846)
    * feat: query params
    
    * Update x/ecocredit/server/core/query_params_test.go
    
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    
    Co-authored-by: technicallyty <48813565+tytech3@users.noreply.github.com>
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>

---
+++ x/ecocredit/server/core/query_params.go
@@ -0,0 +1,34 @@
+package core
+
+import (
+	"context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/regen-network/regen-ledger/x/ecocredit"
+	"github.com/regen-network/regen-ledger/x/ecocredit/v1beta1"
+)
+
+// Params queries the ecocredit module parameters.
+// TODO: remove params https://github.com/regen-network/regen-ledger/issues/729
+// Currently this is an ugly hack that grabs v1alpha types and converts them into v1beta types.
+// will be gone with #729.
+func (k Keeper) Params(ctx context.Context, _ *v1beta1.QueryParamsRequest) (*v1beta1.QueryParamsResponse, error) {
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	var params ecocredit.Params
+	k.params.GetParamSet(sdkCtx, &params)
+	v1beta1types := make([]*v1beta1.CreditType, len(params.CreditTypes))
+	for i, typ := range params.CreditTypes {
+		v1beta1types[i] = &v1beta1.CreditType{
+			Abbreviation: typ.Abbreviation,
+			Name:         typ.Name,
+			Unit:         typ.Unit,
+			Precision:    typ.Precision,
+		}
+	}
+	v1beta1Params := v1beta1.Params{
+		CreditClassFee:       params.CreditClassFee,
+		AllowedClassCreators: params.AllowedClassCreators,
+		AllowlistEnabled:     params.AllowlistEnabled,
+		CreditTypes:          v1beta1types,
+	}
+	return &v1beta1.QueryParamsResponse{Params: &v1beta1Params}, nil
+}
