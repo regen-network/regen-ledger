eb32c7dcbf6a8ec73fe143eed9f517ba9b72db52
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-03-08 11:41:00 -0800

    chore: v1beta1 -> v1

--- x/ecocredit/server/core/query_class_info.go
+++ x/ecocredit/server/core/query_class_info.go
@@ -2,18 +2,18 @@ package core
 
 import (
 	"context"
-	"github.com/regen-network/regen-ledger/x/ecocredit/v1beta1"
+	v1 "github.com/regen-network/regen-ledger/x/ecocredit/v1"
 )
 
 // ClassInfo queries for information on a credit class.
-func (k Keeper) ClassInfo(ctx context.Context, request *v1beta1.QueryClassInfoRequest) (*v1beta1.QueryClassInfoResponse, error) {
+func (k Keeper) ClassInfo(ctx context.Context, request *v1.QueryClassInfoRequest) (*v1.QueryClassInfoResponse, error) {
 	classInfo, err := k.stateStore.ClassInfoStore().GetByName(ctx, request.ClassId)
 	if err != nil {
 		return nil, err
 	}
-	var ci v1beta1.ClassInfo
+	var ci v1.ClassInfo
 	if err = PulsarToGogoSlow(classInfo, &ci); err != nil {
 		return nil, err
 	}
-	return &v1beta1.QueryClassInfoResponse{Info: &ci}, nil
+	return &v1.QueryClassInfoResponse{Info: &ci}, nil
 }
--- x/ecocredit/server/core/query_class_info_test.go
+++ x/ecocredit/server/core/query_class_info_test.go
@@ -2,8 +2,8 @@ package core
 
 import (
 	"github.com/cosmos/cosmos-sdk/orm/types/ormerrors"
-	ecocreditv1beta1 "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1beta1"
-	"github.com/regen-network/regen-ledger/x/ecocredit/v1beta1"
+	ecocreditv1 "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1"
+	v1 "github.com/regen-network/regen-ledger/x/ecocredit/v1"
 	"gotest.tools/v3/assert"
 	"testing"
 )
@@ -11,7 +11,7 @@ import (
 func TestQuery_ClassInfo(t *testing.T) {
 	t.Parallel()
 	s := setupBase(t)
-	err := s.stateStore.ClassInfoStore().Insert(s.ctx, &ecocreditv1beta1.ClassInfo{
+	err := s.stateStore.ClassInfoStore().Insert(s.ctx, &ecocreditv1.ClassInfo{
 		Name:       "C01",
 		Admin:      s.addr,
 		Metadata:   nil,
@@ -20,11 +20,11 @@ func TestQuery_ClassInfo(t *testing.T) {
 	assert.NilError(t, err)
 
 	// query an invalid class
-	_, err = s.k.ClassInfo(s.ctx, &v1beta1.QueryClassInfoRequest{ClassId: "C02"})
+	_, err = s.k.ClassInfo(s.ctx, &v1.QueryClassInfoRequest{ClassId: "C02"})
 	assert.ErrorContains(t, err, ormerrors.NotFound.Error())
 
 	// query a valid class
-	res, err := s.k.ClassInfo(s.ctx, &v1beta1.QueryClassInfoRequest{ClassId: "C01"})
+	res, err := s.k.ClassInfo(s.ctx, &v1.QueryClassInfoRequest{ClassId: "C01"})
 	assert.NilError(t, err)
 	assert.Equal(t, "C01", res.Info.Name)
 	assert.DeepEqual(t, s.addr.Bytes(), res.Info.Admin)
