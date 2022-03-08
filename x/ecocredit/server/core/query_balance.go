b9942e4867f0a7891f12e98163def195898ee9f3
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-03-08 09:55:44 -0800

    chore: conflicts

--- x/ecocredit/server/core/query_balance.go
+++ x/ecocredit/server/core/query_balance.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"github.com/cosmos/cosmos-sdk/orm/types/ormerrors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/regen-network/regen-ledger/x/ecocredit/v1beta1"
+	v1 "github.com/regen-network/regen-ledger/x/ecocredit/v1"
 )
 
 // Balance queries the balance (both tradable and retired) of a given credit
 // batch for a given account.
-func (k Keeper) Balance(ctx context.Context, req *v1beta1.QueryBalanceRequest) (*v1beta1.QueryBalanceResponse, error) {
+func (k Keeper) Balance(ctx context.Context, req *v1.QueryBalanceRequest) (*v1.QueryBalanceResponse, error) {
 	batch, err := k.stateStore.BatchInfoStore().GetByBatchDenom(ctx, req.BatchDenom)
 	if err != nil {
 		return nil, err
@@ -22,14 +22,14 @@ func (k Keeper) Balance(ctx context.Context, req *v1beta1.QueryBalanceRequest) (
 	balance, err := k.stateStore.BatchBalanceStore().Get(ctx, addr, batch.Id)
 	if err != nil {
 		if ormerrors.IsNotFound(err) {
-			return &v1beta1.QueryBalanceResponse{
+			return &v1.QueryBalanceResponse{
 				TradableAmount: "0",
 				RetiredAmount:  "0",
 			}, nil
 		}
 		return nil, err
 	}
-	return &v1beta1.QueryBalanceResponse{
+	return &v1.QueryBalanceResponse{
 		TradableAmount: balance.Tradable,
 		RetiredAmount:  balance.Retired,
 	}, nil
--- x/ecocredit/server/core/query_balance_test.go
+++ x/ecocredit/server/core/query_balance_test.go
@@ -2,8 +2,8 @@ package core
 
 import (
 	"github.com/cosmos/cosmos-sdk/orm/types/ormerrors"
-	ecocreditv1beta1 "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1beta1"
-	"github.com/regen-network/regen-ledger/x/ecocredit/v1beta1"
+	ecocreditv1 "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1"
+	v1 "github.com/regen-network/regen-ledger/x/ecocredit/v1"
 	"gotest.tools/v3/assert"
 	"testing"
 )
@@ -17,14 +17,14 @@ func TestQuery_Balance(t *testing.T) {
 	retired := "50.3214"
 
 	// make a batch and give s.addr some balance
-	assert.NilError(t, s.stateStore.BatchInfoStore().Insert(s.ctx, &ecocreditv1beta1.BatchInfo{
+	assert.NilError(t, s.stateStore.BatchInfoStore().Insert(s.ctx, &ecocreditv1.BatchInfo{
 		ProjectId:  1,
 		BatchDenom: batchDenom,
 		Metadata:   nil,
 		StartDate:  nil,
 		EndDate:    nil,
 	}))
-	assert.NilError(t, s.stateStore.BatchBalanceStore().Insert(s.ctx, &ecocreditv1beta1.BatchBalance{
+	assert.NilError(t, s.stateStore.BatchBalanceStore().Insert(s.ctx, &ecocreditv1.BatchBalance{
 		Address:  s.addr,
 		BatchId:  1,
 		Tradable: tradable,
@@ -32,7 +32,7 @@ func TestQuery_Balance(t *testing.T) {
 	}))
 
 	// valid query
-	res, err := s.k.Balance(s.ctx, &v1beta1.QueryBalanceRequest{
+	res, err := s.k.Balance(s.ctx, &v1.QueryBalanceRequest{
 		Account:    s.addr.String(),
 		BatchDenom: batchDenom,
 	})
@@ -41,7 +41,7 @@ func TestQuery_Balance(t *testing.T) {
 	assert.Equal(t, retired, res.RetiredAmount)
 
 	// random addr should just give 0
-	res, err = s.k.Balance(s.ctx, &v1beta1.QueryBalanceRequest{
+	res, err = s.k.Balance(s.ctx, &v1.QueryBalanceRequest{
 		Account:    noBalanceAddr.String(),
 		BatchDenom: batchDenom,
 	})
@@ -50,7 +50,7 @@ func TestQuery_Balance(t *testing.T) {
 	assert.Equal(t, "0", res.RetiredAmount)
 
 	// query with invalid batch should return not found
-	_, err = s.k.Balance(s.ctx, &v1beta1.QueryBalanceRequest{
+	_, err = s.k.Balance(s.ctx, &v1.QueryBalanceRequest{
 		Account:    s.addr.String(),
 		BatchDenom: "A00-00000000-00000000-001",
 	})
