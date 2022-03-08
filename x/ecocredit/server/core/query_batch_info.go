ad60bd8ea27b6b6f2b28492452b365c4f4d513a7
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-03-08 11:37:47 -0800

    chore: v1beta1 -> v1

--- x/ecocredit/server/core/query_batch_info.go
+++ x/ecocredit/server/core/query_batch_info.go
@@ -3,11 +3,11 @@ package core
 import (
 	"context"
 	"github.com/regen-network/regen-ledger/x/ecocredit"
-	"github.com/regen-network/regen-ledger/x/ecocredit/v1beta1"
+	v1 "github.com/regen-network/regen-ledger/x/ecocredit/v1"
 )
 
 // BatchInfo queries for information on a credit batch.
-func (k Keeper) BatchInfo(ctx context.Context, request *v1beta1.QueryBatchInfoRequest) (*v1beta1.QueryBatchInfoResponse, error) {
+func (k Keeper) BatchInfo(ctx context.Context, request *v1.QueryBatchInfoRequest) (*v1.QueryBatchInfoResponse, error) {
 	if err := ecocredit.ValidateDenom(request.BatchDenom); err != nil {
 		return nil, err
 	}
@@ -17,9 +17,9 @@ func (k Keeper) BatchInfo(ctx context.Context, request *v1beta1.QueryBatchInfoRe
 		return nil, err
 	}
 
-	var bi v1beta1.BatchInfo
+	var bi v1.BatchInfo
 	if err = PulsarToGogoSlow(batch, &bi); err != nil {
 		return nil, err
 	}
-	return &v1beta1.QueryBatchInfoResponse{Info: &bi}, nil
+	return &v1.QueryBatchInfoResponse{Info: &bi}, nil
 }
--- x/ecocredit/server/core/query_batch_info_test.go
+++ x/ecocredit/server/core/query_batch_info_test.go
@@ -2,8 +2,8 @@ package core
 
 import (
 	"github.com/cosmos/cosmos-sdk/orm/types/ormerrors"
-	ecocreditv1beta1 "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1beta1"
-	"github.com/regen-network/regen-ledger/x/ecocredit/v1beta1"
+	ecocreditv1 "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1"
+	v1 "github.com/regen-network/regen-ledger/x/ecocredit/v1"
 	"gotest.tools/v3/assert"
 	"testing"
 )
@@ -12,7 +12,7 @@ func TestQuery_BatchInfo(t *testing.T) {
 	t.Parallel()
 	s := setupBase(t)
 	batchDenom := "C01-20200101-20220101-001"
-	assert.NilError(t, s.stateStore.BatchInfoStore().Insert(s.ctx, &ecocreditv1beta1.BatchInfo{
+	assert.NilError(t, s.stateStore.BatchInfoStore().Insert(s.ctx, &ecocreditv1.BatchInfo{
 		ProjectId:  1,
 		BatchDenom: batchDenom,
 		Metadata:   nil,
@@ -21,11 +21,11 @@ func TestQuery_BatchInfo(t *testing.T) {
 	}))
 
 	// invalid query
-	_, err := s.k.BatchInfo(s.ctx, &v1beta1.QueryBatchInfoRequest{BatchDenom: "A00-00000000-00000000-000"})
+	_, err := s.k.BatchInfo(s.ctx, &v1.QueryBatchInfoRequest{BatchDenom: "A00-00000000-00000000-000"})
 	assert.ErrorContains(t, err, ormerrors.NotFound.Error())
 
 	// good query
-	res, err := s.k.BatchInfo(s.ctx, &v1beta1.QueryBatchInfoRequest{BatchDenom: batchDenom})
+	res, err := s.k.BatchInfo(s.ctx, &v1.QueryBatchInfoRequest{BatchDenom: batchDenom})
 	assert.NilError(t, err)
 	assert.Equal(t, uint64(1), res.Info.ProjectId)
 }
