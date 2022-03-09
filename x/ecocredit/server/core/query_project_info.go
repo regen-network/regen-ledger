415ebc14bf368bfbc53b394501905ad59a2c9b5f
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-03-08 16:49:37 -0800

    chore: bytes to string md

--- x/ecocredit/server/core/query_project_info.go
+++ x/ecocredit/server/core/query_project_info.go
@@ -2,17 +2,25 @@ package core
 
 import (
 	"context"
+	"github.com/regen-network/regen-ledger/types"
 	v1 "github.com/regen-network/regen-ledger/x/ecocredit/v1"
 )
 
 // ProjectInfo queries project info from the given project name.
 func (k Keeper) ProjectInfo(ctx context.Context, request *v1.QueryProjectInfoRequest) (*v1.QueryProjectInfoResponse, error) {
-	pInfo, err := k.stateStore.ProjectInfoStore().GetByName(ctx, request.ProjectId)
+	info, err := k.stateStore.ProjectInfoStore().GetByName(ctx, request.ProjectId)
 	if err != nil {
 		return nil, err
 	}
+
+	bz, err := types.DecodeMetadata(info.Metadata)
+	if err != nil {
+		return nil, err
+	}
+	info.Metadata = string(bz)
+
 	var pi v1.ProjectInfo
-	if err = PulsarToGogoSlow(pInfo, &pi); err != nil {
+	if err = PulsarToGogoSlow(info, &pi); err != nil {
 		return nil, err
 	}
 	return &v1.QueryProjectInfoResponse{Info: &pi}, nil
