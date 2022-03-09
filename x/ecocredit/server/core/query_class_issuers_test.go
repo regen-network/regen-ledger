8c1f9021bdb5064fd8736f9fb5213d692aad55f5
Tyler <48813565+technicallyty@users.noreply.github.com>
2022-03-09 09:41:42 -0800

    feat(x/ecocredit): query `ClassIssuers` ORM (#839)
    * feat: query class issuers
    
    * Update x/ecocredit/server/core/query_class_issuers_test.go
    
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    
    * chore: v1beta1 -> v1
    
    Co-authored-by: technicallyty <48813565+tytech3@users.noreply.github.com>
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>

---
+++ x/ecocredit/server/core/query_class_issuers_test.go
@@ -0,0 +1,56 @@
+package core
+
+import (
+	"github.com/cosmos/cosmos-sdk/orm/types/ormerrors"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	ecocreditv1 "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1"
+	v1 "github.com/regen-network/regen-ledger/x/ecocredit/v1"
+	"gotest.tools/v3/assert"
+	"testing"
+)
+
+func TestQuery_ClassIssuers(t *testing.T) {
+	t.Parallel()
+	s := setupBase(t)
+
+	// make a class with 3 issuers.
+	addrs := genAddrs(2)
+	issuers := append(addrs, s.addr)
+	err := s.stateStore.ClassInfoStore().Insert(s.ctx, &ecocreditv1.ClassInfo{
+		Name:       "C01",
+		Admin:      s.addr,
+		Metadata:   nil,
+		CreditType: "C",
+	})
+	assert.NilError(t, err)
+	assert.NilError(t, s.stateStore.ClassIssuerStore().Insert(s.ctx, &ecocreditv1.ClassIssuer{
+		ClassId: 1,
+		Issuer:  s.addr,
+	}))
+	assert.NilError(t, s.stateStore.ClassIssuerStore().Insert(s.ctx, &ecocreditv1.ClassIssuer{
+		ClassId: 1,
+		Issuer:  addrs[0],
+	}))
+	assert.NilError(t, s.stateStore.ClassIssuerStore().Insert(s.ctx, &ecocreditv1.ClassIssuer{
+		ClassId: 1,
+		Issuer:  addrs[1],
+	}))
+
+	// base request
+	res, err := s.k.ClassIssuers(s.ctx, &v1.QueryClassIssuersRequest{ClassId: "C01"})
+	assert.NilError(t, err)
+	assert.Equal(t, len(issuers), len(res.Issuers))
+
+	// bad request
+	_, err = s.k.ClassIssuers(s.ctx, &v1.QueryClassIssuersRequest{ClassId: "F01"})
+	assert.ErrorContains(t, err, ormerrors.NotFound.Error())
+
+	// paginated request
+	res, err = s.k.ClassIssuers(s.ctx, &v1.QueryClassIssuersRequest{
+		ClassId: "C01",
+		Pagination: &query.PageRequest{Limit: 1, CountTotal: true},
+	})
+	assert.NilError(t, err)
+	assert.Equal(t, 1, len(res.Issuers))
+	assert.Equal(t, uint64(3), res.Pagination.Total)
+}
