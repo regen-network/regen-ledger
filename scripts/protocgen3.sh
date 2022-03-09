f06e9ef65358b85088ef1dc70203582fb8da6475
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2022-03-08 18:33:12 -0800

    fix versions

--- scripts/protocgen3.sh
+++ scripts/protocgen3.sh
@@ -4,17 +4,20 @@ proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1
 
 for dir in $proto_dirs; do
 
+  # get the version name, e.g. from "./proto/regen/ecocredit/v1alpha1", extract "v1alpha1"
+  version=$(basename $dir)
+
   # get the module name, e.g. from "./proto/regen/ecocredit/v1alpha1", extract "ecocredit"
   module=$(basename $(dirname $dir))
 
-  filename="protobuf.md"
+  filename="protobuf_${version}.md"
   destination="./x/${module}/spec"
 
   # get the parent name, e.g. from "./proto/regen/ecocredit/basket/v1alpha1", extract "ecocredit"
   parent=$(basename $(dirname $(dirname $dir)))
 
   if [ $parent != "regen" ] ; then
-    filename="protobuf_${module}.md"
+    filename="protobuf_${module}_${version}.md"
     destination="./x/${parent}/spec"
   fi
 
