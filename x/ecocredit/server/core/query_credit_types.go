feb4894249c3430c8f194379b8097d763b7c7d08
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-03-09 10:08:56 -0800

    chore: v1beta1 -> v1

--- x/ecocredit/server/core/query_credit_types.go
+++ x/ecocredit/server/core/query_credit_types.go
@@ -2,14 +2,14 @@ package core
 
 import (
 	"context"
-	ecocreditv1beta1 "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1beta1"
-	"github.com/regen-network/regen-ledger/x/ecocredit/v1beta1"
+	ecocreditv1 "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1"
+	v1 "github.com/regen-network/regen-ledger/x/ecocredit/v1"
 )
 
 // CreditTypes queries the list of allowed types that credit classes can have.
-func (k Keeper) CreditTypes(ctx context.Context, _ *v1beta1.QueryCreditTypesRequest) (*v1beta1.QueryCreditTypesResponse, error) {
-	creditTypes := make([]*v1beta1.CreditType, 0)
-	it, err := k.stateStore.CreditTypeStore().List(ctx, ecocreditv1beta1.CreditTypePrimaryKey{})
+func (k Keeper) CreditTypes(ctx context.Context, _ *v1.QueryCreditTypesRequest) (*v1.QueryCreditTypesResponse, error) {
+	creditTypes := make([]*v1.CreditType, 0)
+	it, err := k.stateStore.CreditTypeStore().List(ctx, ecocreditv1.CreditTypePrimaryKey{})
 	if err != nil {
 		return nil, err
 	}
@@ -19,11 +19,11 @@ func (k Keeper) CreditTypes(ctx context.Context, _ *v1beta1.QueryCreditTypesRequ
 		if err != nil {
 			return nil, err
 		}
-		var cType v1beta1.CreditType
+		var cType v1.CreditType
 		if err = PulsarToGogoSlow(ct, &cType); err != nil {
 			return nil, err
 		}
 		creditTypes = append(creditTypes, &cType)
 	}
-	return &v1beta1.QueryCreditTypesResponse{CreditTypes: creditTypes}, nil
+	return &v1.QueryCreditTypesResponse{CreditTypes: creditTypes}, nil
 }
--- x/ecocredit/server/core/query_credit_types_test.go
+++ x/ecocredit/server/core/query_credit_types_test.go
@@ -1,8 +1,8 @@
 package core
 
 import (
-	ecocreditv1beta1 "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1beta1"
-	"github.com/regen-network/regen-ledger/x/ecocredit/v1beta1"
+	ecocreditv1 "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1"
+	v1 "github.com/regen-network/regen-ledger/x/ecocredit/v1"
 	"gotest.tools/v3/assert"
 	"testing"
 )
@@ -12,13 +12,13 @@ func TestQuery_CreditTypes(t *testing.T) {
 	s := setupBase(t)
 
 	// insert a few credit types
-	assert.NilError(t, s.stateStore.CreditTypeStore().Insert(s.ctx, &ecocreditv1beta1.CreditType{
+	assert.NilError(t, s.stateStore.CreditTypeStore().Insert(s.ctx, &ecocreditv1.CreditType{
 		Abbreviation: "C",
 		Name:         "carbon",
 		Unit:         "a ton",
 		Precision:    6,
 	}))
-	assert.NilError(t, s.stateStore.CreditTypeStore().Insert(s.ctx, &ecocreditv1beta1.CreditType{
+	assert.NilError(t, s.stateStore.CreditTypeStore().Insert(s.ctx, &ecocreditv1.CreditType{
 		Abbreviation: "F",
 		Name:         "foobar",
 		Unit:         "foo per inch",
@@ -26,7 +26,7 @@ func TestQuery_CreditTypes(t *testing.T) {
 	}))
 
 	// base query should return all types
-	res, err := s.k.CreditTypes(s.ctx, &v1beta1.QueryCreditTypesRequest{})
+	res, err := s.k.CreditTypes(s.ctx, &v1.QueryCreditTypesRequest{})
 	assert.NilError(t, err)
 	assert.Equal(t, 2, len(res.CreditTypes))
 	assert.Equal(t, uint32(6), res.CreditTypes[0].Precision)
