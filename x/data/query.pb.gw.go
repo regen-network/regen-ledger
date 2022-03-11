03a628ee7f4770e0e531cef16ec591ea97fc5c98
Tyler <48813565+technicallyty@users.noreply.github.com>
2022-03-11 14:19:38 -0800

    fix: update query path to match proto file path (#877)
    Co-authored-by: technicallyty <48813565+tytech3@users.noreply.github.com>

--- x/data/query.pb.gw.go
+++ x/data/query.pb.gw.go
@@ -303,26 +303,19 @@ func local_request_Query_Resolvers_0(ctx context.Context, marshaler runtime.Mars
 
 }
 
+var (
+	filter_Query_ResolverInfo_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_Query_ResolverInfo_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq QueryResolverInfoRequest
 	var metadata runtime.ServerMetadata
 
-	var (
-		val string
-		ok  bool
-		err error
-		_   = err
-	)
-
-	val, ok = pathParams["url"]
-	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "url")
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-
-	protoReq.Url, err = runtime.String(val)
-
-	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "url", err)
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_ResolverInfo_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.ResolverInfo(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -334,22 +327,11 @@ func local_request_Query_ResolverInfo_0(ctx context.Context, marshaler runtime.M
 	var protoReq QueryResolverInfoRequest
 	var metadata runtime.ServerMetadata
 
-	var (
-		val string
-		ok  bool
-		err error
-		_   = err
-	)
-
-	val, ok = pathParams["url"]
-	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "url")
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-
-	protoReq.Url, err = runtime.String(val)
-
-	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "url", err)
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_ResolverInfo_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.ResolverInfo(ctx, &protoReq)
@@ -631,7 +613,7 @@ var (
 
 	pattern_Query_Resolvers_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"regen", "data", "v1", "resolvers", "iri"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_ResolverInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"regen", "data", "v1", "resolver", "url"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_ResolverInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"regen", "data", "v1", "resolver"}, "", runtime.AssumeColonVerbOpt(false)))
 )
 
 var (
