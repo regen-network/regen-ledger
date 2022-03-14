bf710e3ba1f4d30702dcddec5cb0d5ebe30d89dd
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-03-14 10:02:57 -0700

    wip: prune

--- x/ecocredit/server/marketplace/sell_test.go
+++ x/ecocredit/server/marketplace/sell_test.go
@@ -1,8 +1,6 @@
 package marketplace
 
 import (
-	"github.com/regen-network/regen-ledger/types/math"
-	"github.com/regen-network/regen-ledger/x/ecocredit/server"
 	"testing"
 	"time"
 
@@ -10,12 +8,15 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 	"gotest.tools/v3/assert"
 
+	"github.com/cosmos/cosmos-sdk/orm/types/ormerrors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	marketApi "github.com/regen-network/regen-ledger/api/regen/ecocredit/marketplace/v1"
 	ecocreditv1 "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1"
+	"github.com/regen-network/regen-ledger/types/math"
 	"github.com/regen-network/regen-ledger/x/ecocredit"
 	v1 "github.com/regen-network/regen-ledger/x/ecocredit/marketplace"
+	"github.com/regen-network/regen-ledger/x/ecocredit/server"
 )
 
 func TestSell_Valid(t *testing.T) {
@@ -190,6 +191,59 @@ func TestSell_Invalid(t *testing.T) {
 		},
 	})
 	assert.ErrorContains(t, err, "expiration must be in the future")
+}
+
+func TestSell_Prune(t *testing.T) {
+	t.Parallel()
+	s := setupBase(t)
+	any := gomock.Any()
+	batchDenom := "C01-20200101-20200201-001"
+	start, end := timestamppb.Now(), timestamppb.Now()
+	ask := sdk.NewInt64Coin("ufoo", 10)
+	creditType := ecocredit.CreditType{
+		Name:         "carbon",
+		Abbreviation: "C",
+		Unit:         "tonnes",
+		Precision:    6,
+	}
+	marketTestSetup(t, s, batchDenom, ask.Denom, ask.Denom[1:], "C01", start, end, creditType)
+	s.paramsKeeper.EXPECT().GetParamSet(any, any).Do(func(any interface{}, p *ecocredit.Params) {
+		p.CreditTypes = []*ecocredit.CreditType{&creditType}
+	}).Times(2)
+
+	blockTime, err := time.Parse("2006-01-02", "2020-01-01")
+	assert.NilError(t, err)
+	expired, err := time.Parse("2006-01-02", "2019-12-30")
+	assert.NilError(t, err)
+	notExpired, err := time.Parse("2006-01-02", "2022-01-01")
+	assert.NilError(t, err)
+
+	res, err := s.k.Sell(s.ctx, &v1.MsgSell{
+		Owner:  s.addr.String(),
+		Orders: []*v1.MsgSell_Order{
+			{BatchDenom: batchDenom, Quantity: "10", AskPrice: &ask, Expiration: &expired},
+			{BatchDenom: batchDenom, Quantity: "10", AskPrice: &ask, Expiration: &notExpired},
+		},
+	})
+	assert.NilError(t, err)
+
+	// setup block time so the orders expire
+	s.sdkCtx = s.sdkCtx.WithBlockTime(blockTime)
+	s.ctx = sdk.WrapSDKContext(s.sdkCtx)
+
+	// prune the orders
+	err = s.k.PruneSellOrders(s.ctx)
+	assert.NilError(t, err)
+
+	assert.Equal(t, 2, len(res.SellOrderIds))
+	shouldBeExpired := res.SellOrderIds[0]
+	shouldBeValid := res.SellOrderIds[1]
+
+	_, err = s.marketStore.SellOrderStore().Get(s.ctx, shouldBeExpired)
+	assert.ErrorContains(t, err, ormerrors.NotFound.Error())
+
+	_, err = s.marketStore.SellOrderStore().Get(s.ctx, shouldBeValid)
+	assert.NilError(t, err)
 }
 
 // marketTestSetup creates a batch with retired/tradable supply of 100 each, class, market, and issues a balance of 100
