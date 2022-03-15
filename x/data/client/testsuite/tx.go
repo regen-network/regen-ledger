7195314c0bea88bc9cdfd51d3ec1b3c6cececa89
MD Aleem <72057206+aleem1314@users.noreply.github.com>
2022-03-15 20:09:26 +0530

    feat(x/data): add CLI commands for data resolver (#866)
    * add query cmds
    
    * wip cli tx cmds
    
    * wip: register-resolver cmd
    
    * chore: proto-gen
    
    * chore: fix errors
    
    * make proto-gen
    
    * add  logs
    
    * add logs
    
    * all query tests
    
    * fix test
    
    * fix test
    
    * cleanup
    
    * fix failing tests
    
    * add logs
    
    * fix more tests
    
    * fix: resolver info tests
    
    * fix : query resolvers test
    
    * Update x/data/client/tx.go
    
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    
    * chore: update test_cover.sh
    
    Co-authored-by: technicallyty <48813565+tytech3@users.noreply.github.com>
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>

--- x/data/client/testsuite/tx.go
+++ x/data/client/testsuite/tx.go
@@ -1,16 +1,24 @@
 package testsuite
 
 import (
+	"crypto"
 	"fmt"
+
+	"github.com/stretchr/testify/suite"
+
+	tmcli "github.com/tendermint/tendermint/libs/cli"
+
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"github.com/cosmos/cosmos-sdk/testutil"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	banktestutil "github.com/cosmos/cosmos-sdk/x/bank/client/testutil"
+
 	"github.com/regen-network/regen-ledger/types/testutil/cli"
 	"github.com/regen-network/regen-ledger/types/testutil/network"
+	"github.com/regen-network/regen-ledger/x/data"
 	"github.com/regen-network/regen-ledger/x/data/client"
-	"github.com/stretchr/testify/suite"
 )
 
 type IntegrationTestSuite struct {
@@ -18,6 +26,9 @@ type IntegrationTestSuite struct {
 
 	cfg     network.Config
 	network *network.Network
+
+	resolverID uint64
+	iri        string
 }
 
 func NewIntegrationTestSuite(cfg network.Config) *IntegrationTestSuite {
@@ -108,6 +119,44 @@ func (s *IntegrationTestSuite) SetupSuite() {
 		)
 		s.Require().NoError(err)
 	}
+
+	_, err = cli.ExecTestCLICmd(val1.ClientCtx, client.MsgDefineResolverCmd(),
+		append(
+			[]string{
+				"http://foo.bar",
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, account1.String()),
+			},
+			commonFlags...,
+		),
+	)
+	s.Require().NoError(err)
+
+	cmd := client.QueryResolverInfoCmd()
+	out, err := cli.ExecTestCLICmd(val1.ClientCtx, cmd, []string{"http://foo.bar", fmt.Sprintf("--%s=json", tmcli.OutputFlag)})
+	s.Require().NoError(err)
+	var rInfo data.QueryResolverInfoResponse
+	s.Require().NoError(val1.ClientCtx.Codec.UnmarshalJSON(out.Bytes(), &rInfo))
+	s.resolverID = rInfo.Id
+
+	content := []byte("abcdefg")
+	_, chs := s.createDataContent(content)
+
+	s.iri, err = chs.Data[0].GetGraph().ToIRI()
+	s.Require().NoError(err)
+
+	bz, err := val1.ClientCtx.Codec.MarshalJSON(chs)
+	s.Require().NoError(err)
+	filePath := testutil.WriteToNewTempFile(s.T(), string(bz)).Name()
+	cmd = client.MsgRegisterResolverCmd()
+	_, err = cli.ExecTestCLICmd(val1.ClientCtx, cmd, append(
+		[]string{
+			fmt.Sprintf("%d", s.resolverID),
+			filePath,
+			fmt.Sprintf("--%s=%s", flags.FlagFrom, account1.String()),
+		},
+		commonFlags...,
+	))
+	s.Require().NoError(err)
 }
 
 func (s *IntegrationTestSuite) TearDownSuite() {
@@ -234,4 +283,157 @@ func (s *IntegrationTestSuite) TestTxSignData() {
 			}
 		})
 	}
+}
+
+func (s *IntegrationTestSuite) TestDefineResolverCmd() {
+	val := s.network.Validators[0]
+	clientCtx := val.ClientCtx
+	require := s.Require()
+
+	var commonFlags = []string{
+		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+		fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+		fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+	}
+
+	testCases := []struct {
+		name        string
+		resolverUrl string
+		expErr      bool
+		errMsg      string
+	}{
+		{
+			"empty url",
+			"",
+			true,
+			"empty url",
+		},
+		{
+			"invalid url",
+			"abcd",
+			true,
+			"invalid URI",
+		},
+		{
+			"valid test",
+			"http:foo.bar",
+			false,
+			"",
+		},
+	}
+
+	cmd := client.MsgDefineResolverCmd()
+	for _, tc := range testCases {
+		s.Run(tc.name, func() {
+			args := []string{tc.resolverUrl}
+			args = append(args, commonFlags...)
+			_, err := cli.ExecTestCLICmd(clientCtx, cmd, args)
+			if tc.expErr {
+				require.Error(err)
+				require.Contains(err.Error(), tc.errMsg, err.Error())
+			} else {
+				require.NoError(err)
+			}
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestRegisterResolverCmd() {
+	val := s.network.Validators[0]
+	clientCtx := val.ClientCtx
+	require := s.Require()
+
+	var commonFlags = []string{
+		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+		fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+		fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+	}
+
+	content := []byte("xyzabc123")
+	_, chs := s.createDataContent(content)
+
+	bz, err := val.ClientCtx.Codec.MarshalJSON(chs)
+	require.NoError(err)
+	filePath := testutil.WriteToNewTempFile(s.T(), string(bz)).Name()
+
+	testCases := []struct {
+		name     string
+		args     []string
+		expErr   bool
+		errInRes bool
+		errMsg   string
+	}{
+		{
+			"empty args",
+			[]string{},
+			true,
+			false,
+			"accepts 2 arg(s), received 0",
+		},
+		{
+			"invalid file path",
+			[]string{fmt.Sprintf("%d", s.resolverID), "test.json"},
+			true,
+			false,
+			"no such file or directory",
+		},
+		{
+			"invalid resolver id",
+			[]string{fmt.Sprintf("%d", 12345), filePath},
+			false,
+			true,
+			"not found",
+		},
+		{
+			"valid test",
+			[]string{fmt.Sprintf("%d", s.resolverID), filePath},
+			false,
+			false,
+			"",
+		},
+	}
+
+	cmd := client.MsgRegisterResolverCmd()
+	for _, tc := range testCases {
+		s.Run(tc.name, func() {
+			args := append(tc.args, commonFlags...)
+			res, err := cli.ExecTestCLICmd(clientCtx, cmd, args)
+			if tc.expErr {
+				require.Error(err)
+				require.Contains(err.Error(), tc.errMsg, err.Error())
+			} else {
+				if tc.errInRes {
+					require.Contains(res.String(), tc.errMsg)
+				} else {
+					require.NoError(err)
+				}
+			}
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) createDataContent(content []byte) (string, *data.ContentHashes) {
+	require := s.Require()
+
+	hash := crypto.BLAKE2b_256.New()
+	_, err := hash.Write(content)
+	require.NoError(err)
+	digest := hash.Sum(nil)
+
+	ch := data.ContentHash{
+		Sum: &data.ContentHash_Graph_{
+			Graph: &data.ContentHash_Graph{
+				Hash:                      digest,
+				DigestAlgorithm:           data.DigestAlgorithm_DIGEST_ALGORITHM_BLAKE2B_256,
+				CanonicalizationAlgorithm: data.GraphCanonicalizationAlgorithm_GRAPH_CANONICALIZATION_ALGORITHM_URDNA2015,
+			},
+		},
+	}
+
+	iri, err := ch.GetGraph().ToIRI()
+	require.NoError(err)
+
+	return iri, &data.ContentHashes{Data: []*data.ContentHash{&ch}}
 }
