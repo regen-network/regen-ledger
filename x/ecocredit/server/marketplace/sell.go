3f1dff4d78ee9b33bbb756e3efb40004971026df
Tyler <48813565+technicallyty@users.noreply.github.com>
2022-03-15 16:24:34 -0700

    feat(marketplace): escrow sell order credits (#878)
    * feat: escrow credits
    
    * fix: revert mock times
    
    * fix: typo
    
    * chore: add event emission to sell
    
    * fix: assert precision of sell order qty
    
    * feat: pruning
    
    * chore: fmt.Errorf -> sdkerrors.ErrInvalidRequest
    
    * chore: change comment
    
    * chore: move prune test to its own file
    
    Co-authored-by: technicallyty <48813565+tytech3@users.noreply.github.com>

--- x/ecocredit/server/marketplace/sell.go
+++ x/ecocredit/server/marketplace/sell.go
@@ -2,8 +2,6 @@ package marketplace
 
 import (
 	"context"
-	"fmt"
-
 	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/cosmos/cosmos-sdk/orm/types/ormerrors"
@@ -29,7 +27,7 @@ func (k Keeper) Sell(ctx context.Context, req *marketplacev1.MsgSell) (*marketpl
 	for i, order := range req.Orders {
 		batch, err := k.coreStore.BatchInfoTable().GetByBatchDenom(ctx, order.BatchDenom)
 		if err != nil {
-			return nil, fmt.Errorf("batch denom %v: %v", order.BatchDenom, err)
+			return nil, sdkerrors.ErrInvalidRequest.Wrapf("batch denom %s: %s", order.BatchDenom, err.Error())
 		}
 		ct, err := server.GetCreditTypeFromBatchDenom(ctx, k.coreStore, k.params, batch.BatchDenom)
 		if err != nil {
@@ -40,14 +38,13 @@ func (k Keeper) Sell(ctx context.Context, req *marketplacev1.MsgSell) (*marketpl
 		if order.Expiration != nil && order.Expiration.Before(sdkCtx.BlockTime()) {
 			return nil, sdkerrors.ErrInvalidRequest.Wrapf("expiration must be in the future: %s", order.Expiration)
 		}
-		sellQty, err := math.NewDecFromString(order.Quantity)
+		sellQty, err := math.NewPositiveFixedDecFromString(order.Quantity, ct.Precision)
 		if err != nil {
 			return nil, err
 		}
-		if err := assertHasBalance(ctx, k.coreStore, ownerAcc, batch.Id, sellQty); err != nil {
+		if err = k.escrowCredits(ctx, ownerAcc, batch.Id, sellQty); err != nil {
 			return nil, err
 		}
-
 		// TODO: pending param refactor https://github.com/regen-network/regen-ledger/issues/624
 		//has, err := isDenomAllowed(ctx, k.stateStore, order.AskPrice.Denom)
 		//if err != nil {
@@ -72,6 +69,16 @@ func (k Keeper) Sell(ctx context.Context, req *marketplacev1.MsgSell) (*marketpl
 		}
 		sellOrderIds[i] = id
 		sdkCtx.GasMeter().ConsumeGas(gasCostPerIteration, "create sell order")
+		if err = sdkCtx.EventManager().EmitTypedEvent(&marketplacev1.EventSell{
+			OrderId:           id,
+			BatchDenom:        batch.BatchDenom,
+			Quantity:          order.Quantity,
+			AskPrice:          order.AskPrice,
+			DisableAutoRetire: order.DisableAutoRetire,
+			Expiration:        order.Expiration,
+		}); err != nil {
+			return nil, err
+		}
 	}
 	return &marketplacev1.MsgSellResponse{SellOrderIds: sellOrderIds}, nil
 }
@@ -91,4 +98,58 @@ func (k Keeper) getOrCreateMarketId(ctx context.Context, creditTypeAbbrev, bankD
 	default:
 		return 0, err
 	}
+}
+
+func (k Keeper) escrowCredits(ctx context.Context, ownerAcc sdk.AccAddress, batchId uint64, sellQty math.Dec) error {
+	bal, err := k.coreStore.BatchBalanceTable().Get(ctx, ownerAcc, batchId)
+	if err != nil {
+		return err
+	}
+	tradable, err := math.NewDecFromString(bal.Tradable)
+	if err != nil {
+		return err
+	}
+	newTradable, err := math.SafeSubBalance(tradable, sellQty)
+	if err != nil {
+		return sdkerrors.ErrInvalidRequest.Wrapf("tradable balance: %v, sell order request: %v - %s", tradable, sellQty, err.Error())
+	}
+
+	escrowed, err := math.NewDecFromString(bal.Escrowed)
+	if err != nil {
+		return err
+	}
+	newEscrowed, err := math.SafeAddBalance(escrowed, sellQty)
+	if err != nil {
+		return sdkerrors.ErrInvalidRequest.Wrapf("escrowed balance: %v, sell order request: %v - %s", escrowed, sellQty, err.Error())
+	}
+	bal.Tradable = newTradable.String()
+	bal.Escrowed = newEscrowed.String()
+
+	if err = k.coreStore.BatchBalanceTable().Update(ctx, bal); err != nil {
+		return err
+	}
+
+	supply, err := k.coreStore.BatchSupplyTable().Get(ctx, batchId)
+	if err != nil {
+		return err
+	}
+	supTradable, err := math.NewDecFromString(supply.TradableAmount)
+	if err != nil {
+		return err
+	}
+	supEscrow, err := math.NewDecFromString(supply.EscrowedAmount)
+	if err != nil {
+		return err
+	}
+	supTradable, err = math.SafeSubBalance(supTradable, sellQty)
+	if err != nil {
+		return err
+	}
+	supEscrow, err = math.SafeAddBalance(supEscrow, sellQty)
+	if err != nil {
+		return err
+	}
+	supply.EscrowedAmount = supEscrow.String()
+	supply.TradableAmount = supTradable.String()
+	return k.coreStore.BatchSupplyTable().Save(ctx, supply)
 }
