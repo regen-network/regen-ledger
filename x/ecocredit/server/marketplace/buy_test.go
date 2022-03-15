2ae117ce07ffe8a83cf5ef19866c1ee3bb1d640f
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-03-15 15:14:30 -0700

    chore: add invalid test

--- x/ecocredit/server/marketplace/buy_test.go
+++ x/ecocredit/server/marketplace/buy_test.go
@@ -172,3 +172,67 @@ func TestBuy_OrderFilled(t *testing.T) {
 	assert.NilError(t, err)
 	assert.Check(t, retiredBal.Equal(math.NewDecFromInt64(10)))
 }
+
+func TestBuy_Invalid(t *testing.T) {
+	t.Parallel()
+	s := setupBase(t)
+	_, _, buyerAddr := testdata.KeyTestPubAddr()
+	batchDenom := "C01-20200101-20200201-001"
+	start, end := timestamppb.Now(), timestamppb.Now()
+	ask := sdk.NewInt64Coin("ufoo", 10)
+	creditType := ecocredit.CreditType{
+		Name:         "carbon",
+		Abbreviation: "C",
+		Unit:         "tonnes",
+		Precision:    6,
+	}
+	marketTestSetup(t, s, batchDenom, ask.Denom, ask.Denom[1:], "C01", start, end, creditType)
+	// make a sell order
+	any := gomock.Any()
+	s.paramsKeeper.EXPECT().GetParamSet(any, any).Do(func(any interface{}, p *ecocredit.Params) {
+		p.CreditTypes = []*ecocredit.CreditType{&creditType}
+	}).AnyTimes()
+	sellExp := time.Now()
+	res, err := s.k.Sell(s.ctx, &marketplace.MsgSell{
+		Owner:  s.addr.String(),
+		Orders: []*marketplace.MsgSell_Order{
+			{BatchDenom: batchDenom, Quantity: "10", AskPrice: &ask, DisableAutoRetire: false, Expiration: &sellExp},
+		},
+	})
+	assert.NilError(t, err)
+	sellOrderId := res.SellOrderIds[0]
+
+	s.bankKeeper.EXPECT().HasBalance(any, any, any).Return(true).Times(3)
+
+	// sell order not found
+	_, err = s.k.Buy(s.ctx, &marketplace.MsgBuy{
+		Buyer:  buyerAddr.String(),
+		Orders: []*marketplace.MsgBuy_Order{
+			{Selection: &marketplace.MsgBuy_Order_Selection{Sum: &marketplace.MsgBuy_Order_Selection_SellOrderId{SellOrderId: 532}},
+				Quantity: "10", BidPrice: &ask, DisableAutoRetire: false, Expiration: &sellExp},
+		},
+	})
+	assert.ErrorContains(t, err, ormerrors.NotFound.Error())
+
+
+	// exceeds decimal precision
+	_, err = s.k.Buy(s.ctx, &marketplace.MsgBuy{
+		Buyer:  buyerAddr.String(),
+		Orders: []*marketplace.MsgBuy_Order{
+			{Selection: &marketplace.MsgBuy_Order_Selection{Sum: &marketplace.MsgBuy_Order_Selection_SellOrderId{SellOrderId: sellOrderId}},
+				Quantity: "10.23958230958", BidPrice: &ask, DisableAutoRetire: false, Expiration: &sellExp},
+		},
+	})
+	assert.ErrorContains(t, err, "exceeds maximum decimal places")
+
+	// mismatch auto retire settings
+	_, err = s.k.Buy(s.ctx, &marketplace.MsgBuy{
+		Buyer:  buyerAddr.String(),
+		Orders: []*marketplace.MsgBuy_Order{
+			{Selection: &marketplace.MsgBuy_Order_Selection{Sum: &marketplace.MsgBuy_Order_Selection_SellOrderId{SellOrderId: sellOrderId}},
+				Quantity: "10", BidPrice: &ask, DisableAutoRetire: true, Expiration: &sellExp},
+		},
+	})
+	assert.ErrorContains(t, err, "auto-retire mismatch")
+
+}
