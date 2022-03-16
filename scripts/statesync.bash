b7836a648db74e68ad300fafaa6dc7e6c0b01728
kaustubhkapatral <kaustubh.kapatral@gmail.com>
2022-03-16 15:46:36 +0530

    updated script with peer info and sed

--- scripts/statesync.bash
+++ scripts/statesync.bash
@@ -2,7 +2,7 @@
 # This is the Regen Network State sync file, which is based on the Gaia State sync file, which is based on scripts written by Bitcanna and Microtick. The key difference is that this approach uses environment variables to override configuration files.
 # http://public-rpc.regen.vitwit.com:26657
 # https://regen.stakesystems.io:2053
-# http://rpc.regen.forbole.com:80
+# https://rpc.regen.forbole.com:443
 
 
 
@@ -11,13 +11,19 @@ set -uxe
 # set environment variables
 export GOPATH=~/go
 export PATH=$PATH:~/go/bin
+export HOME_DIR=~/.regen
 
+MONIKER=$1
+if [ -z $MONIKER ]
+then
+    MONIKER=test-sync
+fi
 
 # MAKE HOME FOLDER AND GET GENESIS
-regen init statesync
-wget -O ~/.regen/config/genesis.json https://cloudflare-ipfs.com/ipfs/QmdwHTcBcrowCpiFhuSnf5C3jxRMH8jBRqp7jimbHWLZFs
+regen init $MONIKER --home $HOME_DIR
+wget https://raw.githubusercontent.com/regen-network/mainnet/main/regen-1/genesis.json -O $HOME_DIR/config/genesis.json 
 
-INTERVAL=1000
+INTERVAL=10
 
 # GET TRUST HASH AND TRUST HEIGHT
 
@@ -34,10 +40,20 @@ echo "TRUST HASH: $TRUST_HASH"
 # expor state sync vars
 export REGEN_STATESYNC_ENABLE=true
 export REGEN_P2P_MAX_NUM_OUTBOUND_PEERS=200
-export REGEN_STATESYNC_RPC_SERVERS="http://public-rpc.regen.vitwit.com:26657,http://rpc.regen.forbole.com:80,https://regen.stakesystems.io:2053"
+export REGEN_P2P_MAX_NUM_INBOUND_PEERS=200
+export REGEN_STATESYNC_RPC_SERVERS="http://public-rpc.regen.vitwit.com:26657,https://rpc.regen.forbole.com:443,https://regen.stakesystems.io:2053"
 export REGEN_STATESYNC_TRUST_HEIGHT=$BLOCK_HEIGHT
 export REGEN_STATESYNC_TRUST_HASH=$TRUST_HASH
-export REGEN_P2P_SEEDS="aebb8431609cb126a977592446f5de252d8b7fa1@104.236.201.138:26656"
-export REGEN_P2P_PERSISTENT_PEERS="69975e7afdf731a165e40449fcffc75167a084fc@104.131.169.70:26656,d35d652b6cb3bf7d6cb8d4bd7c036ea03e7be2ab@116.203.182.185:26656,ffacd3202ded6945fed12fa4fd715b1874985b8c@3.98.38.91:26656"
+export REGEN_P2P_SEEDS=$(curl -s https://raw.githubusercontent.com/regen-network/mainnet/main/regen-1/seed-nodes.txt | paste -sd,)
+export REGEN_P2P_PERSISTENT_PEERS=$(curl -s https://raw.githubusercontent.com/regen-network/mainnet/main/regen-1/peer-nodes.txt | paste -sd,)
+
+sed -i '/persistent_peers =/c\persistent_peers = "'"$REGEN_P2P_PERSISTENT_PEERS"'"' $HOME_DIR/config/config.toml
+sed -i '/seeds =/c\seeds = "'"$REGEN_P2P_SEEDS"'"' $HOME_DIR/config/config.toml
+sed -i '/max_num_outbound_peers =/c\max_num_outbound_peers = '$REGEN_P2P_MAX_NUM_OUTBOUND_PEERS'' $HOME_DIR/config/config.toml
+sed -i '/max_num_inbound_peers =/c\max_num_inbound_peers = '$REGEN_P2P_MAX_NUM_INBOUND_PEERS'' $HOME_DIR/config/config.toml
+sed -i '/enable =/c\enable = true' $HOME_DIR/config/config.toml
+sed -i '/rpc_servers =/c\rpc_servers = "'"$REGEN_STATESYNC_RPC_SERVERS"'"' $HOME_DIR/config/config.toml
+sed -i '/trust_height =/c\trust_height = '$REGEN_STATESYNC_TRUST_HEIGHT'' $HOME_DIR/config/config.toml
+sed -i '/trust_hash =/c\trust_hash = "'"$REGEN_STATESYNC_TRUST_HASH"'"' $HOME_DIR/config/config.toml
 
 regen start --x-crisis-skip-assert-invariants
