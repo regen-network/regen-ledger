e4c3bed9beda21364378e98dc6c0e847d9ddc717
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-03-17 16:35:51 -0700

    chore: v1 -> marketplace

--- x/ecocredit/server/marketplace/query_sell_order.go
+++ x/ecocredit/server/marketplace/query_sell_order.go
@@ -5,18 +5,18 @@ import (
 
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
-	v1 "github.com/regen-network/regen-ledger/x/ecocredit/marketplace"
+	"github.com/regen-network/regen-ledger/x/ecocredit/marketplace"
 	"github.com/regen-network/regen-ledger/x/ecocredit/server/basket"
 )
 
-func (k Keeper) SellOrder(ctx context.Context, req *v1.QuerySellOrderRequest) (*v1.QuerySellOrderResponse, error) {
+func (k Keeper) SellOrder(ctx context.Context, req *marketplace.QuerySellOrderRequest) (*marketplace.QuerySellOrderResponse, error) {
 	order, err := k.stateStore.SellOrderTable().Get(ctx, req.SellOrderId)
 	if err != nil {
 		return nil, sdkerrors.ErrInvalidRequest.Wrapf("could not get sell order with id %d: %s", req.SellOrderId, err.Error())
 	}
-	var so v1.SellOrder
+	var so marketplace.SellOrder
 	if err = basket.PulsarToGogoSlow(order, &so); err != nil {
 		return nil, err
 	}
-	return &v1.QuerySellOrderResponse{SellOrder: &so}, nil
+	return &marketplace.QuerySellOrderResponse{SellOrder: &so}, nil
 }
