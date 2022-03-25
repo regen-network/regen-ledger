e18585a9ad99768bafd26a4bb90987c2a15c02a7
Robert Zaremba <robert@zaremba.ch>
2022-03-25 21:56:50 +0100

    Update types/testutil/checkers/strings.go
    Co-authored-by: Tyler <48813565+technicallyty@users.noreply.github.com>

--- types/testutil/checkers/strings.go
+++ types/testutil/checkers/strings.go
@@ -2,8 +2,8 @@ package structvalid
 
 import "fmt"
 
-// StrMaxLen checks if given string s length is not bigger then max.
-// Appends error message got the errs list otherwise.
+// StrMaxLen checks if given string s length is not bigger than max.
+// Appends error message, get the errs list otherwise.
 func StrMaxLen(field, s string, max int, errs []string) []string {
 	if len(s) > max {
 		return append(errs, fmt.Sprintf("Max length of %q is %d.", field, len(s)))
