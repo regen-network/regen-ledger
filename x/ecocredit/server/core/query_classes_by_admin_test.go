79339ec9fa0dc062c29b92fca759d419723b2710
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-03-29 13:06:42 -0700

    feat: query classes by admin address

---
+++ x/ecocredit/server/core/query_classes_by_admin_test.go
@@ -0,0 +1,39 @@
+package core
+
+import (
+	"testing"
+
+	api "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1"
+	"github.com/regen-network/regen-ledger/x/ecocredit/core"
+	"gotest.tools/v3/assert"
+
+	"github.com/cosmos/cosmos-sdk/testutil/testdata"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/query"
+)
+
+func TestQueryClassesByAdmin(t *testing.T) {
+	t.Parallel()
+	s := setupBase(t)
+	_, _, addr := testdata.KeyTestPubAddr()
+	_, _, noClasses := testdata.KeyTestPubAddr()
+	assert.NilError(t, s.stateStore.ClassInfoTable().Insert(s.ctx, &api.ClassInfo{Name: "C01", Admin: s.addr, CreditType: "C"}))
+	assert.NilError(t, s.stateStore.ClassInfoTable().Insert(s.ctx, &api.ClassInfo{Name: "C02", Admin: s.addr, CreditType: "C"}))
+	assert.NilError(t, s.stateStore.ClassInfoTable().Insert(s.ctx, &api.ClassInfo{Name: "C03", Admin: addr, CreditType: "C"}))
+
+	// valid query
+	res, err := s.k.ClassesByAdmin(s.ctx, &core.QueryClassesByAdminRequest{Admin: s.addr.String(), Pagination: &query.PageRequest{Limit: 1, CountTotal: true}})
+	assert.NilError(t, err)
+	assert.Equal(t, len(res.Classes), 1)
+	assert.Equal(t, "C01", res.Classes[0].Name)
+	assert.Equal(t, uint64(2), res.Pagination.Total)
+
+	// should be empty
+	res, err = s.k.ClassesByAdmin(s.ctx, &core.QueryClassesByAdminRequest{Admin: noClasses.String(), Pagination: &query.PageRequest{Limit: 10, CountTotal: true}})
+	assert.NilError(t, err)
+	assert.Equal(t, 0, len(res.Classes))
+
+	// invalid address
+	_, err = s.k.ClassesByAdmin(s.ctx, &core.QueryClassesByAdminRequest{Admin: "invalid_address"})
+	assert.ErrorContains(t, err, sdkerrors.ErrInvalidAddress.Error())
+}
