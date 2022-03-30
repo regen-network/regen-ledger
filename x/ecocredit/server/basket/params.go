ea479415215f5cbd724b7b9d3a3fd558655a3200
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-03-30 15:51:52 -0700

    chore: add dummy impl

---
+++ x/ecocredit/server/basket/params.go
@@ -0,0 +1,12 @@
+package basket
+
+import (
+	"context"
+
+	baskettypes "github.com/regen-network/regen-ledger/x/ecocredit/basket"
+)
+
+func (k Keeper) UpdateBasketFee(ctx context.Context, fee *baskettypes.MsgUpdateBasketFee) (*baskettypes.MsgUpdateBasketFeeResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
