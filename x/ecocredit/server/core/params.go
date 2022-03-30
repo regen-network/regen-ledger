ed87faa33f7eff07059ce6e47af0931b6256f005
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-03-30 14:22:13 -0700

    chore: dummy impl

---
+++ x/ecocredit/server/core/params.go
@@ -0,0 +1,27 @@
+package core
+
+import (
+	"context"
+
+	"github.com/regen-network/regen-ledger/x/ecocredit/core"
+)
+
+func (k Keeper) AddCreditType(ctx context.Context, creditType *core.MsgAddCreditType) (*core.MsgAddCreditTypeResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k Keeper) UpdateClassCreatorAllowlist(ctx context.Context, allowlist *core.MsgUpdateClassCreatorAllowlist) (*core.MsgUpdateClassCreatorAllowlistResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k Keeper) ToggleAllowlist(ctx context.Context, allowlist *core.MsgToggleAllowlist) (*core.MsgToggleAllowlistResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k Keeper) UpdateClassFee(ctx context.Context, fee *core.MsgUpdateClassFee) (*core.MsgUpdateClassFeeResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
