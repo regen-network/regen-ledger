cb900e917f80ac772125097776d5243eb00f6f9d
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-03-29 17:40:45 -0700

    feat: query batches by issuer

---
+++ x/ecocredit/server/core/query_batches_by_issuer_test.go
@@ -0,0 +1,36 @@
+package core
+
+import (
+	"testing"
+
+	api "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1"
+	"github.com/regen-network/regen-ledger/x/ecocredit/core"
+	"gotest.tools/v3/assert"
+
+	"github.com/cosmos/cosmos-sdk/testutil/testdata"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/query"
+)
+
+func TestQueryBatchesByIssuer(t *testing.T) {
+	t.Parallel()
+	s := setupBase(t)
+	_, _, otherAddr := testdata.KeyTestPubAddr()
+	_, _, noBatches := testdata.KeyTestPubAddr()
+	assert.NilError(t, s.stateStore.BatchInfoTable().Insert(s.ctx, &api.BatchInfo{Issuer: s.addr, BatchDenom: "1"}))
+	assert.NilError(t, s.stateStore.BatchInfoTable().Insert(s.ctx, &api.BatchInfo{Issuer: s.addr, BatchDenom: "2"}))
+	assert.NilError(t, s.stateStore.BatchInfoTable().Insert(s.ctx, &api.BatchInfo{Issuer: otherAddr, BatchDenom: "3"}))
+
+	res, err := s.k.BatchesByIssuer(s.ctx, &core.QueryBatchesByIssuerRequest{Issuer: s.addr.String(), Pagination: &query.PageRequest{Limit: 1, CountTotal: true}})
+	assert.NilError(t, err)
+	assert.Equal(t, 1, len(res.Batches))
+	assert.Equal(t, "1", res.Batches[0].BatchDenom)
+	assert.Equal(t, uint64(2), res.Pagination.Total)
+
+	res, err = s.k.BatchesByIssuer(s.ctx, &core.QueryBatchesByIssuerRequest{Issuer: noBatches.String()})
+	assert.NilError(t, err)
+	assert.Equal(t, 0, len(res.Batches))
+
+	_, err = s.k.BatchesByIssuer(s.ctx, &core.QueryBatchesByIssuerRequest{Issuer: "foobar"})
+	assert.ErrorContains(t, err, sdkerrors.ErrInvalidAddress.Error())
+}
