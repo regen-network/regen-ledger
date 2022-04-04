db010afc6e93fe8a7dbcd55106cf2ddbe58108c3
Tyler <48813565+technicallyty@users.noreply.github.com>
2022-04-04 12:51:00 -0700

    chore: apply suggestions from code review
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>

--- x/ecocredit/server/marketplace/buy.go
+++ x/ecocredit/server/marketplace/buy.go
@@ -16,15 +16,9 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// Buy creates a buy order to be used in the order-book.
+// Buy creates a buy order that matches sell orders based on filter criteria.
 func (k Keeper) Buy(ctx context.Context, req *marketplace.MsgBuy) (*marketplace.MsgBuyResponse, error) {
 	return nil, sdkerrors.ErrInvalidRequest.Wrap("only direct buy orders are enabled at this time")
-	//sdkCtx := sdk.UnwrapSDKContext(ctx)
-	//buyerAcc, err := sdk.AccAddressFromBech32(req.Buyer)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//return &marketplace.MsgBuyResponse{}, nil
 }
 
 // fillOrder moves credits according to the order. it will:
