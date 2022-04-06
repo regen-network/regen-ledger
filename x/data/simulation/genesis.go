2bbebf8b1f154fbb077dcca66be8fc318c81b6e3
MD Aleem <72057206+aleem1314@users.noreply.github.com>
2022-04-07 02:55:54 +0530

    feat(data): add simulations (#949)
    * feat: init data sims
    
    * wip: add msgs
    
    * wip: add weighted operations
    
    * add more sims
    
    * chore: fix failing sims
    
    Co-authored-by: ryanchristo <12519942+ryanchristo@users.noreply.github.com>

---
+++ x/data/simulation/genesis.go
@@ -0,0 +1,8 @@
+package simulation
+
+import "github.com/cosmos/cosmos-sdk/types/module"
+
+// RandomizedGenState generates a random GenesisState for the data module.
+func RandomizedGenState(simState *module.SimulationState) {
+	// TODO: #186
+}
