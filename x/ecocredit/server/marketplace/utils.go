45c4fcaa979bf51dfe59869fd07ebe3a746fc4b8
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-04-06 11:57:12 -0700

    refactor: isDenomAllowed only gets the AskDenom slice param

--- x/ecocredit/server/marketplace/utils.go
+++ x/ecocredit/server/marketplace/utils.go
@@ -9,9 +9,9 @@ import (
 
 // isDenomAllowed checks if the denom is allowed to be used in orders.
 func isDenomAllowed(ctx sdk.Context, denom string, pk ecocredit.ParamKeeper) bool {
-	var params core.Params
-	pk.GetParamSet(ctx, &params)
-	for _, askDenom := range params.AllowedAskDenoms {
+	var allowedDenoms []*core.AskDenom
+	pk.Get(ctx, core.KeyAllowedAskDenoms, allowedDenoms)
+	for _, askDenom := range allowedDenoms {
 		if askDenom.Denom == denom {
 			return true
 		}
