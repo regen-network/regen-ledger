1ff3f405aefefa16b8fce513efc68acb34593bca
Robert Zaremba <robert@zaremba.ch>
2022-04-07 01:36:20 +0200

    feat(math): adding Dec.SdkIntTrim

---
+++ types/math/dec_bench_test.go
@@ -0,0 +1,41 @@
+package math
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func BenchmarkSdkIntTrim(b *testing.B) {
+	s := "12345678901234567890.12345678901234567890"
+	d, err := NewDecFromString(s)
+	if err != nil {
+		b.Error("can't converet test number")
+	}
+
+	b.Run("exp", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			d.SdkIntTrim()
+		}
+	})
+
+	b.Run("quo-integer", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			naiveSdkIntTrim(d)
+		}
+	})
+
+}
+
+func naiveSdkIntTrim(d Dec) sdk.Int {
+	d, err := d.QuoInteger(NewDecFromInt64(1))
+	if err != nil {
+		panic(err)
+	}
+
+	i, err := d.BigInt()
+	if err != nil {
+		panic(err)
+	}
+	return sdk.NewIntFromBigInt(i)
+}
