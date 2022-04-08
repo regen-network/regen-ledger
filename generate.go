dffb4ff18c0f8fff9526359a06e0647fd0d95580
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2022-04-08 15:47:34 -0700

    remove duplicates

--- generate.go
+++ generate.go
@@ -1,8 +1,6 @@
 package regen_ledger
 
 //go:generate mockgen -source=x/data/expected_keepers.go -package mocks -destination x/data/mocks/expected_keepers.go
-//go:generate mockgen -source=x/ecocredit/server/core/keeper.go -package mocks -destination x/ecocredit/server/core/mocks/keeper.go
-//go:generate mockgen -source=x/ecocredit/expected_keepers.go -package mocks -destination x/ecocredit/mocks/expected_keepers.go
 //go:generate mockgen -source=x/ecocredit/expected_keepers.go -package mocks -destination x/ecocredit/mocks/expected_keepers.go
 //go:generate mockgen -source=x/ecocredit/server/basket/keeper.go -package mocks -destination x/ecocredit/server/basket/mocks/keeper.go
-//go:generate mockgen -source=x/ecocredit/expected_keepers.go -package mocks -destination x/ecocredit/mocks/expected_keepers.go
+//go:generate mockgen -source=x/ecocredit/server/core/keeper.go -package mocks -destination x/ecocredit/server/core/mocks/keeper.go
