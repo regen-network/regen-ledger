bf60137c8c1d3a8cfd266a0b3a7abaa270a093ee
Robert Zaremba <robert@zaremba.ch>
2022-04-08 22:47:39 +0200

    review comments

--- x/ecocredit/core/msg_mint_batch_credits_test.go
+++ x/ecocredit/core/msg_mint_batch_credits_test.go
@@ -42,5 +42,32 @@ func TestMsgMintBatchCredits(t *testing.T) {
 }
 
 func TestValidateOriginTx(t *testing.T) {
+	t.Parallel()
+	require := require.New(t)
+	tcs := []struct {
+		name string
+		err  string
+		o    OriginTx
+	}{
+		{"empty type", "origin_tx.typ must be",
+			OriginTx{}},
+		{"wrong type", "origin_tx.typ must be",
+			OriginTx{Typ: "*xxx"}},
+		{"empty tx", "origin_tx.id must be",
+			OriginTx{Typ: "Polygon"}},
+		{"wrong tx", "origin_tx.id must be",
+			OriginTx{Typ: "Polygon", Id: "---"}},
 
+		{"good1", "", OriginTx{Typ: "Polygon", Id: "0x123"}},
+		{"good2", "", OriginTx{Typ: "Ethereum", Id: "0x123"}},
+	}
+
+	for _, tc := range tcs {
+		err := validateOriginTx(&tc.o, true)
+		if tc.err == "" {
+			require.NoError(err, tc.name)
+		} else {
+			require.ErrorContains(err, tc.err, tc.name)
+		}
+	}
 }
