ffc3a5fc47deaa3d5107418a8666958ca2cf91a9
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-04-11 12:30:46 -0700

    feat: server impl for project update msgs

---
+++ x/ecocredit/server/core/update_project_metadata.go
@@ -0,0 +1,30 @@
+package core
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	"github.com/regen-network/regen-ledger/x/ecocredit/core"
+)
+
+func (k Keeper) UpdateProjectMetadata(ctx context.Context, req *core.MsgUpdateProjectMetadata) (*core.MsgUpdateProjectMetadata, error) {
+	admin, err := sdk.AccAddressFromBech32(req.Admin)
+	if err != nil {
+		return nil, err
+	}
+	project, err := k.stateStore.ProjectInfoTable().GetByName(ctx, req.ProjectId)
+	if err != nil {
+		return nil, sdkerrors.ErrInvalidRequest.Wrapf("could not get project with id %s: %s", req.ProjectId, err.Error())
+	}
+	if !sdk.AccAddress(project.Admin).Equals(admin) {
+		return nil, sdkerrors.ErrUnauthorized.Wrapf("%s is not the admin of project %s", req.Admin, req.ProjectId)
+	}
+	project.Metadata = req.NewMetadata
+	if err := k.stateStore.ProjectInfoTable().Update(ctx, project); err != nil {
+		return nil, err
+	}
+
+	return &core.MsgUpdateProjectMetadata{}, nil
+}
