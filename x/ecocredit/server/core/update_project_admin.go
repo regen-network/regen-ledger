5b02cdf4c8f63b5e3e5695e11680895a8a85cf73
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-04-11 12:38:48 -0700

    chore: add dummy server impl

---
+++ x/ecocredit/server/core/update_project_admin.go
@@ -0,0 +1,12 @@
+package core
+
+import (
+	"context"
+
+	"github.com/regen-network/regen-ledger/x/ecocredit/core"
+)
+
+func (k Keeper) UpdateProjectAdmin(ctx context.Context, admin *core.MsgUpdateProjectAdmin) (*core.MsgUpdateProjectAdmin, error) {
+	//TODO implement me
+	panic("implement me")
+}
