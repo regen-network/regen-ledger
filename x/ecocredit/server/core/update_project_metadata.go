0acc9b7eb0dfa95fc2877d79029d245b1cb0424a
Tyler <48813565+technicallyty@users.noreply.github.com>
2022-04-18 08:53:47 -0700

    feat(ecocredit/core): UpdateProjectAdmin/Metadata server impls (#1010)
    * feat: msg update project admin & metadata
    
    * chore: add dummy server impl
    
    * feat: server impl for project update msgs
    
    * fix: use response types
    
    * chore: fix types
    
    * chore: apply suggestions from code review
    
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    
    Co-authored-by: technicallyty <48813565+tytech3@users.noreply.github.com>
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>

--- x/ecocredit/server/core/update_project_metadata.go
+++ x/ecocredit/server/core/update_project_metadata.go
@@ -3,10 +3,29 @@ package core
 import (
 	"context"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
 	"github.com/regen-network/regen-ledger/x/ecocredit/core"
 )
 
+// UpdateProjectMetadata updates the project metadata.
 func (k Keeper) UpdateProjectMetadata(ctx context.Context, req *core.MsgUpdateProjectMetadata) (*core.MsgUpdateProjectMetadataResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	admin, err := sdk.AccAddressFromBech32(req.Admin)
+	if err != nil {
+		return nil, err
+	}
+	project, err := k.stateStore.ProjectInfoTable().GetByName(ctx, req.ProjectId)
+	if err != nil {
+		return nil, sdkerrors.ErrInvalidRequest.Wrapf("could not get project with id %s: %s", req.ProjectId, err.Error())
+	}
+	if !sdk.AccAddress(project.Admin).Equals(admin) {
+		return nil, sdkerrors.ErrUnauthorized.Wrapf("%s is not the admin of project %s", req.Admin, req.ProjectId)
+	}
+	project.Metadata = req.NewMetadata
+	if err := k.stateStore.ProjectInfoTable().Update(ctx, project); err != nil {
+		return nil, err
+	}
+
+	return &core.MsgUpdateProjectMetadataResponse{}, nil
 }
