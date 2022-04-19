fa42e6d04ed4d1482337cb9516ad28a50cd7948e
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-04-19 15:42:42 -0700

    chore: remove proposal cli method

--- x/ecocredit/client/tx.go
+++ x/ecocredit/client/tx.go
@@ -20,7 +20,6 @@ import (
 	"github.com/regen-network/regen-ledger/types"
 	"github.com/regen-network/regen-ledger/x/ecocredit"
 	basketcli "github.com/regen-network/regen-ledger/x/ecocredit/client/basket"
-	corecli "github.com/regen-network/regen-ledger/x/ecocredit/client/core"
 	marketplacecli "github.com/regen-network/regen-ledger/x/ecocredit/client/marketplace"
 	"github.com/regen-network/regen-ledger/x/ecocredit/core"
 )
@@ -46,7 +45,6 @@ func TxCmd(name string) *cobra.Command {
 		TxUpdateClassIssuersCmd(),
 		TxUpdateClassAdminCmd(),
 		TxCreateProject(),
-		corecli.TxCreditTypeProposalCmd(),
 		basketcli.TxCreateBasket(),
 		basketcli.TxPutInBasket(),
 		basketcli.TxTakeFromBasket(),
