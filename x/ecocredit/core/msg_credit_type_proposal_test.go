0ad686db36dc60b4fcfc9492372d8da43f5b345d
Tyler <48813565+technicallyty@users.noreply.github.com>
2022-04-21 16:07:06 -0700

    feat(x/ecocredit): credit type gov handler (#1015)
    * chore: gen api
    
    * wip: proposal handler
    
    * wip: wires
    
    * feat: final gov handler
    
    * chore: cleanup dead code
    
    * chore: cleanup code
    
    * chore: fix app error
    
    * chore: add dummy REST proposal handler
    
    * chore: regen mocks
    
    * chore: not invalid request
    
    * chore: clean test
    
    * refactor: move proposal handler code into server/core
    
    * chore: add tests for new types
    
    * refactor: use setCustomModuleBasics
    
    * fix: restore setCustomKeepers
    
    * Update x/ecocredit/server/expected_keepers.go
    
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    
    * chore: assert credit type name length
    
    * refactor: NewCreditType -> AddCreditType
    
    * Update app/stable_appconfig.go
    
    Co-authored-by: Aaron Craelius <aaron@regen.network>
    
    * chore: import alias
    
    * fix: remove duplicate credit type validate method
    
    * chore: remove proposal cli method
    
    * Revert "chore: remove proposal cli method"
    
    This reverts commit fa42e6d04ed4d1482337cb9516ad28a50cd7948e.
    
    * chore: revert cli removal
    
    * chore: update error type
    
    Co-authored-by: technicallyty <48813565+tytech3@users.noreply.github.com>
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    Co-authored-by: Aaron Craelius <aaron@regen.network>

---
+++ x/ecocredit/core/msg_credit_type_proposal_test.go
@@ -0,0 +1,54 @@
+package core
+
+import (
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestCreditTypeProposal_ValidateBasic(t *testing.T) {
+	validCreditType := &CreditType{"C", "carbon", "carbon ton", 6}
+	type fields struct {
+		Title       string
+		Description string
+		CreditType  *CreditType
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		errMsg string
+	}{
+		{
+			name: "valid",
+			fields: fields{
+				Title:       "hello",
+				Description: "world",
+				CreditType:  validCreditType,
+			},
+		},
+		{
+			name: "invalid: nil credit type",
+			fields: fields{
+				Title:       "hi",
+				Description: "hello",
+				CreditType:  nil,
+			},
+			errMsg: "credit type cannot be nil",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &CreditTypeProposal{
+				Title:       tt.fields.Title,
+				Description: tt.fields.Description,
+				CreditType:  tt.fields.CreditType,
+			}
+			err := m.ValidateBasic()
+			if len(tt.errMsg) != 0 {
+				assert.ErrorContains(t, err, tt.errMsg)
+			} else {
+				assert.NilError(t, err)
+			}
+		})
+	}
+}
