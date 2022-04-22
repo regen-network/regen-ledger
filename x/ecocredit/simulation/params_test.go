bdbaacdd4acc3360a67c2febf9dd3d1c5784ebdc
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-04-21 21:00:03 -0700

    fix: faililng sim test

--- x/ecocredit/simulation/params_test.go
+++ x/ecocredit/simulation/params_test.go
@@ -1,34 +1,17 @@
 package simulation_test
 
 import (
-	"encoding/json"
 	"math/rand"
 	"testing"
 
-	"github.com/regen-network/regen-ledger/x/ecocredit/core"
-	"github.com/regen-network/regen-ledger/x/ecocredit/simulation"
 	"github.com/stretchr/testify/require"
+
+	"github.com/regen-network/regen-ledger/x/ecocredit/simulation"
 )
 
 func TestParamChanges(t *testing.T) {
 	s := rand.NewSource(1)
 	r := rand.New(s)
-
-	bz, err := json.Marshal([]*core.CreditType{
-		{
-			Abbreviation: "C",
-			Name:         "carbon",
-			Unit:         "metric ton CO2 equivalent",
-			Precision:    6,
-		},
-		{
-			Abbreviation: "BIO",
-			Name:         "biodiversity",
-			Unit:         "ton",
-			Precision:    6,
-		}},
-	)
-	require.NoError(t, err)
 
 	expected := []struct {
 		composedKey string
@@ -39,12 +22,11 @@ func TestParamChanges(t *testing.T) {
 		{"ecocredit/CreditClassFee", "CreditClassFee", "[{\"denom\":\"stake\",\"amount\":\"6\"}]", "ecocredit"},
 		{"ecocredit/AllowlistEnabled", "AllowlistEnabled", "true", "ecocredit"},
 		{"ecocredit/AllowedClassCreators", "AllowedClassCreators", "[\"cosmos10z82e5ztmrm4pujgummvmr7aqjzwlp6ga9ams9\"]", "ecocredit"},
-		{"ecocredit/CreditTypes", "CreditTypes", string(bz), "ecocredit"},
 	}
 
 	paramChanges := simulation.ParamChanges(r)
 
-	require.Len(t, paramChanges, 4)
+	require.Len(t, paramChanges, 3)
 
 	for i, p := range paramChanges {
 		require.Equal(t, expected[i].composedKey, p.ComposedKey())
