6b8088de6ad77732ee332b0e8fac90a80fad2465
Robert Zaremba <robert@zaremba.ch>
2022-04-23 16:24:48 +0200

    implement Msg RecordBridgeEvent

--- x/axelarbridge/server/msg.go
+++ x/axelarbridge/server/msg.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 
+	api "github.com/regen-network/regen-ledger/api/axelar/bridge/v1"
 	"github.com/regen-network/regen-ledger/x/axelarbridge"
 )
 
@@ -11,7 +12,16 @@ func (s serverImpl) ExecBridgeEvent(ctx context.Context, req *axelarbridge.MsgEx
 }
 
 func (s serverImpl) RecordBridgeEvent(ctx context.Context, req *axelarbridge.MsgRecordBridgeEvent) (*axelarbridge.MsgRecordBridgeEventResponse, error) {
-	panic("todo")
+	id, err := s.stateStore.EventTable().InsertReturningID(ctx, &api.Event{
+		SenderAddress: req.SenderAddress,
+		DestAddress:   req.DestAddress,
+		Payload:       req.Payload,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &axelarbridge.MsgRecordBridgeEventResponse{id}, nil
+
 }
 
 func (s serverImpl) SendBridgeEvent(ctx context.Context, req *axelarbridge.MsgSendBridgeEvent) (*axelarbridge.MsgSendBridgeEventResponse, error) {
