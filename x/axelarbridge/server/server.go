4e6547403209199301c6e7224c90245085d4c76d
Robert Zaremba <robert@zaremba.ch>
2022-04-23 14:48:56 +0200

    adding orm table

--- x/axelarbridge/server/server.go
+++ x/axelarbridge/server/server.go
@@ -6,7 +6,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/orm/model/ormdb"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	// api "github.com/regen-network/regen-ledger/api/axelarbridge/v1"
+	api "github.com/regen-network/regen-ledger/api/axelarbridge/v1"
 	servermodule "github.com/regen-network/regen-ledger/types/module/server"
 	"github.com/regen-network/regen-ledger/types/ormstore"
 	"github.com/regen-network/regen-ledger/x/axelarbridge"
@@ -18,12 +18,12 @@ const (
 
 var _ axelarbridge.MsgServer = serverImpl{}
 
-// var ModuleSchema = ormv1alpha1.ModuleSchemaDescriptor{
-// 	SchemaFile: []*ormv1alpha1.ModuleSchemaDescriptor_FileEntry{
-// 		{Id: 1, ProtoFileName: api.File_regen_bridge_v1_state_proto.Path(), StorageType: ormv1alpha1.StorageType_STORAGE_TYPE_DEFAULT_UNSPECIFIED},
-// 	},
-// 	Prefix: []byte{ORMPrefix},
-// }
+var ModuleSchema = ormv1alpha1.ModuleSchemaDescriptor{
+	SchemaFile: []*ormv1alpha1.ModuleSchemaDescriptor_FileEntry{
+		{Id: 1, ProtoFileName: api.File_regen_bridge_v1_state_proto.Path(), StorageType: ormv1alpha1.StorageType_STORAGE_TYPE_DEFAULT_UNSPECIFIED},
+	},
+	Prefix: []byte{ORMPrefix},
+}
 
 type serverImpl struct {
 	storeKey   sdk.StoreKey
@@ -33,10 +33,10 @@ type serverImpl struct {
 }
 
 func newServer(storeKey sdk.StoreKey, router *baseapp.MsgServiceRouter) serverImpl {
-	// db, err := ormstore.NewStoreKeyDB(&ModuleSchema, storeKey, ormdb.ModuleDBOptions{})
-	// if err != nil {
-	// 	panic(err)
-	// }
+	db, err := ormstore.NewStoreKeyDB(&ModuleSchema, storeKey, ormdb.ModuleDBOptions{})
+	if err != nil {
+		panic(err)
+	}
 
 	stateStore, err := api.NewStateStore(db)
 	if err != nil {
