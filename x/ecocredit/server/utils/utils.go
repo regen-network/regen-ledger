0db5bff16fe200a09dd129462ed07870a5fe4a81
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-04-26 13:49:45 -0700

    chore: cleaup GetBalance godoc comment

--- x/ecocredit/server/utils/utils.go
+++ x/ecocredit/server/utils/utils.go
@@ -36,9 +36,9 @@ func GetNonNegativeFixedDecs(precision uint32, decimals ...string) ([]math.Dec, 
 	return decs, nil
 }
 
-// GetBalance gets the balance from the account, returning a zero value balance if no balance is found.
-// NOTE: the default value is not inserted into the balance table in the `not found` case. Calling Update when a default
-// value is returned will cause an error. Save should be used when dealing with balances from this function.
+// GetBalance gets the balance from the account, returning a default, zero value balance if no balance is found.
+// NOTE: the default value is not inserted into the balance table in the `not found` case. Calling Update when the default
+// value is returned will cause an error. The `Save` method should be used when dealing with balances from this function.
 func GetBalance(ctx context.Context, table api.BatchBalanceTable, addr types.AccAddress, key uint64) (*api.BatchBalance, error) {
 	bal, err := table.Get(ctx, addr, key)
 	if err != nil {
