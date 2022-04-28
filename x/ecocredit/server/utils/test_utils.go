de3855d3235b1c8c9cf15db7c38e2a01d8072ceb
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-04-28 14:33:09 -0700

    chore: any -> interface{}

--- x/ecocredit/server/utils/test_utils.go
+++ x/ecocredit/server/utils/test_utils.go
@@ -21,22 +21,22 @@ func ExpectParamGet(obj interface{}, paramKeeper *mocks.MockParamKeeper, times i
 	switch obj.(type) {
 	case *[]string:
 		s := obj.(*[]string)
-		paramKeeper.EXPECT().Get(gmAny, gmAny, gmAny).Do(func(_, _ any, param *[]string) {
+		paramKeeper.EXPECT().Get(gmAny, gmAny, gmAny).Do(func(_, _ interface{}, param *[]string) {
 			*param = *s
 		}).Times(times)
 	case *sdk.Coins:
 		coins := obj.(*sdk.Coins)
-		paramKeeper.EXPECT().Get(gmAny, gmAny, gmAny).Do(func(_, _ any, param *sdk.Coins) {
+		paramKeeper.EXPECT().Get(gmAny, gmAny, gmAny).Do(func(_, _ interface{}, param *sdk.Coins) {
 			*param = *coins
 		}).Times(times)
 	case *bool:
 		b := obj.(*bool)
-		paramKeeper.EXPECT().Get(gmAny, gmAny, gmAny).Do(func(_, _ any, param *bool) {
+		paramKeeper.EXPECT().Get(gmAny, gmAny, gmAny).Do(func(_, _ interface{}, param *bool) {
 			*param = *b
 		}).Times(times)
 	case *[]*core.AskDenom:
 		askDenoms := obj.(*[]*core.AskDenom)
-		paramKeeper.EXPECT().Get(gmAny, gmAny, gmAny).Do(func(_, _ any, param *[]*core.AskDenom) {
+		paramKeeper.EXPECT().Get(gmAny, gmAny, gmAny).Do(func(_, _ interface{}, param *[]*core.AskDenom) {
 			*param = *askDenoms
 		}).Times(times)
 	}
