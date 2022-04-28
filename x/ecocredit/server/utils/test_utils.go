aee2ac0d56547203ba2c1f2b0e50d5c0bec2dcc0
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-04-27 17:35:53 -0700

    chore: cleanup func

--- x/ecocredit/server/utils/test_utils.go
+++ x/ecocredit/server/utils/test_utils.go
@@ -9,7 +9,7 @@ import (
 // ExpectParamGet is a helper function that sets up an expected mock call for the provided type.
 func ExpectParamGet[T any](obj *T, paramKeeper *mocks.MockParamKeeper, times int) {
 	gmAny := gomock.Any()
-	paramKeeper.EXPECT().Get(gmAny, gmAny, gmAny).Do(func(a, b interface{}, param *T) {
+	paramKeeper.EXPECT().Get(gmAny, gmAny, gmAny).Do(func(_, _ any, param *T) {
 		*param = *obj
 	}).Times(times)
 }
