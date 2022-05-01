794d05e51231fad42b511e9516b3cf46445c6f6e
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-04-30 18:05:16 -0700

    chore: specify coins in EXPECT calls

--- x/ecocredit/server/marketplace/buy_direct_test.go
+++ x/ecocredit/server/marketplace/buy_direct_test.go
@@ -254,19 +254,22 @@ func TestBuyDirect_MultipleOrders(t *testing.T) {
 	s.testSellSetup(batchDenom, ask.Denom, ask.Denom[1:], classId, start, end, creditType)
 	// make a sell order
 	sellExp := time.Now()
+
+	purchaseAmt1 := "12.3531"
+	purchaseAmt2 := "15.39201"
 	sellOrderIds := s.createSellOrder(&marketplace.MsgSell{
 		Owner: s.addr.String(),
 		Orders: []*marketplace.MsgSell_Order{
 			{
 				BatchDenom:        batchDenom,
-				Quantity:          "12.3531",
+				Quantity:          purchaseAmt1,
 				AskPrice:          &ask,
 				DisableAutoRetire: true,
 				Expiration:        &sellExp,
 			},
 			{
 				BatchDenom:        batchDenom,
-				Quantity:          "15.39201",
+				Quantity:          purchaseAmt2,
 				AskPrice:          &ask,
 				DisableAutoRetire: false,
 				Expiration:        &sellExp,
@@ -278,15 +281,19 @@ func TestBuyDirect_MultipleOrders(t *testing.T) {
 	assert.NilError(s.t, err)
 	balBefore, supBefore := s.getBalanceAndSupply(batch.Key, buyerAddr)
 
-	purchaseAmt1 := "12.3531"
-	purchaseAmt2 := "15.39201"
-
 	orders := []*marketplace.MsgBuyDirect_Order{
 		{sellOrderIds[0], purchaseAmt1, &ask, true, ""},
 		{sellOrderIds[1], purchaseAmt2, &ask, false, ""},
 	}
-	s.bankKeeper.EXPECT().GetBalance(gmAny, gmAny, gmAny).Return(userCoinBalance).Times(len(orders))
-	s.bankKeeper.EXPECT().SendCoins(gmAny, gmAny, gmAny, gmAny).Return(nil).Times(len(orders))
+
+	s.bankKeeper.EXPECT().GetBalance(gmAny, buyerAddr, ask.Denom).Return(userCoinBalance).Times(2)
+
+	// first order is 12.23531 * 10ufoo = 123ufoo
+	cost := sdk.Coins{sdk.NewInt64Coin(ask.Denom, 123)}
+	s.bankKeeper.EXPECT().SendCoins(gmAny, buyerAddr, s.addr, cost).Return(nil).Times(1)
+	// second order is 15.39201 * 10ufoo = 153ufoo
+	cost2 := sdk.Coins{sdk.NewInt64Coin(ask.Denom, 153)}
+	s.bankKeeper.EXPECT().SendCoins(gmAny, buyerAddr, s.addr, cost2).Return(nil).Times(1)
 	_, err = s.k.BuyDirect(s.ctx, &marketplace.MsgBuyDirect{
 		Buyer:  buyerAddr.String(),
 		Orders: orders,
