962ab9ccd64e4701be2065dc68f676acf463bacf
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-05-02 10:10:04 -0700

    chore: fix param changes test

--- x/ecocredit/simulation/params_test.go
+++ x/ecocredit/simulation/params_test.go
@@ -1,28 +1,17 @@
 package simulation_test
 
 import (
-	"encoding/json"
 	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/regen-network/regen-ledger/x/ecocredit/core"
 	"github.com/regen-network/regen-ledger/x/ecocredit/simulation"
 )
 
 func TestParamChanges(t *testing.T) {
 	s := rand.NewSource(1)
 	r := rand.New(s)
-
-	bz1, err := json.Marshal([]*core.AskDenom{
-		{
-			Denom:        "stake",
-			DisplayDenom: "stake",
-			Exponent:     18,
-		},
-	})
-	require.NoError(t, err)
 
 	expected := []struct {
 		composedKey string
@@ -33,11 +22,10 @@ func TestParamChanges(t *testing.T) {
 		{"ecocredit/CreditClassFee", "CreditClassFee", "[{\"denom\":\"stake\",\"amount\":\"6\"}]", "ecocredit"},
 		{"ecocredit/AllowlistEnabled", "AllowlistEnabled", "true", "ecocredit"},
 		{"ecocredit/AllowedClassCreators", "AllowedClassCreators", "[\"cosmos10z82e5ztmrm4pujgummvmr7aqjzwlp6ga9ams9\"]", "ecocredit"},
-		{"ecocredit/AllowedAskDenoms", "AllowedAskDenoms", string(bz1), "ecocredit"},
 	}
 
 	paramChanges := simulation.ParamChanges()
-	require.Len(t, paramChanges, 4)
+	require.Len(t, paramChanges, 3)
 
 	for i, p := range paramChanges {
 		require.Equal(t, expected[i].composedKey, p.ComposedKey())
