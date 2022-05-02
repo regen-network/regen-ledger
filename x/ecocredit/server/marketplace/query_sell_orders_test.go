4ecc7937a5490ef708ba7a6ec69451bbb5cefbc3
Tyler <48813565+technicallyty@users.noreply.github.com>
2022-05-02 10:23:50 -0700

    fix(ecocredit/marketplace): correctly set supply during BuyDirect  (#1069)
    * fix: supply corruption, tests
    
    * chore: cleanup tests
    
    * chore: remove gomock.Any shadowing
    
    * chore: godoc
    
    * chore: remove pointless util
    
    * revert: market -> marketplace
    
    * chore: apply suggestions from code review
    
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    
    * chore: specify coins in EXPECT calls
    
    Co-authored-by: technicallyty <48813565+tytech3@users.noreply.github.com>
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>

--- x/ecocredit/server/marketplace/query_sell_orders_test.go
+++ x/ecocredit/server/marketplace/query_sell_orders_test.go
@@ -30,7 +30,7 @@ var (
 func TestSellOrders(t *testing.T) {
 	t.Parallel()
 	s := setupBase(t)
-	testSellSetup(t, s, batchDenom, ask.Denom, ask.Denom[1:], classId, start, end, creditType)
+	s.testSellSetup(batchDenom, ask.Denom, ask.Denom[1:], classId, start, end, creditType)
 	_, _, addr2 := testdata.KeyTestPubAddr()
 
 	order1 := insertSellOrder(t, s, s.addr, 1)
@@ -52,7 +52,7 @@ func TestSellOrders(t *testing.T) {
 func TestSellOrdersByDenom(t *testing.T) {
 	t.Parallel()
 	s := setupBase(t)
-	testSellSetup(t, s, batchDenom, ask.Denom, ask.Denom[1:], classId, start, end, creditType)
+	s.testSellSetup(batchDenom, ask.Denom, ask.Denom[1:], classId, start, end, creditType)
 
 	// make another batch
 	otherDenom := "C01-19990101-20290101-001"
@@ -98,7 +98,7 @@ func TestSellOrdersByDenom(t *testing.T) {
 func TestSellOrdersByAddress(t *testing.T) {
 	t.Parallel()
 	s := setupBase(t)
-	testSellSetup(t, s, batchDenom, ask.Denom, ask.Denom[1:], classId, start, end, creditType)
+	s.testSellSetup(batchDenom, ask.Denom, ask.Denom[1:], classId, start, end, creditType)
 
 	_, _, otherAddr := testdata.KeyTestPubAddr()
 	_, _, noOrdersAddr := testdata.KeyTestPubAddr()
