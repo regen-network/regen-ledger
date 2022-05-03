30a5a185be9c1e455f5ed17f9bb6f5c3769df35b
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2022-05-03 13:50:39 -0700

    docs: install regen using pre-built packages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -92,8 +92,8 @@ module.exports = {
           collapsable: false,
           children: [
             '/ledger/get-started/',
+            '/ledger/get-started/local-testnet',
             '/ledger/get-started/live-networks',
-            '/ledger/get-started/prerequisites',
           ],
         },
         {
@@ -164,9 +164,11 @@ module.exports = {
           title: 'Get Started',
           collapsable: false,
           children: [
+            '/validators/get-started/',
+            '/validators/get-started/install-regen',
+            '/validators/get-started/install-cosmovisor',
             '/validators/get-started/run-a-full-node',
             '/validators/get-started/create-a-validator',
-            '/validators/get-started/prerequisites',
           ]
         },
         {
