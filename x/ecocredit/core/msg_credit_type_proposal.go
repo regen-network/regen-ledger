bdf6eb1166b8ae9f2a8eb15f3cf969e035fefa1e
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-05-04 14:06:33 -0700

    chore: remove get signers

--- x/ecocredit/core/msg_credit_type_proposal.go
+++ x/ecocredit/core/msg_credit_type_proposal.go
@@ -3,18 +3,11 @@ package core
 import (
 	"fmt"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 
 	"github.com/regen-network/regen-ledger/x/ecocredit"
 )
-
-var _ sdk.Msg = &CreditTypeProposal{}
-
-func (m *CreditTypeProposal) GetSigners() []sdk.AccAddress {
-	return nil
-}
 
 var _ govtypes.Content = &CreditTypeProposal{}
 
