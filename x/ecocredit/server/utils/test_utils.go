a6fe23cd60bffafffbedad644e227c5fe4aac413
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-05-04 12:48:02 -0700

    chore: add context to Do function

--- x/ecocredit/server/utils/test_utils.go
+++ x/ecocredit/server/utils/test_utils.go
@@ -21,22 +21,26 @@ func ExpectParamGet(obj interface{}, paramKeeper *mocks.MockParamKeeper, key []b
 	switch obj.(type) {
 	case *[]string:
 		s := obj.(*[]string)
-		paramKeeper.EXPECT().Get(gmAny, key, gmAny).Do(func(_, _ interface{}, param *[]string) {
+		var expected []string
+		paramKeeper.EXPECT().Get(gmAny, key, &expected).Do(func(_ sdk.Context, _ []byte, param *[]string) {
 			*param = *s
 		}).Times(times)
 	case *sdk.Coins:
 		coins := obj.(*sdk.Coins)
-		paramKeeper.EXPECT().Get(gmAny, key, gmAny).Do(func(_, _ interface{}, param *sdk.Coins) {
+		var expected sdk.Coins
+		paramKeeper.EXPECT().Get(gmAny, key, &expected).Do(func(_ sdk.Context, _ []byte, param *sdk.Coins) {
 			*param = *coins
 		}).Times(times)
 	case *bool:
 		b := obj.(*bool)
-		paramKeeper.EXPECT().Get(gmAny, key, gmAny).Do(func(_, _ interface{}, param *bool) {
+		var expected bool
+		paramKeeper.EXPECT().Get(gmAny, key, &expected).Do(func(_ sdk.Context, _ []byte, param *bool) {
 			*param = *b
 		}).Times(times)
 	case *[]*core.AskDenom:
 		askDenoms := obj.(*[]*core.AskDenom)
-		paramKeeper.EXPECT().Get(gmAny, key, gmAny).Do(func(_, _ interface{}, param *[]*core.AskDenom) {
+		var expected []*core.AskDenom
+		paramKeeper.EXPECT().Get(gmAny, key, &expected).Do(func(_ sdk.Context, _ []byte, param *[]*core.AskDenom) {
 			*param = *askDenoms
 		}).Times(times)
 	}
