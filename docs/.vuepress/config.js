2dfdb663e573107e41e16c6e2d8fbb5f79419eda
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2022-05-06 10:51:32 -0700

    remove clean urls plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -253,11 +253,6 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
-    'vuepress-plugin-clean-urls',
-      {
-        normalSuffix: '/',
-        indexSuffix: '/',
-      },
     ],
   markdown: {
     extendMarkdown: md => {
