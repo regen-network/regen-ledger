d13992286c684d12d7032089331ed7d8e4ef7d82
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2022-05-23 18:25:55 -0700

    add credit type to sell setup

--- x/ecocredit/server/marketplace/keeper_test.go
+++ x/ecocredit/server/marketplace/keeper_test.go
@@ -175,6 +175,13 @@ func assertCreditsEscrowed(t *testing.T, balanceBefore, balanceAfter *ecoApi.Bat
 
 // testSellSetup sets up a batch, class, market, and issues a balance of 100 retired and tradable to the base suite's addr.
 func (s *baseSuite) testSellSetup(batchDenom, bankDenom, displayDenom, classId string, start, end *timestamppb.Timestamp, creditType core.CreditType) {
+	assert.NilError(s.t, s.coreStore.CreditTypeTable().Insert(s.ctx, &ecoApi.CreditType{
+		Abbreviation: "C",
+		Name:         "carbon",
+		Unit:         "metric ton C02",
+		Precision:    6,
+	}))
+
 	assert.NilError(s.t, s.coreStore.ClassTable().Insert(s.ctx, &ecoApi.Class{
 		Id:               classId,
 		Admin:            s.addr,
