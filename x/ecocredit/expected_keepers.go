eb840ba33e26b14878d24c183cf7966da460ee4b
Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
2022-06-08 09:46:47 -0700

    refactor(x/ecocredit): burn basket fee and remove dist keeper (#1167)
    * refactor(x/ecocredit): burn basket fee and remove dist keeper
    
    * refactor(x/ecocredit): burn basket fee and remove dist keeper

--- x/ecocredit/expected_keepers.go
+++ x/ecocredit/expected_keepers.go
@@ -37,10 +37,6 @@ type BankKeeper interface {
 	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
 }
 
-type DistributionKeeper interface {
-	FundCommunityPool(ctx sdk.Context, amount sdk.Coins, sender sdk.AccAddress) error
-}
-
 type ParamKeeper interface {
 
 	// Get fetches a parameter by key from the Subspace's KVStore and sets the provided pointer to the fetched value.
