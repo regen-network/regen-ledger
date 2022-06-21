ce2c6aa5cebf6b34360414278531ea17e9161ec1
aleem1314 <aleem@vitwit.com>
2022-06-21 15:45:51 +0530

    wip

--- x/data/simulation/genesis.go
+++ x/data/simulation/genesis.go
@@ -1,8 +1,56 @@
 package simulation
 
-import "github.com/cosmos/cosmos-sdk/types/module"
+import (
+	"github.com/cosmos/cosmos-sdk/orm/model/ormdb"
+	"github.com/cosmos/cosmos-sdk/orm/model/ormtable"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	dbm "github.com/tendermint/tm-db"
+
+	api "github.com/regen-network/regen-ledger/api/regen/data/v1"
+	"github.com/regen-network/regen-ledger/x/data/server"
+)
 
 // RandomizedGenState generates a random GenesisState for the data module.
 func RandomizedGenState(simState *module.SimulationState) {
-	// TODO: #186
+
+	db := dbm.NewMemDB()
+	backend := ormtable.NewBackend(ormtable.BackendOptions{
+		CommitmentStore: db,
+		IndexStore:      db,
+	})
+
+	ormdb, err := ormdb.NewModuleDB(&server.ModuleSchema, ormdb.ModuleDBOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	ormCtx := ormtable.WrapContextDefault(backend)
+	ss, err := api.NewStateStore(ormdb)
+	if err != nil {
+		panic(err)
+	}
+
+	manager := simState.Accounts[0]
+
+	resolverId, err := ss.ResolverTable().InsertReturningID(ormCtx, &api.Resolver{
+		Url:     "https://foo.bar",
+		Manager: manager.Address,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	err = ss.DataResolverTable().Insert(ormCtx, &api.DataResolver{
+		ResolverId: resolverId,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	ss.DataIDTable().Insert(ormCtx, &api.DataID{})
+
+	ss.DataAnchorTable().Insert(ormCtx, &api.DataAnchor{})
+
+	ss.DataAttestorTable().Insert(ormCtx, &api.DataAttestor{})
+
 }
