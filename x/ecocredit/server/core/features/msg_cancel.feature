6ebfcad552fca91129b681d59782b1521c9d4568
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2022-07-05 12:12:13 -0700

    add cancel and retire scenarios

--- x/ecocredit/server/core/features/msg_cancel.feature
+++ x/ecocredit/server/core/features/msg_cancel.feature
@@ -4,3 +4,52 @@ Feature: Msg/Cancel
   - the owner credit balance is updated
   - the batch supply is updated
   - ...
+
+  Rule: The owner balance is updated
+
+    Scenario: the owner balance is updated
+      Given a credit batch exists
+      And alice has the batch balance
+      """
+      {
+        "retired_amount": "0",
+        "tradable_amount": "10",
+        "escrowed_amount": "0"
+      }
+      """
+      When alice attempts to cancel credit amount "10"
+      Then expect alice batch balance
+      """
+      {
+        "retired_amount": "0",
+        "tradable_amount": "0",
+        "escrowed_amount": "0"
+      }
+      """
+
+    # no failing scenario - state transitions only occur upon successful message execution
+
+  Rule: The batch supply is updated
+
+    Scenario: the batch supply is updated
+      Given a credit batch exists
+      And alice owns tradable credit amount "10"
+      And the batch supply
+      """
+      {
+        "retired_amount": "0",
+        "tradable_amount": "10",
+        "cancelled_amount": "0"
+      }
+      """
+      When alice attempts to cancel credit amount "10"
+      Then expect batch supply
+      """
+      {
+        "retired_amount": "0",
+        "tradable_amount": "0",
+        "cancelled_amount": "10"
+      }
+      """
+
+    # no failing scenario - state transitions only occur upon successful message execution
--- x/ecocredit/server/core/msg_cancel_test.go
+++ x/ecocredit/server/core/msg_cancel_test.go
@@ -3,18 +3,28 @@ package core
 import (
 	"testing"
 
+	"github.com/gogo/protobuf/jsonpb"
 	"github.com/regen-network/gocuke"
+	"github.com/stretchr/testify/require"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	api "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1"
 	"github.com/regen-network/regen-ledger/x/ecocredit/core"
 )
 
 type cancel struct {
 	*baseSuite
-	alice sdk.AccAddress
-	res   *core.MsgCancelResponse
-	err   error
+	alice            sdk.AccAddress
+	creditTypeAbbrev string
+	classId          string
+	classKey         uint64
+	projectId        string
+	batchDenom       string
+	batchKey         uint64
+	tradableAmount   string
+	res              *core.MsgCancelResponse
+	err              error
 }
 
 func TestCancel(t *testing.T) {
@@ -24,4 +34,122 @@ func TestCancel(t *testing.T) {
 func (s *cancel) Before(t gocuke.TestingT) {
 	s.baseSuite = setupBase(t)
 	s.alice = s.addr
+	s.creditTypeAbbrev = "C"
+	s.classId = "C01"
+	s.projectId = "C01-001"
+	s.batchDenom = "C01-001-20200101-20210101-001"
+	s.tradableAmount = "10"
+}
+
+func (s *cancel) ACreditBatchExists() {
+	s.creditBatchSetup()
+}
+
+func (s *cancel) AliceHasTheBatchBalance(a gocuke.DocString) {
+	balance := &api.BatchBalance{}
+	err := jsonpb.UnmarshalString(a.Content, balance)
+	require.NoError(s.t, err)
+
+	balance.BatchKey = s.batchKey
+	balance.Address = s.alice
+
+	// Save because the balance may already exist from setup
+	err = s.stateStore.BatchBalanceTable().Save(s.ctx, balance)
+	require.NoError(s.t, err)
+}
+
+func (s *cancel) AliceOwnsTradableCreditAmount(a string) {
+	err := s.k.stateStore.BatchBalanceTable().Insert(s.ctx, &api.BatchBalance{
+		BatchKey:       s.batchKey,
+		Address:        s.alice,
+		TradableAmount: a,
+	})
+	require.NoError(s.t, err)
+}
+
+func (s *cancel) TheBatchSupply(a gocuke.DocString) {
+	supply := &api.BatchSupply{}
+	err := jsonpb.UnmarshalString(a.Content, supply)
+	require.NoError(s.t, err)
+
+	supply.BatchKey = s.batchKey
+
+	// Save because the supply may already exist from setup
+	err = s.stateStore.BatchSupplyTable().Save(s.ctx, supply)
+	require.NoError(s.t, err)
+}
+
+func (s *cancel) AliceAttemptsToCancelCreditAmount(a string) {
+	s.res, s.err = s.k.Cancel(s.ctx, &core.MsgCancel{
+		Owner: s.alice.String(),
+		Credits: []*core.Credits{
+			{
+				BatchDenom: s.batchDenom,
+				Amount:     a,
+			},
+		},
+	})
+}
+
+func (s *cancel) ExpectAliceBatchBalance(a gocuke.DocString) {
+	expected := &api.BatchBalance{}
+	err := jsonpb.UnmarshalString(a.Content, expected)
+	require.NoError(s.t, err)
+
+	balance, err := s.stateStore.BatchBalanceTable().Get(s.ctx, s.alice, s.batchKey)
+	require.NoError(s.t, err)
+
+	require.Equal(s.t, expected.RetiredAmount, balance.RetiredAmount)
+	require.Equal(s.t, expected.TradableAmount, balance.TradableAmount)
+	require.Equal(s.t, expected.EscrowedAmount, balance.EscrowedAmount)
+}
+
+func (s *cancel) ExpectBatchSupply(a gocuke.DocString) {
+	expected := &api.BatchSupply{}
+	err := jsonpb.UnmarshalString(a.Content, expected)
+	require.NoError(s.t, err)
+
+	balance, err := s.stateStore.BatchSupplyTable().Get(s.ctx, s.batchKey)
+	require.NoError(s.t, err)
+
+	require.Equal(s.t, expected.RetiredAmount, balance.RetiredAmount)
+	require.Equal(s.t, expected.TradableAmount, balance.TradableAmount)
+}
+
+func (s *cancel) creditBatchSetup() {
+	// TODO: Save for now but credit type should not exist prior to unit test #893
+	err := s.k.stateStore.CreditTypeTable().Save(s.ctx, &api.CreditType{
+		Abbreviation: s.creditTypeAbbrev,
+	})
+	require.NoError(s.t, err)
+
+	cKey, err := s.k.stateStore.ClassTable().InsertReturningID(s.ctx, &api.Class{
+		Id:               s.classId,
+		CreditTypeAbbrev: s.creditTypeAbbrev,
+	})
+	require.NoError(s.t, err)
+
+	s.classKey = cKey
+
+	pKey, err := s.k.stateStore.ProjectTable().InsertReturningID(s.ctx, &api.Project{
+		Id:       s.projectId,
+		ClassKey: cKey,
+	})
+	require.NoError(s.t, err)
+
+	bKey, err := s.k.stateStore.BatchTable().InsertReturningID(s.ctx, &api.Batch{
+		ProjectKey: pKey,
+		Denom:      s.batchDenom,
+	})
+	require.NoError(s.t, err)
+
+	err = s.k.stateStore.BatchSupplyTable().Insert(s.ctx, &api.BatchSupply{
+		BatchKey:        bKey,
+		TradableAmount:  s.tradableAmount,
+		RetiredAmount:   "0",
+		CancelledAmount: "0",
+	})
+	require.NoError(s.t, err)
+
+	s.batchKey = bKey
 }
