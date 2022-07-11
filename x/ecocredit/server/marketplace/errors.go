df09d1bb12e88f3287b129b2c8005a69f2ef452c
aleem1314 <aleem@vitwit.com>
2022-07-11 10:18:55 +0530

    chore: fix marketplace error code

--- x/ecocredit/server/marketplace/errors.go
+++ x/ecocredit/server/marketplace/errors.go
@@ -5,5 +5,5 @@ import "github.com/cosmos/cosmos-sdk/types/errors"
 const marketCodespace = "marketplace"
 
 var (
-	ErrBidTooLow = errors.Register(marketCodespace, 0, "bid price too low")
+	ErrBidTooLow = errors.Register(marketCodespace, 2, "bid price too low")
 )
