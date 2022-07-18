3aed97c36e081f3d27775880e491e8ceb3ff6b23
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2022-07-18 14:50:24 -0700

    docs: auto-generate module feature documentation

--- scripts/generate_feature_docs.sh
+++ scripts/generate_feature_docs.sh
@@ -2,15 +2,13 @@
 
 set -eo pipefail
 
-export PATH=~/go/bin:$PATH
-
 go install github.com/raviqqe/gherkin2markdown@latest
 
 echo "Generating data feature docs..."
 
-data_path="modules/data/features"
-data_server_dir="docs/$data_path/server"
-data_types_dir="docs/$data_path/types"
+data_dir="docs/modules/data/features"
+data_server_dir="$data_dir/server"
+data_types_dir="$data_dir/types"
 
 mkdir -p $data_server_dir
 mkdir -p $data_types_dir
@@ -29,25 +27,25 @@ for file in $(find ./x/data -path -prune -o -name '*.feature'); do
       data_readme+="\n## Server\n\n"
     fi
     data_server_count+=1
-    data_readme+="- [$name]($data_path/server/$name.html)\n"
-    gherkin2markdown "$file" > "$data_server_dir/$name.md"
+    data_readme+="- [$name](./server/$name.html)\n"
+    "$GOBIN/gherkin2markdown" "$file" > "$data_server_dir/$name.md"
   else
     if [[ $data_types_count == 0 ]]; then
       data_readme+="## Types\n\n"
     fi
     data_types_count+=1
-    data_readme+="- [$name]($data_path/types/$name.html)\n"
-    gherkin2markdown "$file" > "$data_types_dir/$name.md"
+    data_readme+="- [$name](./types/$name.html)\n"
+    "$GOBIN/gherkin2markdown" "$file" > "$data_types_dir/$name.md"
   fi
 done
 
-echo -e "$data_readme" > "docs/$data_path/README.md"
+echo -e "$data_readme" > "$data_dir/README.md"
 
 echo "Generating ecocredit feature docs..."
 
-eco_path="modules/ecocredit/features"
-eco_server_dir="docs/$eco_path/server"
-eco_types_dir="docs/$eco_path/types"
+eco_dir="docs/modules/ecocredit/features"
+eco_server_dir="$eco_dir/server"
+eco_types_dir="$eco_dir/types"
 
 mkdir -p $eco_server_dir
 mkdir -p $eco_types_dir
@@ -64,16 +62,16 @@ for file in $(find ./x/ecocredit -path -prune -o -name '*.feature'); do
       eco_readme+="\n## Server\n\n"
     fi
     eco_server_count+=1
-    eco_readme+="- [$name]($eco_path/server/$name.html)\n"
-    gherkin2markdown "$file" > "$eco_server_dir/$name.md"
+    eco_readme+="- [$name](./server/$name.html)\n"
+    "$GOBIN/gherkin2markdown" "$file" > "$eco_server_dir/$name.md"
   else
     if [[ $eco_types_count == 0 ]]; then
       eco_readme+="## Types\n\n"
     fi
     eco_types_count+=1
-    eco_readme+="- [$name]($eco_path/types/$name.html)\n"
-    gherkin2markdown "$file" > "$eco_types_dir/$name.md"
+    eco_readme+="- [$name](./types/$name.html)\n"
+    "$GOBIN/gherkin2markdown" "$file" > "$eco_types_dir/$name.md"
   fi
 done
 
-echo -e "$eco_readme" > "docs/$eco_path/README.md"
+echo -e "$eco_readme" > "$eco_dir/README.md"
