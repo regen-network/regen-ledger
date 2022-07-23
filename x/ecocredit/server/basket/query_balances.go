4d667274dfd1e02d4ac45ccdaa0b11be707982a0
MD Aleem <72057206+aleem1314@users.noreply.github.com>
2022-07-23 05:41:50 +0530

    fix(x/ecocredit): improved error for basket queries (#1290)
    * fix: update query response errors
    
    * chore: add changelog
    
    * chore: fix failing test
    
    * Update x/ecocredit/server/basket/query_basket_test.go
    
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    
    * chore: review changes
    
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    (cherry picked from commit d97c5769e78db727b6b20e80281da17a4ce9fe50)

--- x/ecocredit/server/basket/query_balances.go
+++ x/ecocredit/server/basket/query_balances.go
@@ -7,6 +7,8 @@ import (
 	"google.golang.org/grpc/status"
 
 	"github.com/cosmos/cosmos-sdk/orm/model/ormlist"
+	"github.com/cosmos/cosmos-sdk/orm/types/ormerrors"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	api "github.com/regen-network/regen-ledger/api/regen/ecocredit/basket/v1"
 	"github.com/regen-network/regen-ledger/types/ormutil"
@@ -20,7 +22,10 @@ func (k Keeper) BasketBalances(ctx context.Context, request *baskettypes.QueryBa
 
 	basket, err := k.stateStore.BasketTable().GetByBasketDenom(ctx, request.BasketDenom)
 	if err != nil {
-		return nil, err
+		if ormerrors.IsNotFound(err) {
+			return nil, sdkerrors.Wrapf(err, "basket %s not found", request.BasketDenom)
+		}
+		return nil, sdkerrors.Wrapf(err, "failed to get basket %s", request.BasketDenom)
 	}
 
 	pulsarPageReq, err := ormutil.GogoPageReqToPulsarPageReq(request.Pagination)
