dc2c5cbbeb866ba43b4e3ae01d0e341330122e0c
aleem1314 <aleem@vitwit.com>
2022-07-25 12:38:35 +0530

    chore: remove ecocredit migrations

--- x/ecocredit/server/migrations.go
+++ x/ecocredit/server/migrations.go
@@ -3,9 +3,8 @@ package server
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	v3 "github.com/regen-network/regen-ledger/x/ecocredit/migrations/v3"
 )
 
 func (s serverImpl) RunMigrations(ctx sdk.Context, cdc codec.Codec) error {
-	return v3.MigrateState(ctx, s.storeKey, cdc, s.stateStore, s.basketStore, s.paramSpace)
+	return nil
 }
