1127910c3e3439331b38d12d119cd2f0e1fde8aa
aleem1314 <aleem@vitwit.com>
2022-08-02 14:59:18 +0530

    chore: register data module types with authz

--- x/data/codec.go
+++ x/data/codec.go
@@ -2,8 +2,12 @@ package data
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/legacy"
 	"github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
+	authzcodec "github.com/cosmos/cosmos-sdk/x/authz/codec"
 )
 
 func RegisterTypes(registry types.InterfaceRegistry) {
@@ -26,5 +30,12 @@ var (
 )
 
 func init() {
+	RegisterLegacyAminoCodec(legacy.Cdc)
 	RegisterLegacyAminoCodec(amino)
+	cryptocodec.RegisterCrypto(amino)
+	sdk.RegisterLegacyAminoCodec(amino)
+
+	// Register all Amino interfaces and concrete types on the authz Amino codec so that this can later be
+	// used to properly serialize MsgGrant and MsgExec instances
+	RegisterLegacyAminoCodec(authzcodec.Amino)
 }
