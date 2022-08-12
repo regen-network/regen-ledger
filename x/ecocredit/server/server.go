eb6ae5cbf2e5a6484851399fc7f5bb9ab0cdbe25
aleem1314 <aleem@vitwit.com>
2022-08-12 14:46:36 +0530

    feat: add unit tests

--- x/ecocredit/server/server.go
+++ x/ecocredit/server/server.go
@@ -70,7 +70,7 @@ func newServer(storeKey storetypes.StoreKey, paramSpace paramtypes.Subspace,
 	s.basketStore = basketStore
 	s.coreKeeper = core.NewKeeper(coreStore, bankKeeper, s.paramSpace, coreAddr, authority)
 	s.basketKeeper = basket.NewKeeper(basketStore, coreStore, bankKeeper, s.paramSpace, basketAddr)
-	s.marketplaceKeeper = marketplace.NewKeeper(marketStore, coreStore, bankKeeper, s.paramSpace)
+	s.marketplaceKeeper = marketplace.NewKeeper(marketStore, coreStore, bankKeeper, s.paramSpace, authority)
 
 	return s
 }
