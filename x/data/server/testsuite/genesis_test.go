cdbb0cd49316259110aa68f3f3d606545a1cfe64
tyler <{ID}+{username}@users.noreply.github.com>
2022-08-20 16:10:23 -0700

    fix: setup fixture properly

--- x/data/server/testsuite/genesis_test.go
+++ x/data/server/testsuite/genesis_test.go
@@ -3,16 +3,11 @@ package testsuite
 import (
 	"testing"
 
-	sdkmodule "github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/stretchr/testify/suite"
-
-	"github.com/regen-network/regen-ledger/types/fixture"
-	datamodule "github.com/regen-network/regen-ledger/x/data/module"
 )
 
 func TestGenesis(t *testing.T) {
-	ff := fixture.NewFixtureFactory(t, 2)
-	ff.SetModules([]sdkmodule.AppModule{&datamodule.Module{}})
+	ff := setup(t)
 	s := NewGenesisTestSuite(ff)
 	suite.Run(t, s)
 }
