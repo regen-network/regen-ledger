058f89c8eae763e298597524196f8fb66c6913c9
tyler <{ID}+{username}@users.noreply.github.com>
2022-08-23 13:45:13 -0700

    docs: document utility

--- x/ecocredit/server/utils/test_utils.go
+++ x/ecocredit/server/utils/test_utils.go
@@ -15,6 +15,7 @@ import (
 func MatchEvent(event any, emitted sdk.Event) error {
 	tag := "json"
 	valOfEvent := reflect.ValueOf(event)
+	// if the value is a pointer, get the underlying element
 	if valOfEvent.Kind() == reflect.Ptr {
 		valOfEvent = valOfEvent.Elem()
 	}
@@ -22,9 +23,12 @@ func MatchEvent(event any, emitted sdk.Event) error {
 	if typeOfEvent.Kind() != reflect.Struct {
 		return fmt.Errorf("expected event to be struct, got %T", event)
 	}
+
+	// convert key/value of event attributes to map
 	attrMap := mapAttributes(emitted)
 
 	numExportedFields := 0
+	// iterate over the struct fields
 	for i := 0; i < typeOfEvent.NumField(); i++ {
 		underlying := valOfEvent.Field(i)
 		descriptor := typeOfEvent.Field(i)
@@ -32,16 +36,22 @@ func MatchEvent(event any, emitted sdk.Event) error {
 			continue
 		}
 		numExportedFields++
+
+		// extract the json tag, which is the key of the event attribute
 		key := strings.Split(descriptor.Tag.Get(tag), ",")[0]
 		val, ok := attrMap[key]
 		if !ok {
 			return fmt.Errorf("event has no attribute '%s'", key)
 		}
+
+		// convert the underlying struct field value to a string
 		underlyingValue := fmt.Sprintf("%v", underlying.Interface())
+
 		// handle special case for null values
 		if underlyingValue == "<nil>" {
 			underlyingValue = "null"
-		} else if len(val) > 0 && val[0] == '{' { // it's a nested struct
+		} else if len(val) > 0 && val[0] == '{' { // handle nested struct case
+			// nested event structs end up as a json marshalled string, so we must unmarshal it
 			sdkEvent, err := jsonToEvent(val)
 			if err != nil {
 				return err
@@ -54,21 +64,27 @@ func MatchEvent(event any, emitted sdk.Event) error {
 				if err != nil {
 					return err
 				}
-				return MatchEvent(dst.Addr().Interface(), sdkEvent)
+				if err := MatchEvent(dst.Addr().Interface(), sdkEvent); err != nil {
+					return err
+				}
 			} else {
 				dst := reflect.New(typ).Elem()
 				err := json.Unmarshal([]byte(val), dst.Addr().Interface())
 				if err != nil {
 					return err
 				}
-				return MatchEvent(dst.Interface(), sdkEvent)
+				if err := MatchEvent(dst.Interface(), sdkEvent); err != nil {
+					return err
+				}
 			}
 		} else {
+			// it's not a nested struct or nil, so we can just simply compare
 			if underlyingValue != val {
 				return fmt.Errorf("expected %s, got %s for field %s", underlyingValue, val, descriptor.Name)
 			}
 		}
 	}
+	// check that no fields were missed. amount of exported struct fields should be equal to amount of attributes
 	if numAttrs := len(emitted.Attributes); numExportedFields != numAttrs {
 		return fmt.Errorf("emitted event has %d attributes, expected %d", numAttrs, numExportedFields)
 	}
