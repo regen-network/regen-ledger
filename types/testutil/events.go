c2865aaaaf27c82af3c9035c00011b93e944476f
tyler <{ID}+{username}@users.noreply.github.com>
2022-08-24 13:54:46 -0700

    docs: update godoc

--- types/testutil/events.go
+++ types/testutil/events.go
@@ -8,7 +8,7 @@ import (
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
-// MatchEvent matches the values in a proto message struct to the attributes in a sdk.Event.
+// MatchEvent matches the values in a proto message to a sdk.Event.
 func MatchEvent(expected proto.Message, emitted sdk.Event) error {
 	msg, err := sdk.ParseTypedEvent(abci.Event(emitted))
 	if err != nil {
@@ -21,6 +21,8 @@ func MatchEvent(expected proto.Message, emitted sdk.Event) error {
 	return nil
 }
 
+// GetEvent searches through the sdk.Events and attempts to return the sdk.Event whose type matches
+// the proto.MessageName of the passed msg.
 func GetEvent(msg proto.Message, events []sdk.Event) (e sdk.Event, found bool) {
 	for _, e := range events {
 		if proto.MessageName(msg) == e.Type {
