1bdb44fa72d71aec918b06fbb7c8c28216783efa
tyler <{ID}+{username}@users.noreply.github.com>
2022-08-29 15:45:38 -0700

    refactor: simplify query

--- x/ecocredit/server/core/query_all_balances.go
+++ x/ecocredit/server/core/query_all_balances.go
@@ -5,20 +5,19 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/orm/model/ormlist"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	api "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1"
-	"github.com/regen-network/regen-ledger/types/math"
 	"github.com/regen-network/regen-ledger/types/ormutil"
 	"github.com/regen-network/regen-ledger/x/ecocredit/core"
 )
 
-type totalBalances struct {
-	batchDenoms    []string
-	tradableAmount math.Dec
-	retiredAmount  math.Dec
-	escrowedAmount math.Dec
-}
+// problem 1: cannot list by exact match.
+
+// credit type abbrev
+// credit class id
+// batch denom
+
+// return entries straight from ORM
 
 func (k Keeper) AllBalances(ctx context.Context, req *core.QueryAllBalancesRequest) (*core.QueryAllBalancesResponse, error) {
 	pg, err := ormutil.GogoPageReqToPulsarPageReq(req.Pagination)
@@ -31,7 +30,7 @@ func (k Keeper) AllBalances(ctx context.Context, req *core.QueryAllBalancesReque
 	}
 	defer it.Close()
 
-	balanceMap := make(map[string]totalBalances)
+	var res core.QueryAllBalancesResponse
 	for it.Next() {
 		balance, err := it.Value()
 		if err != nil {
@@ -42,63 +41,18 @@ func (k Keeper) AllBalances(ctx context.Context, req *core.QueryAllBalancesReque
 		if err != nil {
 			return nil, err
 		}
-		owner := sdk.AccAddress(balance.Address).String()
-		bal := balanceMap[owner]
-		newBal, err := bal.Add(balance, batch.Denom)
-		if err != nil {
-			return nil, sdkerrors.ErrLogic.Wrapf("unable to add balance for %s in %s denom: %s", owner, batch.Denom, err.Error())
-		}
-		balanceMap[owner] = newBal
-	}
-	var res core.QueryAllBalancesResponse
-	for a, b := range balanceMap {
-		res.Balances = append(res.Balances, &core.CreditBalances{
-			Address: a,
-			Balances: &core.CreditBalance{
-				BatchDenoms:    b.batchDenoms,
-				TradableAmount: b.tradableAmount.String(),
-				RetiredAmount:  b.retiredAmount.String(),
-				EscrowedAmount: b.escrowedAmount.String(),
-			},
+
+		res.Balances = append(res.Balances, &core.BatchBalanceInfo{
+			Address:        sdk.AccAddress(balance.Address).String(),
+			BatchDenom:     batch.Denom,
+			TradableAmount: balance.TradableAmount,
+			RetiredAmount:  balance.RetiredAmount,
+			EscrowedAmount: balance.EscrowedAmount,
 		})
 	}
-	pr, err := ormutil.PulsarPageResToGogoPageRes(it.PageResponse())
+	res.Pagination, err = ormutil.PulsarPageResToGogoPageRes(it.PageResponse())
 	if err != nil {
 		return nil, err
 	}
-	res.Pagination = pr
 	return &res, nil
-}
-
-func (b totalBalances) Add(balance *api.BatchBalance, denom string) (totalBalances, error) {
-	tradable, err := math.NewNonNegativeDecFromString(balance.TradableAmount)
-	if err != nil {
-		return totalBalances{}, err
-	}
-	retired, err := math.NewNonNegativeDecFromString(balance.RetiredAmount)
-	if err != nil {
-		return totalBalances{}, err
-	}
-	escrowed, err := math.NewNonNegativeDecFromString(balance.EscrowedAmount)
-	if err != nil {
-		return totalBalances{}, err
-	}
-
-	sumTradable, err := b.tradableAmount.Add(tradable)
-	if err != nil {
-		return totalBalances{}, err
-	}
-	sumRetired, err := b.retiredAmount.Add(retired)
-	if err != nil {
-		return totalBalances{}, err
-	}
-	sumEscrowed, err := b.escrowedAmount.Add(escrowed)
-	if err != nil {
-		return totalBalances{}, err
-	}
-	b.batchDenoms = append(b.batchDenoms, denom)
-	b.tradableAmount = sumTradable
-	b.retiredAmount = sumRetired
-	b.escrowedAmount = sumEscrowed
-	return b, nil
 }
--- x/ecocredit/server/core/query_all_balances_test.go
+++ x/ecocredit/server/core/query_all_balances_test.go
@@ -15,6 +15,7 @@ func TestQueryAllBalances(t *testing.T) {
 	t.Parallel()
 	s := setupBase(t)
 
+	// using these addresses here to have consistent ordering
 	addr1 := sdk.AccAddress("foo")
 	addr2 := sdk.AccAddress("bar")
 
@@ -27,29 +28,18 @@ func TestQueryAllBalances(t *testing.T) {
 	balance2 := &api.BatchBalance{Address: addr1, BatchKey: bKey2, TradableAmount: "1", RetiredAmount: "2", EscrowedAmount: "3"}
 	balance3 := &api.BatchBalance{Address: addr2, BatchKey: bKey2, TradableAmount: "19", RetiredAmount: "20", EscrowedAmount: "33"}
 
+	balances := []*api.BatchBalance{balance3, balance1, balance2} // the order that ORM sorts the entries
+
 	assert.NilError(t, s.stateStore.BatchBalanceTable().Insert(s.ctx, balance1))
 	assert.NilError(t, s.stateStore.BatchBalanceTable().Insert(s.ctx, balance2))
 	assert.NilError(t, s.stateStore.BatchBalanceTable().Insert(s.ctx, balance3))
 
 	res, err := s.k.AllBalances(s.ctx, &core.QueryAllBalancesRequest{Pagination: &query.PageRequest{Limit: 10, CountTotal: true}})
 	assert.NilError(t, err)
-	assert.Equal(t, len(res.Balances), 2)
+	assert.Equal(t, len(res.Balances), 3)
 	assert.Equal(t, res.Pagination.Total, uint64(3))
 
-	bal1 := res.Balances[0]
-	bal2 := res.Balances[1]
-	assert.Equal(t, bal1.Address, addr2.String())
-	assert.Equal(t, bal2.Address, addr1.String())
-
-	// totals will be balances added from batch1 and batch2.
-	assert.Equal(t, bal2.Balances.TradableAmount, "16")
-	assert.Equal(t, bal2.Balances.RetiredAmount, "17")
-	assert.Equal(t, bal2.Balances.EscrowedAmount, "18")
-	assert.Equal(t, len(bal2.Balances.BatchDenoms), 2)
-
-	// should only have 1 balance
-	assert.Equal(t, bal1.Balances.BatchDenoms[0], "C02-001-20200101-20220101-001")
-	assert.Equal(t, bal1.Balances.TradableAmount, "19")
-	assert.Equal(t, bal1.Balances.RetiredAmount, "20")
-	assert.Equal(t, bal1.Balances.EscrowedAmount, "33")
+	for i, bal := range res.Balances {
+		assertBalanceEqual(s.ctx, t, s.k, bal, balances[i])
+	}
 }
