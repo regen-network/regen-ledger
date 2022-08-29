b86c330611e5e4b886583485f44c88925b875430
MD Aleem <72057206+aleem1314@users.noreply.github.com>
2022-08-30 00:32:20 +0530

    feat(ecocredit): add core state params migrations (#1417)
    * feat: add core state migrations
    
    * chore: add changelog
    
    * Apply suggestions from code review
    
    Co-authored-by: Tyler <48813565+technicallyty@users.noreply.github.com>
    
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
    Co-authored-by: Tyler <48813565+technicallyty@users.noreply.github.com>

--- x/ecocredit/migrations/v3/state.go
+++ x/ecocredit/migrations/v3/state.go
@@ -1,35 +1,62 @@
 package v3
 
 import (
-	basev1beta1 "github.com/cosmos/cosmos-sdk/api/cosmos/base/v1beta1"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 
 	basketapi "github.com/regen-network/regen-ledger/api/regen/ecocredit/basket/v1"
 	api "github.com/regen-network/regen-ledger/api/regen/ecocredit/v1"
+	"github.com/regen-network/regen-ledger/types"
 	"github.com/regen-network/regen-ledger/x/ecocredit/core"
 )
 
 // MigrateState performs in-place store migrations from ConsensusVersion 2 to 3.
-func MigrateState(sdkCtx sdk.Context, ss api.StateStore, basketStore basketapi.StateStore, subspace paramtypes.Subspace) error {
-	// TODO: migrate core params
+func MigrateState(sdkCtx sdk.Context, coreStore api.StateStore, basketStore basketapi.StateStore, subspace paramtypes.Subspace) error {
 
-	// migrate basket params
 	var params core.Params
 	subspace.GetParamSet(sdkCtx, &params)
+
+	// validate credit class fee
+	if err := params.CreditClassFee.Validate(); err != nil {
+		return err
+	}
+
+	// migrate credit class fees
+	classFees := types.CoinsToProtoCoins(params.CreditClassFee)
+	if err := coreStore.ClassFeesTable().Save(sdkCtx, &api.ClassFees{
+		Fees: classFees,
+	}); err != nil {
+		return err
+	}
+
+	// migrate credit class allow list
+	if err := coreStore.AllowListEnabledTable().Save(sdkCtx, &api.AllowListEnabled{
+		Enabled: params.AllowlistEnabled,
+	}); err != nil {
+		return err
+	}
+
+	// migrate allowed class creators to orm table
+	for _, creator := range params.AllowedClassCreators {
+		addr, err := sdk.AccAddressFromBech32(creator)
+		if err != nil {
+			return err
+		}
+
+		if err := coreStore.AllowedClassCreatorTable().Save(sdkCtx, &api.AllowedClassCreator{
+			Address: addr,
+		}); err != nil {
+			return err
+		}
+	}
 
 	// verify basket fee is valid
 	if err := params.BasketFee.Validate(); err != nil {
 		return err
 	}
 
-	basketFees := []*basev1beta1.Coin{}
-	for _, coin := range params.BasketFee {
-		basketFees = append(basketFees, &basev1beta1.Coin{
-			Denom:  coin.Denom,
-			Amount: coin.Amount.String(),
-		})
-	}
+	// migrate basket params
+	basketFees := types.CoinsToProtoCoins(params.BasketFee)
 	if err := basketStore.BasketFeesTable().Save(sdkCtx, &basketapi.BasketFees{
 		Fees: basketFees,
 	}); err != nil {
