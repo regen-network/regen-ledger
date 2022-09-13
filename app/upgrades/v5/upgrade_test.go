6798837c24c8a0577fad7b79dcc4816b582dc6b2
tyler <48813565+technicallyty@users.noreply.github.com>
2022-09-12 19:48:11 -0700

    fix: remove ineffassign

--- app/upgrades/v5/upgrade_test.go
+++ app/upgrades/v5/upgrade_test.go
@@ -28,7 +28,7 @@ func (suite *UpgradeTestSuite) TestV5Upgrade() {
 	plan := upgradetypes.Plan{Name: "v5", Height: dummyUpgradeHeight}
 	err := suite.App.UpgradeKeeper.ScheduleUpgrade(suite.Ctx, plan)
 	suite.Require().NoError(err)
-	plan, exists := suite.App.UpgradeKeeper.GetUpgradePlan(suite.Ctx)
+	_, exists := suite.App.UpgradeKeeper.GetUpgradePlan(suite.Ctx)
 	suite.Require().True(exists)
 
 	suite.Ctx = suite.Ctx.WithBlockHeight(dummyUpgradeHeight)
