ad127d462fd53e78d3b8b2e245626282c1465449
tyler <48813565+technicallyty@users.noreply.github.com>
2022-10-25 10:50:41 -0700

    refactor: use sdkerrors

--- x/intertx/keeper/msg_register_account.go
+++ x/intertx/keeper/msg_register_account.go
@@ -2,17 +2,18 @@ package keeper
 
 import (
 	"context"
-	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	types "github.com/regen-network/regen-ledger/x/intertx/types/v1"
+
+	sdkerrors "cosmossdk.io/errors"
 )
 
 // RegisterAccount implements the Msg/RegisterAccount interface
 func (k Keeper) RegisterAccount(ctx context.Context, msg *types.MsgRegisterAccount) (*types.MsgRegisterAccountResponse, error) {
 	if err := k.icaControllerKeeper.RegisterInterchainAccount(sdk.UnwrapSDKContext(ctx), msg.ConnectionId, msg.Owner, msg.Version); err != nil {
-		wrappedErr := fmt.Errorf("ICAControllerKeeper.RegisterInterchainAccount: %w", err)
+		wrappedErr := sdkerrors.Wrap(err, "error in ICAControllerKeeper.RegisterInterchainAccount")
 		return nil, wrappedErr
 	}
 	return &types.MsgRegisterAccountResponse{}, nil
