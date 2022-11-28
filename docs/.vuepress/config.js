d85432c10ad991108c382788f056446d3b55f04e
Daniel Pittman <dpdanpittman@gmail.com>
2022-11-28 12:46:07 -0500

    docs: added v5 upgrade docs (#1598)
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -202,6 +202,7 @@ module.exports = {
             '/validators/upgrades/v3.0-upgrade',
             '/validators/upgrades/v4.0-upgrade',
             '/validators/upgrades/v4.1-upgrade',
+            // '/validators/upgrades/v5.0-upgrade', // TODO
           ],
         },
       ],
