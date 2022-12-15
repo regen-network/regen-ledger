547a05ea20047fde091a25c0b20e41106969f85e
Daniel Pittman <dpdanpittman@gmail.com>
2022-12-15 01:43:01 -0700

    Removed duplicate import

--- app/client/cli/root.go
+++ app/client/cli/root.go
@@ -11,7 +11,6 @@ import (
 	"github.com/spf13/cast"
 	"github.com/spf13/cobra"
 
-	"github.com/tendermint/tendermint/libs/cli"
 	dbm "github.com/tendermint/tm-db"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
