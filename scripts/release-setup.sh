77e391341393a7397082d380102af72e9fd880a9
Cory <cjlevinson@gmail.com>
2022-12-22 22:05:05 -0800

    build: update 99designs/keyring to use cosmos fork (#1709)
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>

--- scripts/release-setup.sh
+++ scripts/release-setup.sh
@@ -2,10 +2,10 @@
 
 # this script should only be called within github actions running goreleaser
 
-sudo apt-get update
-sudo apt-get install clang
-sudo apt-get install gcc-multilib g++-multilib
-sudo apt-get install gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64
+sudo apt-get update -y
+sudo apt-get install clang -y
+sudo apt-get install gcc-multilib g++-multilib -y
+sudo apt-get install gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64 -y
 
 mkdir /home/runner/work/osxcross
 git clone https://github.com/likhita-809/osxcross-target.git /home/runner/work/osxcross/target
