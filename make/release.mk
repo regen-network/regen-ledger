db98f8c6665cd7c193acfcc603121c4ee4d9b4f4
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2023-01-11 12:58:21 -0800

    update build nightly and cleanup

--- make/release.mk
+++ make/release.mk
@@ -1,3 +1,4 @@
+#!/usr/bin/make -f
 
 GO_MOD                   ?= readonly
 BUILD_TAGS               ?= osusergo,netgo,ledger,static_build
@@ -5,7 +6,6 @@ GO_LINKMODE              ?= external
 RELEASE_TAG              ?= $(shell git describe --tags --abbrev=0)
 GO_MOD_NAME              := $(shell go list -m 2>/dev/null)
 IS_STABLE                := false
-
 
 GORELEASER_SKIP_VALIDATE ?= true
 GORELEASER_DEBUG         ?= false
@@ -21,13 +21,13 @@ GORELEASER_BUILD_VARS := \
 -X github.com/cosmos/cosmos-sdk/version.Version=$(RELEASE_TAG) \
 -X github.com/cosmos/cosmos-sdk/version.Commit=$(GIT_HEAD_COMMIT_LONG)
 
-ldflags = -linkmode=$(GO_LINKMODE) -X github.com/cosmos/cosmos-sdk/version.Name=regen \
+ldflags = -linkmode=$(GO_LINKMODE) \
+-X github.com/cosmos/cosmos-sdk/version.Name=regen \
 -X github.com/cosmos/cosmos-sdk/version.AppName=regen \
 -X github.com/cosmos/cosmos-sdk/version.BuildTags="$(BUILD_TAGS)" \
 -X github.com/cosmos/cosmos-sdk/version.Version=$(shell git describe --tags | sed 's/^v//') \
 -X github.com/cosmos/cosmos-sdk/version.Commit=$(GIT_HEAD_COMMIT_LONG)
 
-# check for nostrip option
 ifeq (,$(findstring nostrip,$(BUILD_OPTIONS)))
 	ldflags                += -s -w
 	GORELEASER_STRIP_FLAGS += -s -w
@@ -38,7 +38,6 @@ ldflags := $(strip $(ldflags))
 
 BUILD_FLAGS := -mod=$(GO_MOD) -tags='$(BUILD_TAGS)' -ldflags '$(ldflags)'
 
-
 ifeq ($(GORELEASER_RELEASE),true)
 	GORELEASER_SKIP_VALIDATE := false
 	GORELEASER_SKIP_PUBLISH  := release --skip-publish=false
@@ -47,8 +46,6 @@ else
 	GORELEASER_SKIP_VALIDATE ?= false
 	GITHUB_TOKEN=
 endif
-
-.PHONY: release
 
 release:
 	docker run \
@@ -71,3 +68,5 @@ release:
 		--skip-validate=$(GORELEASER_SKIP_VALIDATE) \
 		--debug=$(GORELEASER_DEBUG) \
 		--rm-dist
+
+.PHONY: release
