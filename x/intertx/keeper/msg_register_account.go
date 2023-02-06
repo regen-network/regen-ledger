7f923de41df0fe34a2f74ee344ef98c23ef9125f
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2023-02-06 08:36:57 -0800

    fix deprecated errors

--- x/intertx/keeper/msg_register_account.go
+++ x/intertx/keeper/msg_register_account.go
@@ -3,17 +3,17 @@ package keeper
 import (
 	"context"
 
+	"cosmossdk.io/errors"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	types "github.com/regen-network/regen-ledger/x/intertx/types/v1"
-
-	sdkerrors "cosmossdk.io/errors"
 )
 
 // RegisterAccount implements the Msg/RegisterAccount interface
 func (k Keeper) RegisterAccount(ctx context.Context, msg *types.MsgRegisterAccount) (*types.MsgRegisterAccountResponse, error) {
 	if err := k.icaControllerKeeper.RegisterInterchainAccount(sdk.UnwrapSDKContext(ctx), msg.ConnectionId, msg.Owner, msg.Version); err != nil {
-		wrappedErr := sdkerrors.Wrap(err, "error in ICAControllerKeeper.RegisterInterchainAccount")
+		wrappedErr := errors.Wrap(err, "error in ICAControllerKeeper.RegisterInterchainAccount")
 		return nil, wrappedErr
 	}
 	return &types.MsgRegisterAccountResponse{}, nil
