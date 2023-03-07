70e90951c0c1a714038ad53f44f7a0847f481528
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2023-03-07 08:59:58 -0800

    fix import

--- x/ecocredit/server/migrations.go
+++ x/ecocredit/server/migrations.go
@@ -3,10 +3,10 @@ package server
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
-	v4 "github.com/regen-network/regen-ledger/x/ecocredit/v3/migrations/v4"
 
 	ecocreditv1 "github.com/regen-network/regen-ledger/api/v2/regen/ecocredit/v1"
 	v3 "github.com/regen-network/regen-ledger/x/ecocredit/v3/migrations/v3"
+	v4 "github.com/regen-network/regen-ledger/x/ecocredit/v3/migrations/v4"
 )
 
 // Migrator is a struct for handling in-place store migrations.
