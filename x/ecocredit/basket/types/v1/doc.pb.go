0c3ea8d72e7326582723fdf289c4327583e0ef93
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2023-03-28 09:47:09 -0700

    docs: update proto revision comments

--- x/ecocredit/basket/types/v1/doc.pb.go
+++ x/ecocredit/basket/types/v1/doc.pb.go
@@ -9,7 +9,8 @@
 // - Revision 0: v3.0.0 (api/v0.8.0, x/ecocredit/v1.1.0)
 // - Revision 1: v4.0.0 (api/v1.0.0, x/ecocredit/v2.0.0)
 // - Revision 2: v5.0.0 (api/v2.0.0, x/ecocredit/v3.0.0)
-// - Revision 3: (in progress)
+// - Revision 3: v5.1.0 (api/v2.3.0, x/ecocredit/v3.3.0)
+// - Revision 4: (in progress)
 //
 
 package v1
