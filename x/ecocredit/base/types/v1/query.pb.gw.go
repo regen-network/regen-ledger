5c83b1efe049e2d78ad8a810a3d8ff60e8d6c596
Aaron Craelius <aaronc@users.noreply.github.com>
2024-02-16 14:54:33 -0500

    proto-gen

--- x/ecocredit/base/types/v1/query.pb.gw.go
+++ x/ecocredit/base/types/v1/query.pb.gw.go
@@ -4812,9 +4812,9 @@ var (
 
 	pattern_Query_AllowedBridgeChains_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"regen", "ecocredit", "v1", "allowed-bridge-chains"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_ProjectEnrollment_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5, 1, 0, 4, 1, 5, 6}, []string{"regen", "ecocredit", "v1", "project", "project_id", "enrollment", "class_id"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_ProjectEnrollment_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5, 1, 0, 4, 1, 5, 6}, []string{"regen", "ecocredit", "v1", "project", "project_id", "enrollments", "class_id"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_ProjectEnrollments_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5}, []string{"regen", "ecocredit", "v1", "project", "project_id", "enrollment"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_ProjectEnrollments_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5}, []string{"regen", "ecocredit", "v1", "project", "project_id", "enrollments"}, "", runtime.AssumeColonVerbOpt(false)))
 )
 
 var (
