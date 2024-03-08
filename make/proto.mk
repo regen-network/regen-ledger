8d064890f7562887991e7966f121c3e6ac467e44
Robert Zaremba <robert@zaremba.ch>
2024-03-08 23:56:26 +0100

    update proto gen swagger

--- make/proto.mk
+++ make/proto.mk
@@ -37,8 +37,8 @@ proto-check-breaking:
 GOGO_PROTO_URL           = https://raw.githubusercontent.com/regen-network/protobuf/cosmos
 GOOGLE_PROTO_URL         = https://raw.githubusercontent.com/googleapis/googleapis/master
 REGEN_COSMOS_PROTO_URL   = https://raw.githubusercontent.com/regen-network/cosmos-proto/master
-COSMOS_PROTO_URL         = https://raw.githubusercontent.com/cosmos/cosmos-sdk/v0.45.4/proto/cosmos
-COSMOS_ORM_PROTO_URL     = https://raw.githubusercontent.com/cosmos/cosmos-sdk/orm/v1.0.0-alpha.10/proto/cosmos
+COSMOS_PROTO_URL         = https://raw.githubusercontent.com/cosmos/cosmos-sdk/v0.47.10/proto/cosmos
+COSMOS_ORM_PROTO_URL     = https://raw.githubusercontent.com/cosmos/cosmos-sdk/orm/v1.0.0-alpha.12/proto/cosmos
 
 GOGO_PROTO_TYPES         = third_party/proto/gogoproto
 GOOGLE_PROTO_TYPES       = third_party/proto/google
