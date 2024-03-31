86a418034a2aa826c870008f367f9abda7615cc4
Aaron Craelius <aaronc@users.noreply.github.com>
2024-03-31 17:55:05 -0400

    MsgUpdateProjectFee validation tests

--- x/ecocredit/base/types/v1/msg_update_project_fee_test.go
+++ x/ecocredit/base/types/v1/msg_update_project_fee_test.go
@@ -1,3 +1,57 @@
 package v1
 
-// TODO
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/regen-network/gocuke"
+	"github.com/stretchr/testify/require"
+)
+
+type msgUpdateProjectFee struct {
+	gocuke.TestingT
+	msg *MsgUpdateProjectFee
+	err error
+}
+
+func TestMsgUpdateProjectFee(t *testing.T) {
+	gocuke.NewRunner(t, &msgUpdateProjectFee{}).Path("./features/msg_update_project_fee.feature").Run()
+}
+
+func (s *msgUpdateProjectFee) Before() {
+	s.msg = &MsgUpdateProjectFee{}
+}
+
+func (s *msgUpdateProjectFee) Authority(a string) {
+	s.msg.Authority = a
+}
+
+func (s *msgUpdateProjectFee) Fee(a string) {
+	coin, err := sdk.ParseCoinNormalized(a)
+	require.NoError(s, err)
+	s.msg.Fee = &coin
+}
+
+func (s *msgUpdateProjectFee) TheMessageIsValidated() {
+	s.err = s.msg.ValidateBasic()
+}
+
+func (s *msgUpdateProjectFee) ExpectErrorContains(a string) {
+	if a == "" {
+		require.NoError(s, s.err)
+	} else {
+		require.ErrorContains(s, s.err, a)
+	}
+}
+
+func (s *msgUpdateProjectFee) ExpectNoError() {
+	require.NoError(s, s.err)
+}
+
+func (s *msgUpdateProjectFee) ExpectGetsignersReturns(a string) {
+	require.Equal(s, a, s.msg.GetSigners()[0].String())
+}
+
+func (s *msgUpdateProjectFee) NilFee() {
+	s.msg.Fee = nil
+}
