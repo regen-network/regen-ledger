d3836f2b822f0763eb4bc30c6058187df47631e0
Jeancarlo <jeancarlobarrios@gmail.com>
2024-06-25 21:43:27 -0600

    chore: fixing testsuite tests by upgrade to new fixtures and signatures

--- app/testsuite/app_test.go
+++ app/testsuite/app_test.go
@@ -4,8 +4,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/require"
 	dbm "github.com/cometbft/cometbft-db"
+	"github.com/stretchr/testify/require"
 
 	"github.com/cometbft/cometbft/libs/log"
 
@@ -13,18 +13,15 @@ import (
 )
 
 func TestSimAppExportAndBlockedAddrs(t *testing.T) {
-	encCfg := app.MakeEncodingConfig()
 	db := dbm.NewMemDB()
 	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
-	regenApp := NewAppWithCustomOptions(t, false, SetupOptions{
-		Logger:             logger,
-		DB:                 db,
-		InvCheckPeriod:     0,
-		EncConfig:          encCfg,
-		HomePath:           app.DefaultNodeHome,
-		SkipUpgradeHeights: map[int64]bool{},
-		AppOpts:            EmptyAppOptions{},
-	})
+	setupOptions := SetupOptions{
+		Logger:         logger,
+		DB:             db,
+		InvCheckPeriod: 0,
+		AppOpts:        EmptyAppOptions{},
+	}
+	regenApp := NewAppWithCustomOptions(t, false, setupOptions)
 
 	for acc := range app.GetMaccPerms() {
 		require.Equal(t, true, regenApp.BankKeeper.BlockedAddr(regenApp.AccountKeeper.GetModuleAddress(acc)),
@@ -34,7 +31,7 @@ func TestSimAppExportAndBlockedAddrs(t *testing.T) {
 	regenApp.Commit()
 
 	// Making a new app object with the db, so that initchain hasn't been called
-	app2 := app.NewRegenApp(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, map[int64]bool{}, app.DefaultNodeHome, 0, encCfg, EmptyAppOptions{})
-	_, err := app2.ExportAppStateAndValidators(false, []string{})
+	app2 := app.NewRegenApp(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, setupOptions.InvCheckPeriod, EmptyAppOptions{})
+	_, err := app2.ExportAppStateAndValidators(false, []string{}, []string{})
 	require.NoError(t, err, "ExportAppStateAndValidators should not have an error")
 }
